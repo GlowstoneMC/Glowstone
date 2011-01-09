ac773660dbee3860a4de0deb8a8ff01f512fd833
Graham Edgecombe <grahamedgecombe@gmail.com>
2011-01-09 18:00:12 +0000

    All packets added except window-related ones.

---
+++ src/net/lightstone/msg/MultiBlockChangeMessage.java
@@ -0,0 +1,45 @@
+package net.lightstone.msg;
+
+public final class MultiBlockChangeMessage extends Message {
+
+	private final int chunkX, chunkZ;
+	private final short[] coordinates;
+	private final byte[] types, metadata;
+
+	public MultiBlockChangeMessage(int chunkX, int chunkZ, short[] coordinates, byte[] types, byte[] metadata) {
+		if (coordinates.length != types.length || types.length != metadata.length) {
+			throw new IllegalArgumentException();
+		}
+
+		this.chunkX = chunkX;
+		this.chunkZ = chunkZ;
+		this.coordinates = coordinates;
+		this.types = types;
+		this.metadata = metadata;
+	}
+
+	public int getChunkX() {
+		return chunkX;
+	}
+
+	public int getChunkZ() {
+		return chunkZ;
+	}
+
+	public int getChanges() {
+		return coordinates.length;
+	}
+
+	public short[] getCoordinates() {
+		return coordinates;
+	}
+
+	public byte[] getTypes() {
+		return types;
+	}
+
+	public byte[] getMetadata() {
+		return metadata;
+	}
+
+}
