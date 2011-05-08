c289704b384fe863a8b59a56f5705802ee5f7290
SpaceManiac <tad.hardesty@suddenlink.net>
2011-05-07 21:26:24 -0500

    Hopefully complete Inventory implementation

--- src/main/java/net/glowstone/inventory/GlowInventory.java
+++ src/main/java/net/glowstone/inventory/GlowInventory.java
@@ -11,108 +11,180 @@ import org.bukkit.inventory.Inventory;
  */
 public class GlowInventory implements Inventory {
 
+    protected ItemStack[] slots;
+
+    public GlowInventory(int size) {
+        slots = new ItemStack[size];
+    }
+
+    // Basic Stuff ///////////////
+
     public int getSize() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return slots.length;
     }
 
     public String getName() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return "Generic Inventory";
     }
 
+    // Get, Set, Add, Remove /////
+
     public ItemStack getItem(int index) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return slots[index];
     }
 
     public void setItem(int index, ItemStack item) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        slots[index] = item;
     }
 
     public HashMap<Integer, ItemStack> addItem(ItemStack... items) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        HashMap<Integer, ItemStack> result = new HashMap<Integer, ItemStack>();
+        for (ItemStack stack : items) {
+            int open = firstEmpty();
+            if (open < 0) break;
+            slots[open] = stack;
+            result.put(open, stack);
+        }
+        return result;
     }
 
     public HashMap<Integer, ItemStack> removeItem(ItemStack... items) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        HashMap<Integer, ItemStack> result = new HashMap<Integer, ItemStack>();
+        for (ItemStack stack : items) {
+            HashMap<Integer, ? extends ItemStack> stacks = all(stack);
+            for (Integer slot : stacks.keySet()) {
+                slots[slot] = null;
+                result.put(slot, stacks.get(slot));
+            }
+        }
+        return result;
     }
 
     public ItemStack[] getContents() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return slots.clone();
     }
 
     public void setContents(ItemStack[] items) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        slots = items;
     }
 
+    // Contains family ///////////
+
     public boolean contains(int materialId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return first(materialId) >= 0;
     }
 
     public boolean contains(Material material) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return first(material) >= 0;
     }
 
     public boolean contains(ItemStack item) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return first(item) >= 0;
     }
 
     public boolean contains(int materialId, int amount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        HashMap<Integer, ? extends ItemStack> found = all(materialId);
+        int total = 0;
+        for (ItemStack stack : found.values()) {
+            total += stack.getAmount();
+        }
+        return total >= amount;
     }
 
     public boolean contains(Material material, int amount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return contains(material.getId(), amount);
     }
 
     public boolean contains(ItemStack item, int amount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return contains(item.getTypeId(), amount);
     }
 
+    // All Family ////////////////
+
     public HashMap<Integer, ? extends ItemStack> all(int materialId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        HashMap<Integer, ItemStack> result = new HashMap<Integer, ItemStack>();
+        for (int i = 0; i < slots.length; ++i) {
+            if (slots[i].getTypeId() == materialId) {
+                result.put(i, slots[i]);
+            }
+        }
+        return result;
     }
 
     public HashMap<Integer, ? extends ItemStack> all(Material material) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return all(material.getId());
     }
 
     public HashMap<Integer, ? extends ItemStack> all(ItemStack item) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        HashMap<Integer, ItemStack> result = new HashMap<Integer, ItemStack>();
+        for (int i = 0; i < slots.length; ++i) {
+            if (slots[i] != null && slots[i].equals(item)) {
+                result.put(i, slots[i]);
+            }
+        }
+        return result;
     }
 
+    // First Family //////////////
+
     public int first(int materialId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        for (int i = 0; i < slots.length; ++i) {
+            if (slots[i] != null && slots[i].getTypeId() == materialId) return i;
+        }
+        return -1;
     }
 
     public int first(Material material) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return first(material.getId());
     }
 
     public int first(ItemStack item) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        for (int i = 0; i < slots.length; ++i) {
+            if (slots[i] != null && slots[i].equals(item)) return i;
+        }
+        return -1;
     }
 
     public int firstEmpty() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        for (int i = 0; i < slots.length; ++i) {
+            if (slots[i] == null) return i;
+        }
+        return -1;
     }
 
+    // Remove Family /////////////
+
     public void remove(int materialId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        HashMap<Integer, ? extends ItemStack> stacks = all(materialId);
+        for (Integer slot : stacks.keySet()) {
+            slots[slot] = null;
+        }
     }
 
     public void remove(Material material) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        HashMap<Integer, ? extends ItemStack> stacks = all(material);
+        for (Integer slot : stacks.keySet()) {
+            slots[slot] = null;
+        }
     }
 
     public void remove(ItemStack item) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        HashMap<Integer, ? extends ItemStack> stacks = all(item);
+        for (Integer slot : stacks.keySet()) {
+            slots[slot] = null;
+        }
     }
 
+    // Clear Family //////////////
+
     public void clear(int index) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        slots[index] = null;
     }
 
     public void clear() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        for (int i = 0; i < slots.length; ++i) {
+            slots[i] = null;
+        }
     }
     
 }
