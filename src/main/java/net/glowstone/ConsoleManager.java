ce01174cf20d90e015be16bf2be21f331a7be621
SpaceManiac <tad.hardesty@platymuus.com>
2011-12-07 01:13:41 -0600

    Implement new ConsoleCommandSender stuff

--- src/main/java/net/glowstone/ConsoleManager.java
+++ src/main/java/net/glowstone/ConsoleManager.java
@@ -22,6 +22,7 @@ import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Set;
 import java.util.logging.Formatter;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
@@ -46,6 +47,8 @@ import jline.SimpleCompletor;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandException;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.permissions.*;
+import org.bukkit.plugin.Plugin;
 
 /**
  * A meta-class to handle all logging and input-related console improvements.
@@ -257,14 +260,75 @@ public final class ConsoleManager {
         }
     }
     
-    private class ColoredCommandSender extends ConsoleCommandSender {
-        public ColoredCommandSender() {
-            super(server);
+    private class ColoredCommandSender implements ConsoleCommandSender {
+        private final PermissibleBase perm = new PermissibleBase(this);
+        
+        public String getName() {
+            return "CONSOLE";
         }
         
-        @Override
         public void sendMessage(String text) {
             server.getLogger().info(text);
+        }
+
+        public boolean isOp() {
+            return true;
+        }
+
+        public void setOp(boolean value) {
+            throw new UnsupportedOperationException("Cannot change operator status of server console");
+        }
+
+        public boolean isPlayer() {
+            return false;
+        }
+
+        public GlowServer getServer() {
+            return server;
+        }
+
+        public boolean isPermissionSet(String name) {
+            return perm.isPermissionSet(name);
+        }
+
+        public boolean isPermissionSet(Permission perm) {
+            return this.perm.isPermissionSet(perm);
+        }
+
+        public boolean hasPermission(String name) {
+            return perm.hasPermission(name);
+        }
+
+        public boolean hasPermission(Permission perm) {
+            return this.perm.hasPermission(perm);
+        }
+
+        public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value) {
+            return perm.addAttachment(plugin, name, value);
+        }
+
+        public PermissionAttachment addAttachment(Plugin plugin) {
+            return perm.addAttachment(plugin);
+        }
+
+        public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value, int ticks) {
+            return perm.addAttachment(plugin, name, value, ticks);
+        }
+
+        public PermissionAttachment addAttachment(Plugin plugin, int ticks) {
+            return perm.addAttachment(plugin, ticks);
+        }
+
+        public void removeAttachment(PermissionAttachment attachment) {
+            perm.removeAttachment(attachment);
+        }
+
+        public void recalculatePermissions() {
+            perm.recalculatePermissions();
+        }
+
+        public Set<PermissionAttachmentInfo> getEffectivePermissions() {
+            return perm.getEffectivePermissions();
         }
     }
     
