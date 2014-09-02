6bc1b917e1c9a0a6000614cb3a5ed430025120eb
Tad Hardesty <tad@platymuus.com>
2014-09-02 11:29:53 -0500

    Updated version number for 1.8.

--- src/main/java/net/glowstone/GlowServer.java
+++ src/main/java/net/glowstone/GlowServer.java
@@ -60,12 +60,12 @@ public final class GlowServer implements Server {
     /**
      * The game version supported by the server.
      */
-    public static final String GAME_VERSION = "1.8-pre3";
+    public static final String GAME_VERSION = "1.8";
 
     /**
      * The protocol version supported by the server.
      */
-    public static final int PROTOCOL_VERSION = 46;
+    public static final int PROTOCOL_VERSION = 47;
 
     /**
      * Creates a new server on TCP port 25565 and starts listening for
