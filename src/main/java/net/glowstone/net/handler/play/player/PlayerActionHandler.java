79198455af8a86ceefea77d760023a1f88c94816
Tad Hardesty <tad@platymuus.com>
2014-09-05 22:38:10 -0500

    Fixed sneaking and sprinting handling in PlayerActionHandler (fixes #165).

--- src/main/java/net/glowstone/net/handler/play/player/PlayerActionHandler.java
+++ src/main/java/net/glowstone/net/handler/play/player/PlayerActionHandler.java
@@ -9,25 +9,21 @@ import net.glowstone.net.message.play.player.PlayerActionMessage;
 public final class PlayerActionHandler implements MessageHandler<GlowSession, PlayerActionMessage> {
     public void handle(GlowSession session, PlayerActionMessage message) {
         final GlowPlayer player = session.getPlayer();
-        if (message.getId() != player.getEntityId()) {
-            // vanilla client doesn't send this, ignore the message for now
-            return;
-        }
 
         switch (message.getAction()) {
-            case 1: // crouch
+            case 0: // crouch
                 player.setSneaking(true);
                 break;
-            case 2: // uncrouch
+            case 1: // uncrouch
                 player.setSneaking(false);
                 break;
-            case 3: // leave bed
+            case 2: // leave bed
                 // todo
                 break;
-            case 4: // start sprinting
+            case 3: // start sprinting
                 player.setSprinting(true);
                 break;
-            case 5: // stop sprinting
+            case 4: // stop sprinting
                 player.setSprinting(false);
                 break;
             default:
