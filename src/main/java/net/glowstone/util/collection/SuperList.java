27d0318cb8700d285075757b52e61a0fd49684a8
Marcos Vives Del Sol <socram8888@gmail.com>
2014-10-02 18:14:03 +0200

    Add new useful JCF classes

---
+++ src/main/java/net/glowstone/util/collection/SuperList.java
@@ -0,0 +1,217 @@
+package net.glowstone.util.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Set which delegates to other sets.
+ */
+public class SuperList<E> extends SuperCollection<E> implements List<E> {
+
+    public SuperList() {
+        super(SuperCollection.AdditionMode.LAST);
+    }
+
+    public SuperList(List<List<E>> parents) {
+        super(parents, SuperCollection.AdditionMode.LAST);
+    }
+
+    @Override
+    public List<List<E>> getParents() {
+        return (List<List<E>>) super.getParents();
+    }
+
+    @Override
+    public List<E> asClone() {
+        List<E> output = new ArrayList<>();
+
+        for (List<E> parent : getParents()) {
+            output.addAll(parent);
+        }
+
+        return output;
+    }
+
+    @Override
+    protected Class<? extends Collection> getCollectionClass() {
+        return List.class;
+    }
+
+    @Override
+    public void add(int index, E object) {
+        int relativePos = index;
+
+        for (List<E> parent : getParents()) {
+            int parentSize = parent.size();
+
+            if (parentSize <= relativePos) {
+                parent.add(relativePos, object);
+                return;
+            }
+
+            relativePos -= parentSize;
+        }
+
+        throw new IndexOutOfBoundsException("This list does not contain position " + index);
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> objects) {
+        int relativePos = index;
+        int modified = 0;
+
+        for (List<E> parent : getParents()) {
+            int parentSize = parent.size();
+
+            if (parentSize <= relativePos) {
+                return parent.addAll(relativePos, objects);
+            }
+
+            relativePos -= parentSize;
+        }
+
+        throw new IndexOutOfBoundsException("This list does not contain position " + index);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        if (object == this) {
+            return true;
+        }
+
+        if (!(object instanceof List)) {
+            return false;
+        }
+
+        List other = (List) object;
+
+        if (other.size() != size()) {
+            return false;
+        }
+
+        Iterator thisIterator = iterator();
+        Iterator otherIterator = other.iterator();
+        while (thisIterator.hasNext()) {
+            if (!thisIterator.next().equals(otherIterator.next())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public E get(int index) {
+        int relativePos = index;
+
+        for (List<E> parent : getParents()) {
+            int parentSize = parent.size();
+
+            if (relativePos < parentSize) {
+                return parent.get(relativePos);
+            }
+
+            relativePos -= parentSize;
+        }
+
+        throw new IndexOutOfBoundsException("This list does not contain position " + index);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        // Override if possible, because this is *really* slow
+        return new SuperIterator(getParents());
+    }
+
+    @Override
+    public E remove(int index) {
+        int relativePos = index;
+
+        for (List<E> parent : getParents()) {
+            int parentSize = parent.size();
+
+            if (relativePos < parentSize) {
+                return parent.remove(relativePos);
+            }
+
+            relativePos -= parentSize;
+        }
+
+        throw new IndexOutOfBoundsException("This list does not contain position " + index);
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        int relativePos = 0;
+
+        for (List<E> parent : getParents()) {
+            int parentIndex = parent.indexOf(object);
+
+            if (parentIndex >= 0) {
+                return relativePos + parentIndex;
+            }
+
+            relativePos += parent.size();
+        }
+
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        List<List<E>> parents = getParents();
+        ListIterator<List<E>> iterator = parents.listIterator(parents.size());
+        int index = -1;
+
+        while (iterator.hasPrevious()) {
+            List<E> parent = iterator.next();
+
+            if (index < 0) {
+                index = parent.lastIndexOf(object);
+            } else {
+                index += parent.size();
+            }
+        }
+
+        return index;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return new SuperListIterator(getParents());
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return new SuperListIterator(getParents(), index);
+    }
+
+    @Override
+    public E set(int index, E object) {
+        int relativePos = index;
+
+        for (List<E> parent : getParents()) {
+            int parentSize = parent.size();
+
+            if (relativePos < parentSize) {
+                return parent.set(relativePos, object);
+            }
+
+            relativePos -= parentSize;
+        }
+
+        throw new IndexOutOfBoundsException("This list does not contain position " + index);
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException("TODO - NOT YET IMPLEMENTED");
+    }
+}
---
+++ src/main/java/net/glowstone/util/collection/SuperListIterator.java
@@ -0,0 +1,129 @@
+package net.glowstone.util.collection;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+/**
+ * An Iterator which delegates in other iterators.
+ */
+public class SuperListIterator<E> implements ListIterator<E> {
+    private final ListIterator<List<E>> parentIterator;
+    private ListIterator<E> childIterator;
+    private int index;
+
+    public SuperListIterator(List<List<E>> parents) {
+        this(parents.listIterator());
+    }
+
+    public SuperListIterator(ListIterator<List<E>> parentIterator) {
+        this.parentIterator = parentIterator;
+    }
+
+    public SuperListIterator(List<List<E>> parents, int index) {
+        this(parents);
+        this.index = index;
+
+        while (parentIterator.hasNext()) {
+            List<E> child = parentIterator.next();
+            int childSize = child.size(); 
+
+            if (index <= childSize) {
+                childIterator = child.listIterator(index);
+                return;
+            }
+
+            index -= childSize;
+        }
+
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (childIterator == null || !childIterator.hasNext()) {
+            if (parentIterator.hasNext()) {
+                childIterator = parentIterator.next().listIterator();
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void checkNext() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public int nextIndex() {
+        checkNext();
+        return index + 1;
+    }
+
+    @Override
+    public E next() {
+        checkNext();
+        index++;
+        return childIterator.next();
+    }
+    
+    @Override
+    public boolean hasPrevious() {
+        while (childIterator == null || !childIterator.hasPrevious()) {
+            if (parentIterator.hasPrevious()) {
+                List<E> child = parentIterator.previous();
+                childIterator = child.listIterator(child.size());
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void checkPrevious() {
+        if (!hasPrevious()) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public int previousIndex() {
+        checkPrevious();
+        return index - 1;
+    }
+
+    @Override
+    public E previous() {
+        checkPrevious();
+        index--;
+        return childIterator.previous();
+    }
+
+    @Override
+    public void add(E object) {
+        throw new UnsupportedOperationException("TODO - NOT YET IMPLEMENTED");
+    }
+
+    @Override
+    public void remove() {
+        if (childIterator == null) {
+            throw new IllegalStateException("next() must be called before using remove()");
+        }
+
+        childIterator.remove();
+    }
+
+    @Override
+    public void set(E object) {
+        if (childIterator == null) {
+            throw new IllegalStateException("next() must be called before using remove()");
+        }
+
+        childIterator.set(object);
+    }
+}
