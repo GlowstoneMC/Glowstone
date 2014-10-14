d09f072f913361fcf63127ed15401b62f9e711dc
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2014-10-14 12:27:22 +0200

    Add BlockCocoa

---
+++ src/main/java/net/glowstone/block/blocktype/BlockCocoa.java
@@ -0,0 +1,57 @@
+package net.glowstone.block.blocktype;
+
+import net.glowstone.EventFactory;
+import net.glowstone.block.GlowBlock;
+import net.glowstone.block.GlowBlockState;
+import net.glowstone.entity.GlowPlayer;
+
+import org.bukkit.event.block.BlockGrowEvent;
+import org.bukkit.material.CocoaPlant;
+import org.bukkit.material.CocoaPlant.CocoaPlantSize;
+import org.bukkit.material.MaterialData;
+
+public class BlockCocoa extends BlockAttachable implements IBlockGrowable {
+
+    @Override
+    public boolean isFertilizable(GlowBlock block) {
+        final MaterialData data = block.getState().getData();
+        if (data instanceof CocoaPlant) {
+            if (((CocoaPlant) data).getSize() != CocoaPlantSize.LARGE) {
+                return true;
+            }
+        } else {
+            warnMaterialData(CocoaPlant.class, data);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean canGrowWithChance(GlowBlock block) {
+        return true;
+    }
+
+    @Override
+    public void grow(GlowPlayer player, GlowBlock block) {
+        final MaterialData data = block.getState().getData();
+        if (data instanceof CocoaPlant) {
+            final CocoaPlant cocoa = (CocoaPlant) data;
+            CocoaPlantSize size = cocoa.getSize();
+            if (size.equals(CocoaPlantSize.SMALL)) {
+                cocoa.setSize(CocoaPlantSize.MEDIUM);
+            } else if (size.equals(CocoaPlantSize.MEDIUM)) {
+                cocoa.setSize(CocoaPlantSize.LARGE);
+            } else {
+                return;
+            }
+            final GlowBlockState state = block.getState();
+            state.setData(cocoa);
+            BlockGrowEvent growEvent = new BlockGrowEvent(block, state);
+            EventFactory.callEvent(growEvent);
+            if (!growEvent.isCancelled()) {
+                state.update(true);
+            }
+        } else {
+            warnMaterialData(CocoaPlant.class, data);
+        }
+    }
+}
