507ce06e6763edc7147b4f24d5ef8bc1738fe6e7
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2014-10-15 10:29:12 +0200

    Add BlockSapling

--- src/main/java/net/glowstone/block/ItemTable.java
+++ src/main/java/net/glowstone/block/ItemTable.java
@@ -192,6 +192,7 @@ public final class ItemTable {
         reg(Material.TNT, new BlockTNT());
         reg(Material.BROWN_MUSHROOM, new BlockMushroom(Material.BROWN_MUSHROOM));
         reg(Material.RED_MUSHROOM, new BlockMushroom(Material.RED_MUSHROOM));
+        reg(Material.SAPLING, new BlockSapling());
 
         reg(Material.FLINT_AND_STEEL, new ItemFlintAndSteel());
         reg(Material.SIGN, new ItemSign());
---
+++ src/main/java/net/glowstone/block/blocktype/BlockSapling.java
@@ -0,0 +1,55 @@
+package net.glowstone.block.blocktype;
+
+import org.bukkit.TreeSpecies;
+import org.bukkit.TreeType;
+import org.bukkit.block.BlockFace;
+import org.bukkit.material.MaterialData;
+import org.bukkit.material.Tree;
+
+import net.glowstone.block.GlowBlock;
+
+public class BlockSapling extends BlockType {
+
+    @Override
+    public boolean canTickRandomly() {
+        return true;
+    }
+
+    @Override
+    public void updateBlock(GlowBlock block) {
+        if (block.getRelative(BlockFace.UP).getLightLevel() >= 9 && random.nextInt(7) == 0) {
+            int dataValue = block.getData();
+            if ((dataValue & 8) == 0) {
+                block.setData((byte) (dataValue | 8));
+            } else {
+                final MaterialData data = block.getState().getData();
+                if (data instanceof Tree) {
+                    final Tree tree = (Tree) data;
+                    final TreeType type = getTreeType(tree.getSpecies());
+                    block.getWorld().generateTree(block.getLocation(), type);
+                } else {
+                    warnMaterialData(Tree.class, data);
+                }
+            }
+        }
+    }
+
+    private TreeType getTreeType(TreeSpecies species) {
+        switch (species) {
+            case GENERIC:
+                return TreeType.TREE;
+            case REDWOOD:
+                return TreeType.REDWOOD;
+            case BIRCH:
+                return TreeType.BIRCH;
+            case JUNGLE:
+                return TreeType.JUNGLE;
+            case ACACIA:
+                return TreeType.ACACIA;
+            case DARK_OAK:
+                return TreeType.DARK_OAK;
+            default:
+                return TreeType.TREE;
+        }
+    }
+}
