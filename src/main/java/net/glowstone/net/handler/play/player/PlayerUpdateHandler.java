281e3c1b7a49d84e904511b9038ee1adc13e9d44
dequis <dx@dxzone.com.ar>
2014-10-18 19:36:13 -0300

    Call PlayerMoveEvent.
    Based partially on the code of @JeromSar. The event is called more often
    than CraftBukkit due to the lack of movement buffering.

--- src/main/java/net/glowstone/net/handler/play/player/PlayerUpdateHandler.java
+++ src/main/java/net/glowstone/net/handler/play/player/PlayerUpdateHandler.java
@@ -1,9 +1,12 @@
 package net.glowstone.net.handler.play.player;
 
 import com.flowpowered.networking.MessageHandler;
+import net.glowstone.EventFactory;
 import net.glowstone.net.GlowSession;
+import net.glowstone.net.message.play.game.PositionRotationMessage;
 import net.glowstone.net.message.play.player.PlayerUpdateMessage;
 import org.bukkit.Location;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public final class PlayerUpdateHandler implements MessageHandler<GlowSession, PlayerUpdateMessage> {
 
@@ -11,6 +14,7 @@ public final class PlayerUpdateHandler implements MessageHandler<GlowSession, Pl
     public void handle(GlowSession session, PlayerUpdateMessage message) {
         Location original = session.getPlayer().getLocation();
         Location newLoc = original.clone();
+        Location finalLoc = newLoc;
         message.update(newLoc);
 
         // don't let players move more than 16 blocks in a single packet.
@@ -20,7 +24,22 @@ public final class PlayerUpdateHandler implements MessageHandler<GlowSession, Pl
             return;
         }
 
+        if (!original.equals(newLoc)) {
+            final PlayerMoveEvent event = EventFactory.callEvent(new PlayerMoveEvent(session.getPlayer(), original, newLoc));
+
+            if (event.isCancelled()) {
+                finalLoc = original;
+            } else if (!event.getTo().equals(newLoc)) {
+                finalLoc = event.getTo();
+            }
+
+            if (!newLoc.equals(finalLoc)) {
+                session.send(new PositionRotationMessage(finalLoc, session.getPlayer().getEyeHeight(true)));
+            }
+        }
+
+
         // do stuff with onGround if we need to
-        session.getPlayer().setRawLocation(newLoc);
+        session.getPlayer().setRawLocation(finalLoc);
     }
 }
