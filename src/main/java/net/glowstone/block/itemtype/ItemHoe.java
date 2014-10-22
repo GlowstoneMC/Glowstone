acc5ed2000847d4145dfef8174aadd658acf8e90
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2014-10-22 09:52:40 +0200

    Fixed hoe interaction with coarse dirt and podzol.

--- src/main/java/net/glowstone/block/itemtype/ItemHoe.java
+++ src/main/java/net/glowstone/block/itemtype/ItemHoe.java
@@ -12,10 +12,16 @@ public class ItemHoe extends ItemType {
 
     @Override
     public void rightClickBlock(GlowPlayer player, GlowBlock target, BlockFace face, ItemStack holding, Vector clickedLoc) {
-        if ((target.getType() == Material.GRASS || target.getType() == Material.DIRT) &&
-                target.getRelative(BlockFace.UP).getType() == Material.AIR) {
-            target.getWorld().playSound(target.getLocation().add(0.5D, 0.5D, 0.5D), Sound.STEP_GRAVEL, 1, 0.8F);
-            target.setType(Material.SOIL);
+        if (target.getRelative(BlockFace.UP).getType() == Material.AIR) {
+            if (target.getType() == Material.GRASS || (target.getType() == Material.DIRT && target.getData() == 0)) {
+                // grass or ordinary dirt: become soil
+                target.getWorld().playSound(target.getLocation().add(0.5D, 0.5D, 0.5D), Sound.STEP_GRAVEL, 1, 0.8F);
+                target.setType(Material.SOIL);
+            } else if (target.getType() == Material.DIRT && target.getData() == 1) {
+                // coarse dirt: become regular dirt
+                target.getWorld().playSound(target.getLocation().add(0.5D, 0.5D, 0.5D), Sound.STEP_GRAVEL, 1, 0.8F);
+                target.setData((byte) 0); // changing it to normal dirt
+            }
         }
     }
 }
