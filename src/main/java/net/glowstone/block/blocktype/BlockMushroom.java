837467cb6c668cb95882b5ae474510b4743a82b6
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2014-10-30 15:53:07 +0100

    Fill BlockMushroom and BlockSapling grow() method

--- src/main/java/net/glowstone/block/blocktype/BlockMushroom.java
+++ src/main/java/net/glowstone/block/blocktype/BlockMushroom.java
@@ -1,9 +1,19 @@
 package net.glowstone.block.blocktype;
 
-import org.bukkit.Material;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.TreeType;
+import org.bukkit.block.BlockState;
+import org.bukkit.event.world.StructureGrowEvent;
+
+import net.glowstone.EventFactory;
 import net.glowstone.block.GlowBlock;
 import net.glowstone.entity.GlowPlayer;
+import net.glowstone.generator.TreeGenerator;
+import net.glowstone.util.BlockStateDelegate;
 
 public class BlockMushroom extends BlockType implements IBlockGrowable {
 
@@ -25,5 +35,26 @@ public class BlockMushroom extends BlockType implements IBlockGrowable {
 
     @Override
     public void grow(GlowPlayer player, GlowBlock block) {
+        TreeType type;
+        if (mushroomType.equals(Material.BROWN_MUSHROOM)) {
+            type = TreeType.BROWN_MUSHROOM;
+        } else if (mushroomType.equals(Material.RED_MUSHROOM)) {
+            type = TreeType.RED_MUSHROOM;
+        } else {
+            return;
+        }
+        final Location loc = block.getLocation();
+        final BlockStateDelegate blockStateDelegate = new BlockStateDelegate();
+        final TreeGenerator generator = new TreeGenerator(blockStateDelegate);
+        if (generator.generate(random, loc, type)) {
+            final List<BlockState> blockStates = new ArrayList<BlockState>(blockStateDelegate.getBlockStates());
+            StructureGrowEvent growEvent = new StructureGrowEvent(loc, type, true, player, blockStates);
+            EventFactory.callEvent(growEvent);
+            if (!growEvent.isCancelled()) {
+                for (BlockState state : blockStates) {
+                    state.update(true);
+                }
+            }
+        }
     }
 }
