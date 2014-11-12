97f59f826ae4ea740b22ce6d28c47d09568ef9a4
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2014-11-12 17:14:56 +0100

    Fix seeds, potatoes and carrots placement checks

--- src/main/java/net/glowstone/block/itemtype/ItemFoodSeeds.java
+++ src/main/java/net/glowstone/block/itemtype/ItemFoodSeeds.java
@@ -20,7 +20,7 @@ public class ItemFoodSeeds extends ItemFood {
 
     @Override
     public void rightClickBlock(GlowPlayer player, GlowBlock target, BlockFace face, ItemStack holding, Vector clickedLoc) {
-        if (target.getType() == soilType && target.getRelative(BlockFace.UP).getType() == Material.AIR) {
+        if (target.getType() == soilType && target.getRelative(BlockFace.UP).getType() == Material.AIR && face == BlockFace.UP) {
             final GlowBlockState state = target.getRelative(BlockFace.UP).getState();
             state.setType(cropsType);
             state.setRawData((byte) 0);
