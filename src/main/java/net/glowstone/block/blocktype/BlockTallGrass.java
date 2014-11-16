5a88e9626d9c66b97e986d414992ae9b7790f7ae
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2014-11-16 09:32:30 +0100

    Check above block is air when growing tall grass

--- src/main/java/net/glowstone/block/blocktype/BlockTallGrass.java
+++ src/main/java/net/glowstone/block/blocktype/BlockTallGrass.java
@@ -61,17 +61,19 @@ public class BlockTallGrass extends BlockNeedsAttached implements IBlockGrowable
         if (data instanceof LongGrass) {
             final GrassSpecies species = ((LongGrass) data).getSpecies();
             if (species == GrassSpecies.NORMAL || species == GrassSpecies.FERN_LIKE) {
-                final GlowBlockState blockState = block.getState();
                 final GlowBlockState headBlockState = block.getRelative(BlockFace.UP).getState();
-                blockState.setType(Material.DOUBLE_PLANT);
-                blockState.setRawData((byte) (species.ordinal() + 1));
-                headBlockState.setType(Material.DOUBLE_PLANT);
-                headBlockState.setRawData((byte) 8);
-                BlockGrowEvent growEvent = new BlockGrowEvent(block, blockState);
-                EventFactory.callEvent(growEvent);
-                if (!growEvent.isCancelled()) {
-                    blockState.update(true);
-                    headBlockState.update(true);
+                if (headBlockState.getType() == Material.AIR) {
+                    final GlowBlockState blockState = block.getState();
+                    blockState.setType(Material.DOUBLE_PLANT);
+                    blockState.setRawData((byte) (species.ordinal() + 1));
+                    headBlockState.setType(Material.DOUBLE_PLANT);
+                    headBlockState.setRawData((byte) 8);
+                    BlockGrowEvent growEvent = new BlockGrowEvent(block, blockState);
+                    EventFactory.callEvent(growEvent);
+                    if (!growEvent.isCancelled()) {
+                        blockState.update(true);
+                        headBlockState.update(true);
+                    }
                 }
             }
         } else {
