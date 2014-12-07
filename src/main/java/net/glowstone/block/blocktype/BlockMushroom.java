5ebc1246057c21522815de037f0732d96e6aab2d
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2014-12-07 10:32:40 +0100

    Update BlockMushroom to use Dirt MaterialData

--- src/main/java/net/glowstone/block/blocktype/BlockMushroom.java
+++ src/main/java/net/glowstone/block/blocktype/BlockMushroom.java
@@ -3,12 +3,15 @@ package net.glowstone.block.blocktype;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.DirtType;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.TreeType;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.event.world.StructureGrowEvent;
+import org.bukkit.material.MaterialData;
+import org.bukkit.material.Dirt;
 
 import net.glowstone.EventFactory;
 import net.glowstone.block.GlowBlock;
@@ -28,13 +31,12 @@ public class BlockMushroom extends BlockAttachable implements IBlockGrowable {
     public boolean canPlaceAt(GlowBlock block, BlockFace against) {
         final GlowBlock belowBlock = block.getRelative(BlockFace.DOWN);
         final Material type = belowBlock.getType();
-        if (type == Material.GRASS ||
-                (type == Material.DIRT && belowBlock.getData() != 2)) {
+        final MaterialData data = belowBlock.getState().getData();
+        if (type == Material.GRASS || (data instanceof Dirt && ((Dirt) data).getType() != DirtType.PODZOL)) {
             if (block.getLightLevel() < 13) { // checking light level for dirt, coarse dirt and grass
                 return true;
             }
-        } else if (type == Material.MYCEL ||
-                (type == Material.DIRT && belowBlock.getData() == 2)) {
+        } else if (type == Material.MYCEL || (data instanceof Dirt && ((Dirt) data).getType() == DirtType.PODZOL)) {
             // not checking light level if mycel or podzol
             return true;
         }
