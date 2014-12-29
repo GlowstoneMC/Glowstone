275df7051ddd32abb5e58d2cce789a8e56c1dcf2
turt2live <travpc@gmail.com>
2014-12-28 19:34:07 -0700

    Fix order of arguments

--- src/main/java/net/glowstone/constants/GlowEnchantment.java
+++ src/main/java/net/glowstone/constants/GlowEnchantment.java
@@ -128,31 +128,31 @@ public final class GlowEnchantment extends Enchantment implements WeightedRandom
     private static final int GROUP_DIG = 3;
 
     private static enum Impl {
-        PROTECTION_ENVIRONMENTAL(0, "Protection", 10, 1, 11, 20, 4, EnchantmentTarget.ARMOR, GROUP_PROTECT),
-        PROTECTION_FIRE(1, "Fire Protection", 5, 10, 8, 12, 4, EnchantmentTarget.ARMOR, GROUP_PROTECT),
-        PROTECTION_FALL(2, "Feather Falling", 5, 5, 6, 10, 4, EnchantmentTarget.ARMOR_FEET, GROUP_PROTECT),
-        PROTECTION_EXPLOSIONS(3, "Blast Protection", 2, 5, 8, 12, 4, EnchantmentTarget.ARMOR),
-        PROTECTION_PROJECTILE(4, "Projectile Protection", 5, 3, 6, 15, 4, EnchantmentTarget.ARMOR, GROUP_PROTECT),
-        OXYGEN(5, "Respiration", 2, 10, 10, 30, 3, EnchantmentTarget.ARMOR_HEAD),
-        WATER_WORKER(6, "Aqua Affinity", 2, 1, 0, 40, 1, EnchantmentTarget.ARMOR_HEAD),
-        THORNS(7, "Thorns", 1, 10, 20, 50, 3, EnchantmentTarget.ARMOR_TORSO, new MatcherAdapter(EnchantmentTarget.ARMOR)),
-        DEPTH_STRIDER(8, "Depth Strider", 2, 10, 10, 15, 3, EnchantmentTarget.ARMOR_FEET),
-        DAMAGE_ALL(16, "Sharpness", 10, 1, 11, 20, 5, EnchantmentTarget.WEAPON, SWORD_OR_AXE, GROUP_ATTACK),
-        DAMAGE_UNDEAD(17, "Smite", 5, 5, 8, 20, 5, EnchantmentTarget.WEAPON, SWORD_OR_AXE, GROUP_ATTACK),
-        DAMAGE_ARTHROPODS(18, "Bane of Arthropods", 5, 5, 8, 20, 5, EnchantmentTarget.WEAPON, SWORD_OR_AXE, GROUP_ATTACK),
-        KNOCKBACK(19, "Knockback", 5, 5, 20, 50, 2, EnchantmentTarget.WEAPON),
-        FIRE_ASPECT(20, "Fire Aspect", 2, 10, 20, 50, 2, EnchantmentTarget.WEAPON),
-        LOOT_BONUS_MOBS(21, "Looting", 2, 15, 9, 50, 3, EnchantmentTarget.WEAPON),
-        DIG_SPEED(32, "Efficiency", 10, 1, 10, 50, 5, EnchantmentTarget.TOOL, DIGGING_TOOLS),
-        SILK_TOUCH(33, "Silk Touch", 1, 15, 0, 50, 1, EnchantmentTarget.TOOL, DIGGING_TOOLS, GROUP_DIG),
-        DURABILITY(34, "Unbreaking", 5, 5, 8, 50, 3, EnchantmentTarget.TOOL, ALL_THINGS),
-        LOOT_BONUS_BLOCKS(35, "Fortune", 2, 15, 9, 50, 3, EnchantmentTarget.TOOL, BASE_TOOLS, GROUP_DIG),
-        ARROW_DAMAGE(48, "Power", 10, 1, 10, 15, 5, EnchantmentTarget.BOW),
-        ARROW_KNOCKBACK(49, "Punch", 2, 12, 20, 25, 2, EnchantmentTarget.BOW),
-        ARROW_FIRE(50, "Flame", 2, 20, 0, 30, 1, EnchantmentTarget.BOW),
-        ARROW_INFINITE(51, "Infinity", 1, 20, 0, 30, 1, EnchantmentTarget.BOW),
-        LUCK(61, "Luck of the Sea", 2, 15, 9, 50, 3, EnchantmentTarget.FISHING_ROD),
-        LURE(62, "Lure", 2, 15, 9, 50, 3, EnchantmentTarget.FISHING_ROD);
+        PROTECTION_ENVIRONMENTAL(0, "Protection", 4, 10, 1, 11, 20, EnchantmentTarget.ARMOR, GROUP_PROTECT),
+        PROTECTION_FIRE(1, "Fire Protection", 4, 5, 10, 8, 12, EnchantmentTarget.ARMOR, GROUP_PROTECT),
+        PROTECTION_FALL(2, "Feather Falling", 4, 5, 5, 6, 10, EnchantmentTarget.ARMOR_FEET, GROUP_PROTECT),
+        PROTECTION_EXPLOSIONS(3, "Blast Protection", 4, 2, 5, 8, 12, EnchantmentTarget.ARMOR),
+        PROTECTION_PROJECTILE(4, "Projectile Protection", 4, 5, 3, 6, 15, EnchantmentTarget.ARMOR, GROUP_PROTECT),
+        OXYGEN(5, "Respiration", 3, 2, 10, 10, 30, EnchantmentTarget.ARMOR_HEAD),
+        WATER_WORKER(6, "Aqua Affinity", 1, 2, 1, 0, 40, EnchantmentTarget.ARMOR_HEAD),
+        THORNS(7, "Thorns", 3, 1, 10, 20, 50, EnchantmentTarget.ARMOR_TORSO, new MatcherAdapter(EnchantmentTarget.ARMOR)),
+        DEPTH_STRIDER(8, "Depth Strider", 3, 2, 10, 10, 15, EnchantmentTarget.ARMOR_FEET),
+        DAMAGE_ALL(16, "Sharpness", 5, 10, 1, 11, 20, EnchantmentTarget.WEAPON, SWORD_OR_AXE, GROUP_ATTACK),
+        DAMAGE_UNDEAD(17, "Smite", 5, 5, 5, 8, 20, EnchantmentTarget.WEAPON, SWORD_OR_AXE, GROUP_ATTACK),
+        DAMAGE_ARTHROPODS(18, "Bane of Arthropods", 5, 5, 5, 8, 20, EnchantmentTarget.WEAPON, SWORD_OR_AXE, GROUP_ATTACK),
+        KNOCKBACK(19, "Knockback", 2, 5, 5, 20, 50, EnchantmentTarget.WEAPON),
+        FIRE_ASPECT(20, "Fire Aspect", 2, 2, 10, 20, 50, EnchantmentTarget.WEAPON),
+        LOOT_BONUS_MOBS(21, "Looting", 3, 2, 15, 9, 50, EnchantmentTarget.WEAPON),
+        DIG_SPEED(32, "Efficiency", 5, 10, 1, 10, 50, EnchantmentTarget.TOOL, DIGGING_TOOLS),
+        SILK_TOUCH(33, "Silk Touch", 1, 1, 15, 0, 50, EnchantmentTarget.TOOL, DIGGING_TOOLS, GROUP_DIG),
+        DURABILITY(34, "Unbreaking", 3, 5, 5, 8, 50, EnchantmentTarget.TOOL, ALL_THINGS),
+        LOOT_BONUS_BLOCKS(35, "Fortune", 3, 2, 15, 9, 50, EnchantmentTarget.TOOL, BASE_TOOLS, GROUP_DIG),
+        ARROW_DAMAGE(48, "Power", 5, 10, 1, 10, 15, EnchantmentTarget.BOW),
+        ARROW_KNOCKBACK(49, "Punch", 2, 2, 12, 20, 25, EnchantmentTarget.BOW),
+        ARROW_FIRE(50, "Flame", 1, 2, 20, 0, 30, EnchantmentTarget.BOW),
+        ARROW_INFINITE(51, "Infinity", 1, 1, 20, 0, 30, EnchantmentTarget.BOW),
+        LUCK(61, "Luck of the Sea", 3, 2, 15, 9, 50, EnchantmentTarget.FISHING_ROD),
+        LURE(62, "Lure", 3, 2, 15, 9, 50, EnchantmentTarget.FISHING_ROD);
 
         private final int id;
         private final String name;
@@ -168,7 +168,7 @@ public final class GlowEnchantment extends Enchantment implements WeightedRandom
             this(id, name, max, weight, minValue, minInc, maxInc, target, new MatcherAdapter(target), GROUP_NONE);
         }
 
-        Impl(int id, String name, int max, int weight,int minValue, int minInc, int maxInc, EnchantmentTarget target, int group) {
+        Impl(int id, String name, int max, int weight, int minValue, int minInc, int maxInc, EnchantmentTarget target, int group) {
             this(id, name, max, weight, minValue, minInc, maxInc, target, new MatcherAdapter(target), group);
         }
 
