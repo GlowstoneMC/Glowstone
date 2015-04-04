b7e145207aadff7bad664fc37913e0bd2964d31e
deathcap <deathcap@gmx.co.uk>
2015-04-04 12:53:46 -0700

    Stub LZMA stream, hopefully temporary

---
+++ src/main/java/LZMA/LzmaInputStream.java
@@ -0,0 +1,16 @@
+package LZMA;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LzmaInputStream extends InputStream { // TODO: what is the mvn dependency for this package?
+
+    public LzmaInputStream(InputStream inputStream) {
+
+    }
+
+    @Override
+    public int read() throws IOException {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
