0822eaa13cfce4e53acd4303ab83584d1cd714d9
deathcap <deathcap@gmx.co.uk>
2015-04-08 22:53:36 -0700

    Correct the particle enums and IDs

--- src/main/java/net/glowstone/constants/GlowParticle.java
+++ src/main/java/net/glowstone/constants/GlowParticle.java
@@ -78,30 +78,30 @@ public final class GlowParticle {
 
     static {
         Arrays.fill(ids, -1);
-        // http://wiki.vg/Protocol#Effect Particle IDs, but keyed by API enum
+        // http://wiki.vg/Protocol#Particle IDs, but keyed by API enum
         set(Effect.EXPLOSION, 0);       // explode
-        set(Effect.EXPLOSION_LARGE, 1); // largeexplosion
+        set(Effect.EXPLOSION_LARGE, 1); // largeexplode
         set(Effect.EXPLOSION_HUGE, 2);  // hugeexplosion
         set(Effect.FIREWORKS_SPARK, 3); // fireworksSpark
         set(Effect.BUBBLE, 4); // bubble
-        set(Effect.WAKE, 5); // wake
-        set(Effect.SPLASH, 6); // splash
+        set(Effect.SPLASH, 5); // splash
+        set(Effect.WAKE, 6); // wake
         set(Effect.SUSPENDED, 7); // suspended
-        set(Effect.SMALL_SMOKE, 8); // townaura
+        set(Effect.VOID_FOG, 8); // depthsuspend
         set(Effect.CRIT, 9); // crit
         set(Effect.MAGIC_CRIT, 10); // magicCrit
         set(Effect.PARTICLE_SMOKE, 11); // smoke
         set(Effect.LARGE_SMOKE, 12); // largesmoke
-        set(Effect.POTION_SWIRL, 13); // mobSpell?
+        set(Effect.POTION_SWIRL, 13); // spell
         set(Effect.INSTANT_SPELL, 14); // instantSpell
         set(Effect.SPELL, 15); // spell
-        //set(Effect., 16); // ?
+        set(Effect.POTION_SWIRL_TRANSPARENT, 16); // mobSpellAmbient
         set(Effect.WITCH_MAGIC, 17); // witchMagic
         set(Effect.WATERDRIP, 18); // dripWater
         set(Effect.LAVADRIP, 19); // dripLava
         set(Effect.VILLAGER_THUNDERCLOUD, 20); // angryVillager
         set(Effect.HAPPY_VILLAGER, 21); // happyVillager
-        set(Effect.VOID_FOG, 22); // depthsuspend
+        set(Effect.SMALL_SMOKE, 22); // townaura
         set(Effect.NOTE, 23); // note
         set(Effect.PORTAL, 24); // portal
         set(Effect.FLYING_GLYPH, 25); // enchantmenttable
@@ -115,12 +115,11 @@ public final class GlowParticle {
         set(Effect.SLIME, 33); // slime
         set(Effect.HEART, 34); // heart
         set(Effect.BARRIER, 35); // barrier
-        // below missing from wiki.vg
-        set(Effect.ITEM_BREAK, 36); // iconcrack
-        set(Effect.TILE_BREAK, 37); // blockcrack
-        set(Effect.TILE_DUST, 38); // blockdust
-        set(Effect.WATER_DROPLET, 39); // ?
-        set(Effect.ITEM_TAKE, 40); // ?
-        set(Effect.MOB_APPEARANCE, 41); // ?
+        set(Effect.ITEM_BREAK, 36); // iconcrack_(id)_(data)
+        set(Effect.TILE_BREAK, 37); // blockcrack_(id+(data<<12))
+        set(Effect.TILE_DUST, 38); // blockdust_(id)
+        set(Effect.WATER_DROPLET, 39); // droplet
+        set(Effect.ITEM_TAKE, 40); // take
+        set(Effect.MOB_APPEARANCE, 41); // mobappearance
     }
 }
