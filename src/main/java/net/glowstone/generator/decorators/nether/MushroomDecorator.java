5520947b33c11e331b66212fad237ae9180a7a8d
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2015-04-12 13:09:27 +0200

    Add brown and red mushrooms population to Nether

---
+++ src/main/java/net/glowstone/generator/decorators/nether/MushroomDecorator.java
@@ -0,0 +1,49 @@
+package net.glowstone.generator.decorators.nether;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import net.glowstone.generator.decorators.BlockDecorator;
+
+import org.bukkit.Chunk;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.material.MaterialData;
+
+public class MushroomDecorator extends BlockDecorator {
+
+    private static final Material[] MATERIALS = {Material.NETHERRACK, Material.QUARTZ_ORE, Material.SOUL_SAND, Material.GRAVEL};
+
+    private final Material type;
+
+    public MushroomDecorator(Material type) {
+        if (type != Material.BROWN_MUSHROOM && type != Material.RED_MUSHROOM) {
+            throw new IllegalArgumentException("MushroomDecorator material must be BROWN_MUSHROOM or RED_MUSHROOM");
+        }
+        this.type = type;
+    }
+
+    @Override
+    public void decorate(World world, Random random, Chunk source) {
+        int sourceX = (source.getX() << 4) + random.nextInt(16);
+        int sourceZ = (source.getZ() << 4) + random.nextInt(16);
+        int sourceY = random.nextInt(128);
+
+        for (int i = 0; i < 64; i++) {
+            int x = sourceX + random.nextInt(8) - random.nextInt(8);
+            int z = sourceZ + random.nextInt(8) - random.nextInt(8);
+            int y = sourceY + random.nextInt(4) - random.nextInt(4);
+
+            final Block block = world.getBlockAt(x, y, z);
+            final Block blockBelow = world.getBlockAt(x, y - 1, z);
+            if (y < 128 && block.getType() == Material.AIR && Arrays.asList(MATERIALS).contains(blockBelow.getType())) {
+                final BlockState state = block.getState();
+                state.setType(type);
+                state.setData(new MaterialData(type));
+                state.update(true);
+            }
+        }
+    }
+}
--- src/main/java/net/glowstone/generator/populators/NetherPopulator.java
+++ src/main/java/net/glowstone/generator/populators/NetherPopulator.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import net.glowstone.generator.decorators.nether.MushroomDecorator;
 import net.glowstone.generator.populators.nether.OrePopulator;
 
 import org.bukkit.Chunk;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.generator.BlockPopulator;
 
@@ -16,9 +18,17 @@ public class NetherPopulator extends BlockPopulator {
     private final List<BlockPopulator> onGroundPopulators = new ArrayList<>();
 
     protected final OrePopulator orePopulator = new OrePopulator();
+    protected final MushroomDecorator brownMushroomDecorator = new MushroomDecorator(Material.BROWN_MUSHROOM);
+    protected final MushroomDecorator redMushroomDecorator = new MushroomDecorator(Material.RED_MUSHROOM);
 
     public NetherPopulator() {
         inGroundPopulators.add(orePopulator);
+
+        onGroundPopulators.add(brownMushroomDecorator);
+        onGroundPopulators.add(redMushroomDecorator);
+
+        brownMushroomDecorator.setAmount(1);
+        redMushroomDecorator.setAmount(1);
     }
 
     @Override
