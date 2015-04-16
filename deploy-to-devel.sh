06be4b8a4c5e6d5d80f9dbdfa78438d5491c9ed8
mastercoms <circuitsoft@openmailbox.org>
2015-04-16 07:16:23 -0400

    do it in circle.yml

--- deploy-to-devel.sh
+++
@@ -1,1 +0,0 @@
-ssh slog@therealms.us -p 2299 'aria2c https://circle-artifacts.com/gh/deathcap/GlowstonePlusPlus/$CIRCLE_BUILD_NUM/artifacts/0/tmp/circle-artifacts.cErmidh/glowstone++-1.8.3-SNAPSHOT.jar'
