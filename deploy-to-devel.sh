b67129046db19da046134c898f5ee594d21f19ae
mastercoms <circuitsoft@openmailbox.org>
2015-04-16 07:07:43 -0400

    fix download problems

--- deploy-to-devel.sh
+++ deploy-to-devel.sh
@@ -1,2 +1,1 @@
-ssh slog@therealms.us -p 2299
-aria2c https://circle-artifacts.com/gh/deathcap/GlowstonePlusPlus/$CIRCLE_BUILD_NUM/artifacts/0/tmp/circle-artifacts.cErmidh/glowstone++-1.8.3-SNAPSHOT.jar
+ssh slog@therealms.us -p 2299 'aria2c https://circle-artifacts.com/gh/deathcap/GlowstonePlusPlus/$CIRCLE_BUILD_NUM/artifacts/0/tmp/circle-artifacts.cErmidh/glowstone++-1.8.3-SNAPSHOT.jar'
