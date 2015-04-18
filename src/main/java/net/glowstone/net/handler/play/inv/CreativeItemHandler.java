ccee51a080e27b1cbbde96fd7f3b76112e8dcfe9
mastercoms <circuitsoft@openmailbox.org>
2015-04-18 12:27:59 -0400

    also send to the client

--- src/main/java/net/glowstone/net/handler/play/inv/CreativeItemHandler.java
+++ src/main/java/net/glowstone/net/handler/play/inv/CreativeItemHandler.java
@@ -69,6 +69,7 @@ public final class CreativeItemHandler implements MessageHandler<GlowSession, Cr
             return;
         }
 
+        view.setItem(viewSlot, event.getCursor());
         player.getOpenInventory().setItem(message.getSlot(), message.getItem());
     }
 }
