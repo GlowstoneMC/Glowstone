c88b0697563899b11437006dc8f24849b62a10f6
mastercoms <circuitsoft@openmailbox.org>
2016-01-03 19:16:33 -0500

    Spawn egg creaturespawnevent

--- src/main/java/net/glowstone/block/itemtype/ItemSpawn.java
+++ src/main/java/net/glowstone/block/itemtype/ItemSpawn.java
@@ -7,11 +7,15 @@ import net.glowstone.entity.GlowEntity;
 import net.glowstone.entity.GlowPlayer;
 import org.bukkit.Location;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
 
 public class ItemSpawn extends ItemType {
     @Override
@@ -21,16 +25,19 @@ public class ItemSpawn extends ItemType {
 
         Class<? extends GlowEntity> spawn = EntityRegistry.getEntity(holding.getDurability());
         try {
-
             Constructor<? extends GlowEntity> constructor = spawn.getConstructor(Location.class);
             entity = constructor.newInstance(target.getLocation());
-            entity.createSpawnMessage();
-
+            CreatureSpawnEvent spawnEvent = new CreatureSpawnEvent((LivingEntity) entity, SpawnReason.SPAWNER_EGG);
+            if (!spawnEvent.isCancelled()) {
+                entity.createSpawnMessage();
+            } else {
+                // TODO: separate spawning and construction for better event cancellation
+                entity.remove();
+            }
         } catch (NoSuchMethodException e) {
-            GlowServer.logger.warning("Invalid entity spawn");
-            e.printStackTrace();
+            GlowServer.logger.log(Level.WARNING, "Invalid entity spawn: ", e);
         } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
-            e.printStackTrace();
+            GlowServer.logger.log(Level.SEVERE, "Unable to spawn entity: ", e);
         }
 
 
