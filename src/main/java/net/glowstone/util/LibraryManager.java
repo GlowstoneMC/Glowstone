fc4fe14452eb8622cc1015264902081093075a38
mastercoms <circuitsoft@tuta.io>
2016-01-24 09:16:37 -0500

    Optimize libraries

--- src/main/java/net/glowstone/util/LibraryManager.java
+++ src/main/java/net/glowstone/util/LibraryManager.java
@@ -41,6 +41,7 @@ public final class LibraryManager {
 
         download("org.xerial", "sqlite-jdbc", "3.7.2");
         download("mysql", "mysql-connector-java", "5.1.34"); // was 5.1.14
+        download("org.slf4j", "slf4j-jdk14", "1.7.7");
     }
 
     private void download(String group, String library, String version) {
