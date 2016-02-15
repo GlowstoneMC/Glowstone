9f3dc17ae83845917bb5106b52fee775ce220200
mastercoms <circuitsoft@tuta.io>
2016-02-15 14:55:06 -0500

    Fix some movement issues
    Fixes #198 by checking if plugins changed a player's position outside of the event
    Better lag/hack prevention (consistent with vanilla) by making a warn check 10 blocks instead of 16,
    and a hack kick 100 blocks
    No longer allows players to go to illegal positions (very far corners of the world)

--- src/main/java/net/glowstone/net/handler/play/player/PlayerUpdateHandler.java
+++ src/main/java/net/glowstone/net/handler/play/player/PlayerUpdateHandler.java
@@ -2,6 +2,7 @@ package net.glowstone.net.handler.play.player;
 
 import com.flowpowered.networking.MessageHandler;
 import net.glowstone.EventFactory;
+import net.glowstone.GlowServer;
 import net.glowstone.entity.GlowPlayer;
 import net.glowstone.net.GlowSession;
 import net.glowstone.net.message.play.game.PositionRotationMessage;
@@ -9,6 +10,8 @@ import net.glowstone.net.message.play.player.PlayerUpdateMessage;
 import org.bukkit.Location;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+
+import java.util.Objects;
 
 public final class PlayerUpdateHandler implements MessageHandler<GlowSession, PlayerUpdateMessage> {
 
@@ -21,11 +24,24 @@ public final class PlayerUpdateHandler implements MessageHandler<GlowSession, Pl
         Location newLocation = oldLocation.clone();
         message.update(newLocation);
 
-        // don't let players move more than 16 blocks in a single packet.
-        // this is NOT robust hack prevention - only to prevent client
-        // confusion about where its actual location is (e.g. during login)
-        if (newLocation.distanceSquared(oldLocation) > 16 * 16) {
+        // don't let players reach an illegal position
+        if (Math.abs(newLocation.getBlockX()) > 32000000 || Math.abs(newLocation.getBlockZ()) > 32000000) {
+            session.getPlayer().kickPlayer("Illegal position");
             return;
+        }
+
+        /*
+          don't let players move more than 100 blocks in a single packet
+          if they move greater than 10 blocks, but less than 100, just warn
+          this is NOT robust hack prevention - only to prevent client
+          confusion about where its actual location is (e.g. during login)
+        */
+        if (newLocation.distanceSquared(oldLocation) > 100 * 100) {
+            GlowServer.logger.warning(session.getPlayer().getName() + " moved too quickly! (hacking)");
+            session.getPlayer().kickPlayer("You moved too quickly :( (Hacking?)");
+            return;
+        } else if (newLocation.distanceSquared(oldLocation) > 100) {
+            GlowServer.logger.warning(session.getPlayer().getName() + " moved too quickly!");
         }
 
 
@@ -42,6 +58,11 @@ public final class PlayerUpdateHandler implements MessageHandler<GlowSession, Pl
                 // teleport to the set destination: fires PlayerTeleportEvent and
                 // handles if the destination is in another world
                 player.teleport(event.getTo(), PlayerTeleportEvent.TeleportCause.PLUGIN);
+                return;
+            }
+
+            if (!Objects.equals(session.getPlayer().getLocation(), oldLocation)) {
+                // plugin changed location on move event
                 return;
             }
         }
