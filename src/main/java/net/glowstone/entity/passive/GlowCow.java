e24f061eb4a2afa1069bb6a81a43580527120a31
LukBukkit <luk.bukkit@gmail.com>
2016-02-20 11:19:56 +0100

    Cow gives now milk

--- src/main/java/net/glowstone/entity/passive/GlowCow.java
+++ src/main/java/net/glowstone/entity/passive/GlowCow.java
@@ -1,14 +1,43 @@
 package net.glowstone.entity.passive;
 
 import net.glowstone.entity.GlowAnimal;
+import net.glowstone.entity.GlowPlayer;
+import net.glowstone.entity.objects.GlowItem;
+import net.glowstone.net.message.play.player.InteractEntityMessage;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Cow;
 import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.ItemStack;
 
 public class GlowCow extends GlowAnimal implements Cow {
 
     public GlowCow(Location location) {
         super(location, EntityType.COW, 10);
         setSize(0.9F, 1.3F);
+    }
+
+    @Override
+    public boolean entityInteract(GlowPlayer player, InteractEntityMessage message) {
+        if (player.getGameMode().equals(GameMode.CREATIVE) || player.getGameMode().equals(GameMode.SPECTATOR))
+            return false;
+        if (!player.getItemInHand().getType().equals(Material.BUCKET)) return false;
+
+        if (player.getItemInHand().getAmount() > 1) {
+            player.getItemInHand().setAmount(player.getItemInHand().getAmount() - 1);
+        } else {
+            player.getItemInHand().setType(Material.AIR);
+        }
+
+        if (player.getInventory().firstEmpty() == -1) {
+            Location playerheadloc = player.getLocation().clone().add(0, 1, 0);
+            GlowItem item = player.getWorld().dropItem(playerheadloc, new ItemStack(Material.MILK_BUCKET, 1));
+            item.setVelocity(playerheadloc.toVector().clone().subtract(this.getLocation().toVector().clone()));
+        } else {
+            player.getInventory().addItem(new ItemStack(Material.MILK_BUCKET, 1));
+        }
+
+        return true;
     }
 }
