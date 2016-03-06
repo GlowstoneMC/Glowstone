2f10b86b894871321c3fca2480ea5c7b64aac10f
Momo <momothereal.mc@gmail.com>
2016-03-06 18:16:24 -0500

    Add Baby spawning with spawn eggs. - GlowCow, GlowPig and GlowSheep need to get their super#entityInteract method called in order to have babies.

--- src/main/java/net/glowstone/entity/GlowAnimal.java
+++ src/main/java/net/glowstone/entity/GlowAnimal.java
@@ -1,8 +1,13 @@
 package net.glowstone.entity;
 
+import net.glowstone.net.message.play.player.InteractEntityMessage;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.EntityType;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SpawnMeta;
 
 /**
  * Represents an Animal, such as a Cow
@@ -18,5 +23,28 @@ public class GlowAnimal extends GlowAgeable implements Animals {
      */
     public GlowAnimal(Location location, EntityType type, double maxHealth) {
         super(location, type, maxHealth);
+    }
+
+    @Override
+    public boolean entityInteract(GlowPlayer player, InteractEntityMessage message) {
+        super.entityInteract(player, message);
+        ItemStack item = player.getItemInHand();
+
+        if (item != null && item.getType() == Material.MONSTER_EGG && item.hasItemMeta()) {
+            SpawnMeta meta = (SpawnMeta) item.getItemMeta();
+            if (meta.hasEntityType() && meta.getEntityType() == this.getType()) {
+                Class<? extends GlowEntity> spawn = EntityRegistry.getEntity(getType().getTypeId());
+                GlowAnimal animal = (GlowAnimal) getWorld().spawn(getLocation(), spawn, CreatureSpawnEvent.SpawnReason.SPAWNER_EGG);
+                animal.setBaby();
+
+                // Consume the egg
+                item.setAmount(item.getAmount() - 1);
+                if (item.getAmount() < 1)
+                    player.getInventory().remove(item);
+                return true;
+            }
+        }
+
+        return false;
     }
 }
