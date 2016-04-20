dec3c259c192f927c0c1aadd92e22c6176296fec
Momo <momo@momoperes.ca>
2016-04-20 18:10:52 -0400

    Removed unused import.

--- src/main/java/net/glowstone/net/handler/play/player/PlayerActionHandler.java
+++ src/main/java/net/glowstone/net/handler/play/player/PlayerActionHandler.java
@@ -1,7 +1,6 @@
 package net.glowstone.net.handler.play.player;
 
 import com.flowpowered.network.MessageHandler;
-import net.glowstone.GlowServer;
 import net.glowstone.entity.GlowPlayer;
 import net.glowstone.net.GlowSession;
 import net.glowstone.net.message.play.player.PlayerActionMessage;
