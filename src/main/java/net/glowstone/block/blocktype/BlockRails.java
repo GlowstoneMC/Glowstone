0e948469517c6bba68774a75d3f10369189f1b0c
mastercoms <circuitsoft@tuta.io>
2016-08-16 21:40:35 -0400

    Fixes #292 (rail placement), remove yaw debug

--- src/main/java/net/glowstone/block/blocktype/BlockRails.java
+++ src/main/java/net/glowstone/block/blocktype/BlockRails.java
@@ -1,7 +1,13 @@
 package net.glowstone.block.blocktype;
 
 import net.glowstone.block.GlowBlock;
+import net.glowstone.block.GlowBlockState;
+import net.glowstone.entity.GlowPlayer;
+import org.bukkit.block.BlockFace;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
+import org.bukkit.material.Rails;
+import org.bukkit.util.Vector;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -10,5 +16,37 @@ public class BlockRails extends BlockNeedsAttached {
     @Override
     public Collection<ItemStack> getDrops(GlowBlock block, ItemStack tool) {
         return Arrays.asList(new ItemStack(block.getType()));
+    }
+
+    private static BlockFace blockFaceFromYaw(float yaw) {
+        // nb: opposite from getOppositeBlockFace in BlockType
+        yaw = yaw % 360;
+        if (yaw < 0) {
+            yaw += 360.0;
+        }
+        if (yaw < 45 || yaw >= 315) {
+            return BlockFace.NORTH;
+        } else if (45 <= yaw && yaw < 135) {
+            return BlockFace.EAST;
+        } else if (135 <= yaw && yaw < 225) {
+            return BlockFace.SOUTH;
+        } else {
+            return BlockFace.WEST;
+        }
+    }
+
+    @Override
+    public void placeBlock(GlowPlayer player, GlowBlockState state, BlockFace face, ItemStack holding, Vector clickedLoc) {
+        super.placeBlock(player, state, face, holding, clickedLoc);
+
+        MaterialData materialData = state.getData();
+        if (materialData instanceof Rails) {
+            Rails rails = (Rails) materialData;
+            float yaw = player.getLocation().getYaw();
+            rails.setDirection(blockFaceFromYaw(yaw), false);
+            state.update(true);
+        } else {
+            warnMaterialData(Rails.class, materialData);
+        }
     }
 }
