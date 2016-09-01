08f97bd2421faf3617a0d3ed45e30c943205dd14
Franco <francochen2000@hotmail.com>
2016-08-31 22:11:50 -0400

    Remove monster egg block drops

--- src/main/java/net/glowstone/block/blocktype/BlockMonsterEgg.java
+++ src/main/java/net/glowstone/block/blocktype/BlockMonsterEgg.java
@@ -1,16 +1,14 @@
 package net.glowstone.block.blocktype;
 
-import net.glowstone.GlowServer;
 import net.glowstone.block.GlowBlock;
 import net.glowstone.entity.GlowPlayer;
 import net.glowstone.entity.monster.GlowSilverfish;
 import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class BlockMonsterEgg extends BlockType {
@@ -24,30 +22,7 @@ public class BlockMonsterEgg extends BlockType {
 
     @Override
     public Collection<ItemStack> getDrops(GlowBlock block, ItemStack tool) {
-        ItemStack stack;
-
-        switch (block.getData()) {
-            case 0: //stone
-            case 1: //cobblestone
-                stack = new ItemStack(Material.COBBLESTONE);
-                break;
-            case 2: //stone bricks
-                stack = new ItemStack(Material.SMOOTH_BRICK);
-                break;
-            case 3: //mossy stone bricks
-                stack = new ItemStack(Material.SMOOTH_BRICK, 2);
-                break;
-            case 4: //cracked stone bricks
-                stack = new ItemStack(Material.SMOOTH_BRICK, 1);
-                break;
-            case 5: //chiseled stone bricks
-                stack = new ItemStack(Material.SMOOTH_BRICK, 3);
-                break;
-            default:
-                GlowServer.logger.warning("Wrong data for block monster_egg: " + block.getData());
-                stack = new ItemStack(Material.STONE);
-        }
-
-        return Arrays.asList(stack);
+        // monster egg blocks do not drop blocks
+        return new ArrayList<>();
     }
 }
