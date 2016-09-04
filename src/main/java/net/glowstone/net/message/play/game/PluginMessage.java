a848f1c95c94b94fece6dc14a110368e90aefdb2
DaMatrix <joey.rabil@gmail.com>
2016-09-04 17:16:24 +0200

    Messed up formatting again

--- src/main/java/net/glowstone/net/message/play/game/PluginMessage.java
+++ src/main/java/net/glowstone/net/message/play/game/PluginMessage.java
@@ -21,8 +21,7 @@ public final class PluginMessage implements Message {
         try {
             ByteBufUtils.writeUTF8(buf, text);
         } catch (IOException e) {
-            GlowServer.logger.log(Level.WARNING,
-                    "Error converting to PluginMessage: \"" + channel + "\", \"" + text + "\"", e);
+            GlowServer.logger.log(Level.WARNING, "Error converting to PluginMessage: \"" + channel + "\", \"" + text + "\"", e);
         }
         byte[] array = buf.array();
         buf.release();
