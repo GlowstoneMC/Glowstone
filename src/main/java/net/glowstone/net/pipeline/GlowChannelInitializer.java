dd313faa4c64a83b4f86496305a2b5a99a3ec15e
mastercoms <circuitsoft@tuta.io>
2016-10-16 12:10:52 -0400

    Some optimizations to channels

--- src/main/java/net/glowstone/net/pipeline/GlowChannelInitializer.java
+++ src/main/java/net/glowstone/net/pipeline/GlowChannelInitializer.java
@@ -1,9 +1,13 @@
 package net.glowstone.net.pipeline;
 
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.ChannelException;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.timeout.IdleStateHandler;
 import io.netty.handler.timeout.ReadTimeoutHandler;
+import net.glowstone.GlowServer;
 import net.glowstone.net.GlowNetworkServer;
 import net.glowstone.net.handler.legacyping.LegacyPingHandler;
 import net.glowstone.net.protocol.ProtocolType;
@@ -36,6 +40,14 @@ public final class GlowChannelInitializer extends ChannelInitializer<SocketChann
         MessageHandler handler = new MessageHandler(connectionManager);
         CodecsHandler codecs = new CodecsHandler(ProtocolType.HANDSHAKE.getProtocol());
         FramingHandler framing = new FramingHandler();
+
+        try {
+            c.config().setOption(ChannelOption.IP_TOS, 0x18);
+        } catch (ChannelException e) {
+            // Not supported on all OSs, like Windows XP and lesser
+            GlowServer.logger.warning("Your OS does not support type of service.");
+        }
+        c.config().setAllocator(PooledByteBufAllocator.DEFAULT);
 
         c.pipeline()
                 .addLast("legacy_ping", new LegacyPingHandler(connectionManager))
