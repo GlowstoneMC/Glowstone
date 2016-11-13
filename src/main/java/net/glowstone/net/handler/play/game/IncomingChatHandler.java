3ad2a70c2b40136934fda1ef88de063270ba0e8c
momothereal <momo@momoperes.ca>
2016-11-12 21:39:41 -0500

    Deny an incoming message if the player hasn't had the login procedure completed

--- src/main/java/net/glowstone/net/handler/play/game/IncomingChatHandler.java
+++ src/main/java/net/glowstone/net/handler/play/game/IncomingChatHandler.java
@@ -8,6 +8,10 @@ public final class IncomingChatHandler implements MessageHandler<GlowSession, In
 
     @Override
     public void handle(GlowSession session, IncomingChatMessage message) {
+        if (!session.isOnline()) {
+            // deny the message if the player hasn't had the login procedure completed
+            return;
+        }
         if (!message.getText().isEmpty()) {
             session.getPlayer().chat(message.getText(), message.isAsync());
         }
