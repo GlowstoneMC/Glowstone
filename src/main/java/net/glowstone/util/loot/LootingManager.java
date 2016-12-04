c933956fd9be9b08cc65c5db74eb939041ae58d4
momothereal <momothereal.mc@gmail.com>
2016-12-03 20:01:04 -0500

    1.11 entities

--- src/main/java/net/glowstone/util/loot/LootingManager.java
+++ src/main/java/net/glowstone/util/loot/LootingManager.java
@@ -27,15 +27,21 @@ public class LootingManager {
         register(EntityType.CHICKEN, baseDir + "chicken.json");
         register(EntityType.COW, baseDir + "cow.json");
         register(EntityType.CREEPER, baseDir + "creeper.json");
+        register(EntityType.DONKEY, baseDir + "horse.json");
+        register(EntityType.ELDER_GUARDIAN, baseDir + "elder_guardian.json");
         register(EntityType.ENDER_DRAGON, baseDir + "ender_dragon.json");
         register(EntityType.ENDERMAN, baseDir + "enderman.json");
         register(EntityType.ENDERMITE, baseDir + "endermite.json");
+        register(EntityType.EVOKER, baseDir + "evocation_illager.json");
         register(EntityType.GHAST, baseDir + "ghast.json");
         register(EntityType.GUARDIAN, baseDir + "guardian.json");
         register(EntityType.HORSE, baseDir + "horse.json");
+        register(EntityType.HUSK, baseDir + "zombie.json");
         register(EntityType.IRON_GOLEM, baseDir + "iron_golem.json");
         register(EntityType.MAGMA_CUBE, baseDir + "magma_cube.json");
+        register(EntityType.MULE, baseDir + "horse.json");
         register(EntityType.MUSHROOM_COW, baseDir + "mushroom_cow.json");
+        register(EntityType.LLAMA, baseDir + "llama.json");
         register(EntityType.OCELOT, baseDir + "ocelot.json");
         register(EntityType.PIG, baseDir + "pig.json");
         register(EntityType.PIG_ZOMBIE, baseDir + "pig_zombie.json");
@@ -45,15 +51,20 @@ public class LootingManager {
         register(EntityType.SHULKER, baseDir + "shulker.json");
         register(EntityType.SILVERFISH, baseDir + "silverfish.json");
         register(EntityType.SKELETON, baseDir + "skeleton.json");
+        register(EntityType.SKELETON_HORSE, baseDir + "skeleton_horse.json");
         register(EntityType.SLIME, baseDir + "slime.json");
         register(EntityType.SNOWMAN, baseDir + "snowman.json");
         register(EntityType.SPIDER, baseDir + "spider.json");
         register(EntityType.SQUID, baseDir + "squid.json");
         register(EntityType.VILLAGER, baseDir + "villager.json");
+        register(EntityType.VINDICATOR, baseDir + "vindication_illager.json");
         register(EntityType.WITCH, baseDir + "witch.json");
         register(EntityType.WITHER, baseDir + "wither.json");
+        register(EntityType.WITHER_SKELETON, baseDir + "wither_skeleton.json");
         register(EntityType.WOLF, baseDir + "wolf.json");
         register(EntityType.ZOMBIE, baseDir + "zombie.json");
+        register(EntityType.ZOMBIE_HORSE, baseDir + "zombie_horse.json");
+        register(EntityType.ZOMBIE_VILLAGER, baseDir + "zombie.json");
     }
 
     private static void register(EntityType type, String location) throws Exception {
@@ -73,10 +84,10 @@ public class LootingManager {
     }
 
     public static LootData generate(GlowLivingEntity entity) {
-        Random random = LootingUtil.random;
         if (!entities.containsKey(entity.getType())) {
             return new LootData(new ItemStack[0], 0);
         }
+        Random random = LootingUtil.random;
         EntityLootTable table = entities.get(entity.getType());
         ArrayList<ItemStack> items = new ArrayList<>();
         for (LootItem lootItem : table.getItems()) {
@@ -117,8 +128,6 @@ public class LootingManager {
             Material material = Material.getMaterial(name);
             if (material != null && count > 0) {
                 items.add(new ItemStack(material, count, (byte) data));
-            } else {
-                GlowServer.logger.info("Not valid: " + material + "(" + name + ")" + "x" + count + " : " + data);
             }
         }
         int experience = table.getExperience().generate(random, entity);
