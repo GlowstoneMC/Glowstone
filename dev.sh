41338d21ac0eb7b62e86b7d584e49ed4183f1e9b
mastercoms <circuitsoft@tuta.io>
2016-12-23 09:00:03 -0500

    don't forget the dev script

--- dev.sh
+++ dev.sh
@@ -4,7 +4,7 @@ then
 fi
 if [ $? -eq 0 ];
 then
-  cp target/glowstone-1.11-SNAPSHOT.jar target/glowstone.jar
+  cp target/glowstone-1.11.2-SNAPSHOT.jar target/glowstone.jar
   (cd target && ../start.sh)
 else
   echo "Glowstone did not compile."
