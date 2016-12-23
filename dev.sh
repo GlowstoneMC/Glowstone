cea0a39530654156b737090b50977603dbf94c29
mastercoms <circuitsoft@tuta.io>
2016-12-23 12:10:22 -0500

    Make version Minecraft-independent

--- dev.sh
+++ dev.sh
@@ -4,7 +4,7 @@ then
 fi
 if [ $? -eq 0 ];
 then
-  cp target/glowstone-1.11.2-SNAPSHOT.jar target/glowstone.jar
+  cp target/glowstone-2016.0.1.0-SNAPSHOT.jar target/glowstone.jar
   (cd target && ../start.sh)
 else
   echo "Glowstone did not compile."
