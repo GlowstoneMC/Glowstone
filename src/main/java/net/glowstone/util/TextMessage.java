c2c0ded07e33b370686b589b5f053229add9b494
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-02-26 07:29:48 -0300

    Remove unused import to keep CheckStyles happy

--- src/main/java/net/glowstone/util/TextMessage.java
+++ src/main/java/net/glowstone/util/TextMessage.java
@@ -2,7 +2,6 @@ package net.glowstone.util;
 
 import lombok.EqualsAndHashCode;
 import org.bukkit.ChatColor;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
