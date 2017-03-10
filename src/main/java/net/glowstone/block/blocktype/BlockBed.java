2f07a0c526cd076a305a807c4ef691bf6f13d28d
mastercoms <circuitsoft@tuta.io>
2017-03-10 15:30:10 -0500

    remove unused import

--- src/main/java/net/glowstone/block/blocktype/BlockBed.java
+++ src/main/java/net/glowstone/block/blocktype/BlockBed.java
@@ -11,7 +11,6 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.Bed;
