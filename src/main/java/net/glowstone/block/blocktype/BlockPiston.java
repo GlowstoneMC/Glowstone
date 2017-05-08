fae0ea9bdf0fb6d1f4ebcd3b3ae5a41173b2cf9b
momothereal <momothereal.mc@gmail.com>
2017-05-08 18:27:44 -0400

    Pistons produce smoke when used
    We won't be implementing pistons until 1.13.

--- src/main/java/net/glowstone/block/blocktype/BlockPiston.java
+++ src/main/java/net/glowstone/block/blocktype/BlockPiston.java
@@ -1,6 +1,10 @@
 package net.glowstone.block.blocktype;
 
+import net.glowstone.block.GlowBlock;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.block.BlockFace;
 import org.bukkit.inventory.ItemStack;
 
 public class BlockPiston extends BlockDirectional {
@@ -28,5 +32,20 @@ public class BlockPiston extends BlockDirectional {
      */
     public boolean isSticky() {
         return sticky;
+    }
+
+    @Override
+    public void onRedstoneUpdate(GlowBlock block) {
+        BlockFace face = BlockDirectional.getFace(block.getData());
+        if (face == null) {
+            return;
+        }
+        if (block.isBlockPowered() || block.isBlockIndirectlyPowered()) {
+            // spawn some smoke because we won't be working on pistons until 1.13
+            block.getWorld().playSound(block.getLocation(), Sound.BLOCK_LAVA_EXTINGUISH, 0.3F, 1.0F);
+            for (int i = 0; i < 3; i++) {
+                block.getWorld().spawnParticle(Particle.SMOKE_LARGE, block.getLocation().clone().add(0.5, 0.8, 0.5), 0);
+            }
+        }
     }
 }
