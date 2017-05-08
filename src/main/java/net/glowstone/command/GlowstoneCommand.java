2bcf31fdb71d49c081b610cec8500c52e0fd9480
momothereal <momothereal.mc@gmail.com>
2017-05-08 19:52:19 -0400

    Introduce /glowstone command (tools, debugging).
    Still WIP

---
+++ src/main/java/net/glowstone/command/GlowstoneCommand.java
@@ -0,0 +1,106 @@
+package net.glowstone.command;
+
+import org.apache.commons.lang.Validate;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.util.StringUtil;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.*;
+
+public class GlowstoneCommand extends BukkitCommand {
+
+    private static final List<String> SUBCOMMANDS = Arrays.asList("about", "help", "property", "vm");
+
+    public GlowstoneCommand() {
+        super("glowstone", "A handful of Glowstone commands for debugging purposes", "/glowstone help", Arrays.asList("gs"));
+        setPermission("glowstone.debug");
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        if (!testPermission(sender)) {
+            return true;
+        }
+        if (args.length == 0 || (args.length > 0 && args[0].equalsIgnoreCase("about"))) {
+            // some info about this Glowstone server
+            sender.sendMessage("Information about this server:");
+            sender.sendMessage(" - " + ChatColor.GOLD + "Server brand: " + ChatColor.AQUA + Bukkit.getName() + ChatColor.RESET + ".");
+            sender.sendMessage(" - " + ChatColor.GOLD + "Server name: " + ChatColor.AQUA + Bukkit.getServerName() + ChatColor.RESET + ".");
+            sender.sendMessage(" - " + ChatColor.GOLD + "Glowstone version: " + ChatColor.AQUA + Bukkit.getVersion() + ChatColor.RESET + ".");
+            sender.sendMessage(" - " + ChatColor.GOLD + "API version: " + ChatColor.AQUA + Bukkit.getBukkitVersion() + ChatColor.RESET + ".");
+            sender.sendMessage(" - " + ChatColor.GOLD + "Players: " + ChatColor.AQUA + Bukkit.getOnlinePlayers().size() + ChatColor.RESET + ".");
+            sender.sendMessage(" - " + ChatColor.GOLD + "Worlds: " + ChatColor.AQUA + Bukkit.getWorlds().size() + ChatColor.RESET + ".");
+            sender.sendMessage(" - " + ChatColor.GOLD + "Plugins: " + ChatColor.AQUA + Bukkit.getPluginManager().getPlugins().length + ChatColor.RESET + ".");
+
+            // thread count
+            int threadCount = 0;
+            Set<Thread> threads = Thread.getAllStackTraces().keySet();
+            for (Thread t : threads) {
+                if (t.getThreadGroup() == Thread.currentThread().getThreadGroup()) {
+                    threadCount++;
+                }
+            }
+            sender.sendMessage(" - " + ChatColor.GOLD + "Threads: " + ChatColor.AQUA + threadCount + ChatColor.RESET + ".");
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("help")) {
+            // some help
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("property")) {
+            if (args.length == 1) {
+                // list all
+                System.getProperties().forEach((key, value) -> {
+                    sender.sendMessage("Property '" + ChatColor.AQUA + key + ChatColor.RESET + "' = \"" + ChatColor.GOLD + value + ChatColor.RESET + "\".");
+                });
+            } else {
+                // get a property
+                String key = args[1].toLowerCase();
+                String value = System.getProperty(key);
+                if (value == null) {
+                    sender.sendMessage(ChatColor.RED + "Unknown system property '" + key + "'.");
+                } else {
+                    sender.sendMessage("Property '" + ChatColor.AQUA + key + ChatColor.RESET + "' = \"" + ChatColor.GOLD + value + ChatColor.RESET + "\".");
+                }
+            }
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("vm")) {
+            RuntimeMXBean runtimeMxBean = ManagementFactory.getRuntimeMXBean();
+            List<String> arguments = runtimeMxBean.getInputArguments();
+            if (arguments.size() == 0) {
+                sender.sendMessage("There are no VM arguments.");
+            } else {
+                sender.sendMessage("Glowstone VM arguments (" + arguments.size() + "):");
+                for (String argument : arguments) {
+                    sender.sendMessage(" - '" + ChatColor.AQUA + argument + ChatColor.RESET + "'.");
+                }
+            }
+
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public List<String> tabComplete(CommandSender sender, String alias, String[] args) throws IllegalArgumentException {
+        Validate.notNull(sender, "Sender cannot be null");
+        Validate.notNull(args, "Arguments cannot be null");
+        Validate.notNull(alias, "Alias cannot be null");
+        if (args.length == 0) {
+            return Collections.emptyList();
+        }
+        if (args.length == 1) {
+            return (List) StringUtil.copyPartialMatches(args[0], SUBCOMMANDS, new ArrayList(SUBCOMMANDS.size()));
+        }
+        System.out.println(args.length + " / " + Arrays.toString(args));
+        if (args.length == 2 && args[0].equalsIgnoreCase("property")) {
+            return (List) StringUtil.copyPartialMatches(args[1], System.getProperties().stringPropertyNames(), new ArrayList(System.getProperties().stringPropertyNames().size()));
+        }
+        return Collections.emptyList();
+    }
+}
