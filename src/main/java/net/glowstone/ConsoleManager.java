04d990456684e7ec0e3d13664d5715c10e999bd8
mastercoms <circuitsoft@tuta.io>
2017-05-10 03:17:17 -0400

    rewrite consolemanager

--- src/main/java/net/glowstone/ConsoleManager.java
+++ src/main/java/net/glowstone/ConsoleManager.java
@@ -1,6 +1,7 @@
 package net.glowstone;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandException;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.conversations.Conversation;
 import org.bukkit.conversations.ConversationAbandonedEvent;
@@ -13,314 +14,128 @@ import org.bukkit.plugin.Plugin;
 import org.jline.reader.*;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
-import org.jline.utils.AttributedStringBuilder;
-import org.jline.utils.AttributedStyle;
 
-import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.logging.*;
-import java.util.logging.Formatter;
+import java.io.IOException;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
 
 /**
- * A meta-class to handle all logging and input-related console improvements.
- * Portions are heavily based on CraftBukkit.
+ * Handles all logging and input-related console improvements.
  */
 public final class ConsoleManager {
 
-    private static final Logger logger = Logger.getLogger("");
-    private static String CONSOLE_DATE = "HH:mm:ss";
-    private static String FILE_DATE = "yyyy/MM/dd HH:mm:ss";
+    private GlowServer server;
+    LineReader reader;
 
-    private static String CONSOLE_PROMPT = "g>";
-    private static String RIGHT_PROMPT = null;
-
-    protected final GlowServer server;
-    private final Map<ChatColor, String> replacements = new EnumMap<>(ChatColor.class);
-    private final ChatColor[] colors = ChatColor.values();
-
-    protected LineReader reader;
     private ConsoleCommandSender sender;
 
-    private boolean running = true;
+    boolean running;
+
+    private static final Map<ChatColor, String> replacements = new EnumMap<>(ChatColor.class);
+
+    static {
+        addReplacement(ChatColor.BLACK, "\u001B[0;30;22m");
+        addReplacement(ChatColor.DARK_BLUE, "\u001B[0;34;22m");
+        addReplacement(ChatColor.DARK_GREEN, "\u001B[0;32;22m");
+        addReplacement(ChatColor.DARK_AQUA, "\u001B[0;36;22m");
+        addReplacement(ChatColor.DARK_RED, "\u001B[0;31;22m");
+        addReplacement(ChatColor.DARK_PURPLE, "\u001B[0;35;22m");
+        addReplacement(ChatColor.GOLD, "\u001B[0;33;22m");
+        addReplacement(ChatColor.GRAY, "\u001B[0;37;22m");
+        addReplacement(ChatColor.DARK_GRAY, "\u001B[0;30;1m");
+        addReplacement(ChatColor.BLUE, "\u001B[0;34;1m");
+        addReplacement(ChatColor.GREEN, "\u001B[0;32;1m");
+        addReplacement(ChatColor.AQUA, "\u001B[0;36;1m");
+        addReplacement(ChatColor.RED, "\u001B[0;31;1m");
+        addReplacement(ChatColor.LIGHT_PURPLE, "\u001B[0;35;1m");
+        addReplacement(ChatColor.YELLOW, "\u001B[0;33;1m");
+        addReplacement(ChatColor.WHITE, "\u001B[0;37;1m");
+        addReplacement(ChatColor.MAGIC, "\u001B[5m");
+        addReplacement(ChatColor.BOLD, "\u001B[21m");
+        addReplacement(ChatColor.STRIKETHROUGH, "\u001B[9m");
+        addReplacement(ChatColor.UNDERLINE, "\u001B[4m");
+        addReplacement(ChatColor.ITALIC, "\u001B[3m");
+        addReplacement(ChatColor.RESET, "\u001B[39;0m");
+    }
+
+    private static void addReplacement(ChatColor formatting, String ansi) {
+        replacements.put(formatting, ansi);
+    }
 
     public ConsoleManager(GlowServer server) {
         this.server = server;
 
-        // terminal must be initialized before standard streams are changed
-
-        for (Handler h : logger.getHandlers()) {
-            logger.removeHandler(h);
-        }
-
-        // add log handler which writes to console
-        logger.addHandler(new FancyConsoleHandler());
-
         try (Terminal terminal = TerminalBuilder.builder()
-                    .name("Glowstone")
                     .system(true)
-                    .nativeSignals(true)
-                    .signalHandler(Terminal.SignalHandler.SIG_IGN)
+                    .name("Glowstone")
                     .build()) {
             reader = LineReaderBuilder.builder()
+                    .appName("Glowstone")
                     .terminal(terminal)
                     .completer(new CommandCompleter())
                     .build();
+            reader.unsetOpt(LineReader.Option.INSERT_TAB);
         } catch (IOException e) {
-            logger.log(Level.SEVERE, "Exception initializing terminal", e);
+            e.printStackTrace();
         }
+    }
 
-        System.setOut(new PrintStream(new LoggerOutputStream(Level.INFO), true));
-        System.setErr(new PrintStream(new LoggerOutputStream(Level.WARNING), true));
+    public void start() {
+        sender = new ColoredCommandSender();
+        if (!running) {
+            running = true;
+            new ConsoleCommandThread().start();
+        }
+    }
 
-        CONSOLE_PROMPT = new AttributedStringBuilder()
-                .style(AttributedStyle.BOLD.foreground(AttributedStyle.YELLOW))
-                .append("g>")
-                .style(AttributedStyle.DEFAULT)
-                .toAnsi(reader.getTerminal());
-
-        registerColors();
+    public void stop() {
+        running = false;
     }
 
     public ConsoleCommandSender getSender() {
         return sender;
     }
 
-    public void start() {
-        sender = new ColoredCommandSender();
-        CONSOLE_DATE = server.getConsoleDateFormat();
-        for (Handler handler : logger.getHandlers()) {
-            if (handler.getClass() == FancyConsoleHandler.class) {
-                handler.setFormatter(new DateOutputFormatter(CONSOLE_DATE, true));
-            }
-        }
-        Thread thread = new ConsoleCommandThread();
-        thread.setName("ConsoleCommandThread");
-        thread.setDaemon(true);
-        thread.start();
-    }
-
-    public void registerColors() {
-        replacements.put(ChatColor.AQUA, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE)).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.BLACK, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.BLACK).boldOff()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.DARK_BLUE, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE).boldOff()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.DARK_GREEN, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN).boldOff()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.DARK_AQUA, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.CYAN).boldOff()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.DARK_RED, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.RED).boldOff()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.DARK_PURPLE, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.MAGENTA).boldOff()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.GOLD, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW).boldOff()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.GRAY, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.WHITE).boldOff()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.DARK_GRAY, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.BLACK).bold()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.BLUE, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE).bold()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.GREEN, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN).bold()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.AQUA, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.CYAN).bold()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.RED, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.RED).bold()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.LIGHT_PURPLE, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.MAGENTA).bold()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.YELLOW, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW).bold()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.WHITE, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.foreground(AttributedStyle.WHITE).bold()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.MAGIC, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.blink()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.BOLD, new AttributedStringBuilder().style(AttributedStyle.BOLD).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.STRIKETHROUGH, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.crossedOut()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.UNDERLINE, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.underline()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.ITALIC, new AttributedStringBuilder().style(AttributedStyle.DEFAULT.italic()).toAnsi(reader.getTerminal()));
-        replacements.put(ChatColor.RESET, new AttributedStringBuilder().style(AttributedStyle.DEFAULT).toAnsi(reader.getTerminal()));
-    }
-
-    public void startFile(String logfile) {
-        File parent = new File(logfile).getParentFile();
-        if (!parent.isDirectory() && !parent.mkdirs()) {
-            logger.warning("Could not create log folder: " + parent);
-        }
-        Handler fileHandler = new RotatingFileHandler(logfile);
-        FILE_DATE = server.getConsoleLogDateFormat();
-        fileHandler.setFormatter(new DateOutputFormatter(FILE_DATE, false));
-        logger.addHandler(fileHandler);
-    }
-
-    public void stop() {
-        running = false;
-        reader.getTerminal().writer().println();
-        for (Handler handler : logger.getHandlers()) {
-            handler.flush();
-            handler.close();
-        }
-    }
-
-    private String colorize(String string) {
-        if (string.indexOf(ChatColor.COLOR_CHAR) < 0) {
-            return string;  // no colors in the message
-        } else {
-            // colorize or strip all colors
-            for (ChatColor color : colors) {
-                if (replacements.containsKey(color)) {
-                    string = string.replaceAll("(?i)" + color, replacements.get(color));
-                } else {
-                    string = string.replaceAll("(?i)" + color, "");
-                }
-            }
-            return string;
-        }
-    }
-
-    private class FancyConsoleHandler extends ConsoleHandler {
-        public FancyConsoleHandler() {
-            setFormatter(new DateOutputFormatter(CONSOLE_DATE, true));
-            setOutputStream(System.out);
-        }
-
-        @Override
-        public synchronized void flush() {
-            reader.getTerminal().flush();
-            super.flush();
-        }
-    }
-
-    private class DateOutputFormatter extends Formatter {
-        private final SimpleDateFormat date;
-        private final boolean color;
-
-        public DateOutputFormatter(String pattern, boolean color) {
-            date = new SimpleDateFormat(pattern);
-            this.color = color;
-        }
-
-        @Override
-        public String format(LogRecord record) {
-            StringBuilder builder = new StringBuilder();
-
-            builder.append(date.format(record.getMillis()));
-            builder.append(" [");
-            builder.append(record.getLevel().getLocalizedName().toUpperCase());
-            builder.append("] ");
-            if (color) {
-                builder.append(colorize(formatMessage(record)));
-            } else {
-                builder.append(formatMessage(record));
-            }
-            builder.append('\n');
-
-            if (record.getThrown() != null) {
-                StringWriter writer = new StringWriter();
-                record.getThrown().printStackTrace(new PrintWriter(writer));
-                builder.append(writer);
-            }
-
-            return builder.toString();
-        }
-    }
-
-    private static class LoggerOutputStream extends ByteArrayOutputStream {
-        private final String separator = System.getProperty("line.separator");
-        private final Level level;
-
-        public LoggerOutputStream(Level level) {
-            this.level = level;
-        }
-
-        @Override
-        public synchronized void flush() throws IOException {
-            super.flush();
-            String record = toString();
-            reset();
-
-            if (!record.isEmpty() && !record.equals(separator)) {
-                logger.logp(level, "LoggerOutputStream", "log" + level, record);
-            }
-        }
-    }
-
-    private static class RotatingFileHandler extends StreamHandler {
-        private final SimpleDateFormat dateFormat;
-        private final String template;
-        private final boolean rotate;
-        private String filename;
-
-        public RotatingFileHandler(String template) {
-            this.template = template;
-            rotate = template.contains("%D");
-            dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            filename = calculateFilename();
-            updateOutput();
-        }
-
-        private void updateOutput() {
-            try {
-                setOutputStream(new FileOutputStream(filename, true));
-            } catch (IOException ex) {
-                logger.log(Level.SEVERE, "Unable to open " + filename + " for writing", ex);
-            }
-        }
-
-        private void checkRotate() {
-            if (rotate) {
-                String newFilename = calculateFilename();
-                if (!filename.equals(newFilename)) {
-                    filename = newFilename;
-                    // note that the console handler doesn't see this message
-                    super.publish(new LogRecord(Level.INFO, "Log rotating to: " + filename));
-                    updateOutput();
-                }
-            }
-        }
-
-        private String calculateFilename() {
-            return template.replace("%D", dateFormat.format(new Date()));
-        }
-
-        @Override
-        public synchronized void publish(LogRecord record) {
-            if (!isLoggable(record)) {
-                return;
-            }
-            checkRotate();
-            super.publish(record);
-            super.flush();
-        }
-
-        @Override
-        public synchronized void flush() {
-            checkRotate();
-            super.flush();
-        }
-    }
-
     private class CommandCompleter implements Completer {
         @Override
         public void complete(LineReader reader, ParsedLine line, List<Candidate> candidates) {
+            List<String> completions = null;
             try {
-                List<String> completions = server.getScheduler().syncIfNeeded(() -> server.getCommandMap().tabComplete(sender, line.line()));
-                if (completions == null) {
-                    return;
-                }
+                completions = server.getScheduler().syncIfNeeded(() -> server.getCommandMap().tabComplete(sender, line.line()));
+            } catch (Exception e) {
+                GlowServer.logger.log(Level.WARNING, "Error while tab completing", e);
+            }
 
-                completions.forEach(s -> candidates.add(new Candidate(s)));
-            } catch (Throwable t) {
-                logger.log(Level.WARNING, "Error while tab completing", t);
+            if (completions != null) {
+                completions.forEach(completion -> candidates.add(new Candidate(completion)));
             }
         }
     }
 
     private class ConsoleCommandThread extends Thread {
+        ConsoleCommandThread() {
+            setName("ConsoleCommandThread");
+            setDaemon(true);
+        }
+
         @Override
         public void run() {
             String command = null;
-            while (running) {
+            while (running && !Thread.interrupted()) {
                 try {
                     command = reader.readLine();
-                } catch (UserInterruptException e) {
-                    logger.log(Level.WARNING, "Exception while executing command: " + command, e);
-                } catch (EndOfFileException e) {
-                    logger.log(Level.SEVERE, "Error while reading commands", e);
+                } catch (CommandException e) {
+                    GlowServer.logger.log(Level.WARNING, "Exception while executing command: " + command, e);
+                } catch (Exception e) {
+                    GlowServer.logger.log(Level.SEVERE, "Error while reading commands", e);
                 }
 
-                if (command == null || (command = command.trim()).isEmpty()) {
-                    continue;
+                if (command != null && !(command = command.trim()).isEmpty()) {
+                    server.getScheduler().runTaskAsynchronously(null, new CommandTask(command));
                 }
-
-                reader.getTerminal().writer().println(CONSOLE_PROMPT + "\"" + command + "\"");
-                reader.getTerminal().flush();
-
-                server.getScheduler().runTask(null, new CommandTask(command));
             }
         }
     }
@@ -328,7 +143,7 @@ public final class ConsoleManager {
     private class CommandTask implements Runnable {
         private final String command;
 
-        public CommandTask(String command) {
+        CommandTask(String command) {
             this.command = command;
         }
 
