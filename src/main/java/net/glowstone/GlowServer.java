a6dfb12b9a5ea393e710f2a3ac162765866e399d
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-05-21 18:33:13 -0700

    Fix warning loading Bukkit plugins also supporting Sponge

--- src/main/java/net/glowstone/GlowServer.java
+++ src/main/java/net/glowstone/GlowServer.java
@@ -859,7 +859,14 @@ public final class GlowServer implements Server {
                 }
             }
 
-            if (!hasSponge) {
+            boolean spongeOnlyPlugins = false;
+            for (File spongePlugin: pluginTypeDetector.spongePlugins) {
+                if (!pluginTypeDetector.bukkitPlugins.contains(spongePlugin)) {
+                    spongeOnlyPlugins = true;
+                }
+            }
+
+            if (!hasSponge && spongeOnlyPlugins) {
                 logger.log(Level.WARNING, "SpongeAPI plugins found, but no Sponge bridge present! They will be ignored.");
                 for (File file : getSpongePlugins()) {
                     logger.log(Level.WARNING, "Ignored SpongeAPI plugin: " + file.getPath());
