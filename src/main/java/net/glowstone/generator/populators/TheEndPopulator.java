4c9f326456643eb3bbcfea8cf87c2c2fa7c6dbb8
mastercoms <circuitsoft@tuta.io>
2017-05-28 16:40:17 -0400

    Spawn the ender dragon in the end

--- src/main/java/net/glowstone/generator/populators/TheEndPopulator.java
+++ src/main/java/net/glowstone/generator/populators/TheEndPopulator.java
@@ -2,7 +2,9 @@ package net.glowstone.generator.populators;
 
 import net.glowstone.generator.decorators.theend.ObsidianPillarDecorator;
 import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.EntityType;
 import org.bukkit.generator.BlockPopulator;
 
 import java.util.Random;
@@ -21,9 +23,8 @@ public class TheEndPopulator extends BlockPopulator {
 
         // spawn the enderdragon
         if (chunk.getX() == 0 && chunk.getZ() == 0) {
-            // TODO: uncomment when entities are functional
-            //final Location loc = new Location(world, (chunk.getX() << 4) + 8, 128, (chunk.getZ() << 4) + 8, random.nextFloat() * 360, 0);
-            //world.spawnEntity(loc, EntityType.ENDER_DRAGON);
+            final Location loc = new Location(world, (chunk.getX() << 4) + 8, 128, (chunk.getZ() << 4) + 8, random.nextFloat() * 360, 0);
+            world.spawnEntity(loc, EntityType.ENDER_DRAGON);
         }
     }
 }
