5faac53886bcf92883ce808f4023f8a39161ab1e
mastercoms <circuitsoft@tuta.io>
2017-05-28 16:31:24 -0400

    Properly spawn item frame

--- src/main/java/net/glowstone/block/itemtype/ItemItemFrame.java
+++ src/main/java/net/glowstone/block/itemtype/ItemItemFrame.java
@@ -1,6 +1,7 @@
 package net.glowstone.block.itemtype;
 
 
+import com.flowpowered.network.Message;
 import net.glowstone.block.GlowBlock;
 import net.glowstone.entity.GlowPlayer;
 import net.glowstone.entity.objects.GlowItemFrame;
@@ -8,10 +9,14 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
+import java.util.List;
+
 public class ItemItemFrame extends ItemType {
 
     @Override
     public void rightClickBlock(GlowPlayer player, GlowBlock target, BlockFace face, ItemStack holding, Vector clickedLoc) {
-        new GlowItemFrame(player, target.getLocation().getBlock().getRelative(face).getLocation(), face);
+        GlowItemFrame entity = new GlowItemFrame(player, target.getLocation().getBlock().getRelative(face).getLocation(), face);
+        List<Message> spawnMessage = entity.createSpawnMessage();
+        entity.getWorld().getRawPlayers().stream().filter(p -> p.canSeeEntity(entity)).forEach(p -> p.getSession().sendAll(spawnMessage.toArray(new Message[spawnMessage.size()])));
     }
 }
