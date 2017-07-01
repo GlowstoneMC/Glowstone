02321e75f5e26f7978bd6d3157b4d6654f0384fb
Martin Panzer <postremus1996@googlemail.com>
2017-07-02 00:47:41 +0200

    Respawn on ground (#513)
    * Player position needs to be send before chunk data
    Also try to fix some of the other more obious situations, where our sending order is different from vanilla during join or respawn
    
    closes #460
    
    * find safe y to spawn the player at
    also center the player on the block

--- src/main/java/net/glowstone/entity/GlowPlayer.java
+++ src/main/java/net/glowstone/entity/GlowPlayer.java
@@ -355,7 +355,45 @@ public class GlowPlayer extends GlowHumanEntity implements Player {
                 return loc;
             }
         }
-        return session.getServer().getWorlds().get(0).getSpawnLocation();
+
+        return findSafeSpawnLocation(session.getServer().getWorlds().get(0).getSpawnLocation());
+    }
+
+    /**
+     * Find a a Location obove or below the specified Location, which
+     * is on ground.
+     * The returned Location will be at the center of the block, X and Y wise.
+     *
+     * @param spawn The Location a safe spawn position should be found at.
+     * @return The location to spawn the player at.
+     */
+    private static Location findSafeSpawnLocation(Location spawn) {
+        World world = spawn.getWorld();
+        int blockX = spawn.getBlockX();
+        int blockY = spawn.getBlockY();
+        int blockZ = spawn.getBlockZ();
+
+        int highestY = world.getHighestBlockYAt(blockX, blockZ);
+
+        int y = blockY;
+        boolean wasPreviousSafe = false;
+        for (; y <= highestY; y++) {
+            Material type = world.getBlockAt(blockX, y, blockZ).getType();
+            boolean safe = Material.AIR.equals(type);
+
+            if (wasPreviousSafe && safe) {
+                y--;
+                break;
+            }
+            wasPreviousSafe = safe;
+        }
+
+        return new Location(
+            world,
+            blockX + 0.5,
+            y,
+            blockZ + 0.5
+        );
     }
 
     public void join(GlowSession session, PlayerReader reader) {
@@ -383,22 +421,23 @@ public class GlowPlayer extends GlowHumanEntity implements Player {
         saveData();
 
         streamBlocks(); // stream the initial set of blocks
-        setCompassTarget(world.getSpawnLocation()); // set our compass target
-        sendTime();
         sendWeather();
         sendRainDensity();
         sendSkyDarkness();
         sendAbilities();
+
+        // send initial location
+        session.send(new PositionRotationMessage(location));
+
         session.send(((GlowWorldBorder) world.getWorldBorder()).createMessage());
+        sendTime();
+        setCompassTarget(world.getSpawnLocation()); // set our compass target
 
         scoreboard = server.getScoreboardManager().getMainScoreboard();
         scoreboard.subscribe(this);
 
         invMonitor = new InventoryMonitor(getOpenInventory());
         updateInventory(); // send inventory contents
-
-        // send initial location
-        session.send(new PositionRotationMessage(location));
 
         if (!server.getResourcePackURL().isEmpty()) {
             setResourcePack(server.getResourcePackURL(), server.getResourcePackHash());
@@ -798,11 +837,13 @@ public class GlowPlayer extends GlowHumanEntity implements Player {
         // spawn into world
         String type = world.getWorldType().getName().toLowerCase();
         session.send(new RespawnMessage(world.getEnvironment().getId(), world.getDifficulty().getValue(), getGameMode().getValue(), type));
+
         setRawLocation(location, false); // take us to spawn position
-        streamBlocks(); // stream blocks
-        setCompassTarget(world.getSpawnLocation()); // set our compass target
         session.send(new PositionRotationMessage(location));
         teleportedTo = location.clone();
+        setCompassTarget(world.getSpawnLocation()); // set our compass target
+
+        streamBlocks(); // stream blocks
 
         sendWeather();
         sendRainDensity();
@@ -835,6 +876,10 @@ public class GlowPlayer extends GlowHumanEntity implements Player {
                 setBedSpawnLocation(null);
                 sendMessage("Your home bed was missing or obstructed");
             }
+        }
+
+        if (!spawnAtBed) {
+            dest = findSafeSpawnLocation(dest);
         }
 
         // fire event and perform spawn
