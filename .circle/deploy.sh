a828189e8958ec5df7d4cddb3124aafeb662616d
momothereal <momothereal.mc@gmail.com>
2017-07-03 13:30:02 -0400

    Remove JavaDocs deployment for now
    It is hanging CircleCI builds

--- .circle/deploy.sh
+++ .circle/deploy.sh
@@ -3,5 +3,5 @@ ssh -t glowstone@185.116.156.30 "rm -rf glowstone.jar worlds/*"
 sftp -b .circle/upload glowstone@185.116.156.30
 ssh -t glowstone@185.116.156.30 "sudo systemctl start glowstone"
 
-sftp -b .circle/upload_jd leaf@glowstone.gserv.me
-ssh -t leaf@glowstone.gserv.me "./deploy_javadocs.sh"
+# sftp -b .circle/upload_jd leaf@glowstone.gserv.me
+# ssh -t leaf@glowstone.gserv.me "./deploy_javadocs.sh"
