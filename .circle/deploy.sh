17274e1dc44586952926c10ed49dfa2e40b45e46
Gareth Coles <gdude2002@gmail.com>
2017-07-04 13:21:41 +0000

    JDs are handled by the server now

--- .circle/deploy.sh
+++ .circle/deploy.sh
@@ -2,6 +2,3 @@ ssh -t glowstone@185.116.156.30 "sudo systemctl stop glowstone"
 ssh -t glowstone@185.116.156.30 "rm -rf glowstone.jar worlds/*"
 sftp -b .circle/upload glowstone@185.116.156.30
 ssh -t glowstone@185.116.156.30 "sudo systemctl start glowstone"
-
-# sftp -b .circle/upload_jd leaf@glowstone.gserv.me
-# ssh -t leaf@glowstone.gserv.me "./deploy_javadocs.sh"
