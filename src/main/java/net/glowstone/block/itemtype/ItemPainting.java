5f784434ffec145eb5a7dc2575a9e3a5c409c704
Martin Panzer <postremus1996@googlemail.com>
2017-07-23 22:28:32 +0200

    Choose more appropriate painting size some more cleanup

--- src/main/java/net/glowstone/block/itemtype/ItemPainting.java
+++ src/main/java/net/glowstone/block/itemtype/ItemPainting.java
@@ -1,8 +1,15 @@
 package net.glowstone.block.itemtype;
 
+import static java.util.Collections.reverseOrder;
+import static java.util.Comparator.comparingInt;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import net.glowstone.block.GlowBlock;
 import net.glowstone.entity.GlowPlayer;
-import net.glowstone.util.PaintingSpawner;
+import net.glowstone.entity.objects.GlowPainting;
+import org.bukkit.Art;
 import org.bukkit.block.BlockFace;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
@@ -10,8 +17,28 @@ import org.bukkit.util.Vector;
 
 public class ItemPainting extends ItemType {
 
+    private static final List<Art> SORTED_ART;
+
+    static {
+        SORTED_ART = Arrays.stream(Art.values())
+            .sorted(
+                reverseOrder(
+                    comparingInt(Art::getBlockHeight)
+                    .thenComparingInt(Art::getBlockWidth)
+                )
+            )
+            .collect(Collectors.toList());
+    }
+
     @Override
     public void rightClickBlock(GlowPlayer player, GlowBlock target, BlockFace face, ItemStack holding, Vector clickedLoc, EquipmentSlot hand) {
-        new PaintingSpawner().spawnAt(target.getLocation(), face);
+        GlowPainting painting = new GlowPainting(target.getRelative(face).getLocation(), face);
+
+        for (Art art : SORTED_ART) {
+            painting.setArtInternal(art);
+            if (!painting.isObstructed()) {
+                break;
+            }
+        }
     }
 }
--- src/main/java/net/glowstone/entity/objects/GlowPainting.java
+++ src/main/java/net/glowstone/entity/objects/GlowPainting.java
@@ -5,48 +5,51 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import net.glowstone.EventFactory;
 import net.glowstone.entity.GlowHangingEntity;
 import net.glowstone.net.message.play.entity.SpawnPaintingMessage;
-import net.glowstone.util.PaintingSpawner;
 import org.bukkit.Art;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Painting;
+import org.bukkit.event.hanging.HangingBreakEvent;
+import org.bukkit.event.hanging.HangingBreakEvent.RemoveCause;
 import org.bukkit.inventory.ItemStack;
 
 public class GlowPainting extends GlowHangingEntity implements Painting {
     private Art art;
 
-    private static final Map<Art, String> artTitle = new HashMap<>();
+    private static final Map<Art, String> ART_TITLE = new HashMap<>();
 
     static {
-        artTitle.put(Art.KEBAB, "Kebab");
-        artTitle.put(Art.AZTEC, "Aztec");
-        artTitle.put(Art.ALBAN, "Alban");
-        artTitle.put(Art.AZTEC2, "Aztec2");
-        artTitle.put(Art.BOMB, "Bomb");
-        artTitle.put(Art.PLANT, "Plant");
-        artTitle.put(Art.WASTELAND, "Wasteland");
-        artTitle.put(Art.POOL, "Pool");
-        artTitle.put(Art.COURBET, "Courbet");
-        artTitle.put(Art.SEA, "Sea");
-        artTitle.put(Art.SUNSET, "Sunset");
-        artTitle.put(Art.CREEBET, "Creebet");
-        artTitle.put(Art.WANDERER, "Wanderer");
-        artTitle.put(Art.GRAHAM, "Graham");
-        artTitle.put(Art.MATCH, "Match");
-        artTitle.put(Art.BUST, "Bust");
-        artTitle.put(Art.STAGE, "Stage");
-        artTitle.put(Art.VOID, "Void");
-        artTitle.put(Art.SKULL_AND_ROSES, "SkullAndRoses");
-        artTitle.put(Art.WITHER, "Wither");
-        artTitle.put(Art.FIGHTERS, "Fighters");
-        artTitle.put(Art.POINTER, "Pointer");
-        artTitle.put(Art.BURNINGSKULL, "BurningSkull");
-        artTitle.put(Art.SKELETON, "Skeleton");
-        artTitle.put(Art.DONKEYKONG, "DonkeyKong");
+        ART_TITLE.put(Art.KEBAB, "Kebab");
+        ART_TITLE.put(Art.AZTEC, "Aztec");
+        ART_TITLE.put(Art.ALBAN, "Alban");
+        ART_TITLE.put(Art.AZTEC2, "Aztec2");
+        ART_TITLE.put(Art.BOMB, "Bomb");
+        ART_TITLE.put(Art.PLANT, "Plant");
+        ART_TITLE.put(Art.WASTELAND, "Wasteland");
+        ART_TITLE.put(Art.POOL, "Pool");
+        ART_TITLE.put(Art.COURBET, "Courbet");
+        ART_TITLE.put(Art.SEA, "Sea");
+        ART_TITLE.put(Art.SUNSET, "Sunset");
+        ART_TITLE.put(Art.CREEBET, "Creebet");
+        ART_TITLE.put(Art.WANDERER, "Wanderer");
+        ART_TITLE.put(Art.GRAHAM, "Graham");
+        ART_TITLE.put(Art.MATCH, "Match");
+        ART_TITLE.put(Art.BUST, "Bust");
+        ART_TITLE.put(Art.STAGE, "Stage");
+        ART_TITLE.put(Art.VOID, "Void");
+        ART_TITLE.put(Art.SKULL_AND_ROSES, "SkullAndRoses");
+        ART_TITLE.put(Art.WITHER, "Wither");
+        ART_TITLE.put(Art.FIGHTERS, "Fighters");
+        ART_TITLE.put(Art.POINTER, "Pointer");
+        ART_TITLE.put(Art.BURNINGSKULL, "BurningSkull");
+        ART_TITLE.put(Art.SKELETON, "Skeleton");
+        ART_TITLE.put(Art.DONKEYKONG, "DonkeyKong");
     }
 
     public GlowPainting(Location location) {
@@ -55,7 +58,8 @@ public class GlowPainting extends GlowHangingEntity implements Painting {
 
     public GlowPainting(Location location, BlockFace clickedface) {
         super(location, clickedface);
-        this.setArtInternal(Art.KEBAB);
+        this.art = Art.KEBAB;
+        setSize(art.getBlockWidth(), art.getBlockHeight());
     }
 
     @Override
@@ -68,7 +72,7 @@ public class GlowPainting extends GlowHangingEntity implements Painting {
         int x = location.getBlockX();
         int y = location.getBlockY();
         int z = location.getBlockZ();
-        String title = artTitle.get(art);
+        String title = ART_TITLE.get(art);
 
         return Collections.singletonList(
             new SpawnPaintingMessage(this.getEntityId(), this.getUniqueId(), title, x, y, z, facing.ordinal())
@@ -87,11 +91,16 @@ public class GlowPainting extends GlowHangingEntity implements Painting {
 
     @Override
     public boolean setArt(Art art, boolean force) {
-        if (new PaintingSpawner().spawn(location, art, facing.getBlockFace(), force)) {
-            this.art = art;
-            this.remove();
-            return true;
+        Art oldArt = this.art;
+        this.art = art;
+
+        if (isObstructed()) {
+            this.art = oldArt;
+            return false;
         }
+
+        setSize(art.getBlockWidth(), art.getBlockHeight());
+        this.remove();
         return false;
     }
 
@@ -101,17 +110,26 @@ public class GlowPainting extends GlowHangingEntity implements Painting {
 
     @Override
     public boolean setFacingDirection(BlockFace blockFace, boolean force) {
-        if (new PaintingSpawner().spawn(location, art, blockFace, force)) {
-            facing = HangingFace.getByBlockFace(blockFace);
-            this.remove();
-            return true;
+        HangingFace oldFace = facing;
+        this.facing = HangingFace.getByBlockFace(blockFace);
+
+        if (isObstructed()) {
+            this.facing = oldFace;
+            return false;
         }
-        return false;
+
+        this.remove();
+        return true;
     }
 
     @Override
     public void setFacingDirection(BlockFace blockFace) {
         setFacingDirection(blockFace, false);
+    }
+
+    @Override
+    protected void updateBoundingBox() {
+        super.updateBoundingBox();
     }
 
     @Override
@@ -125,10 +143,78 @@ public class GlowPainting extends GlowHangingEntity implements Painting {
                 remove();
             }
         }
+
+        if (ticksLived % (20 * 5) == 0) {
+            if (isObstructed()) {
+                if (EventFactory.callEvent(new HangingBreakEvent(this, RemoveCause.PHYSICS)).isCancelled()) {
+                    return;
+                }
+                if (location.getBlock().getRelative(getAttachedFace()).getType() == Material.AIR) {
+                    world.dropItemNaturally(location, new ItemStack(Material.PAINTING));
+                    remove();
+                }
+            }
+        }
     }
 
     @Override
     protected void pulsePhysics() {
         // item frames aren't affected by physics
+    }
+
+    public boolean isObstructed() {
+        Location topLeftCorner = getTopLeftCorner().getBlock().getRelative(facing.getBlockFace().getOppositeFace()).getLocation();
+        BlockFace right = getLeftFace().getBlockFace().getOppositeFace();
+
+        Location current = topLeftCorner.clone();
+        for (int y = 0; y < art.getBlockHeight(); y++) {
+            for (int x = 0; x < art.getBlockWidth(); x++) {
+                if (!canHoldPainting(current)) {
+                    return true;
+                }
+                current = current.getBlock().getRelative(right).getLocation();
+            }
+            current = current.getBlock().getRelative(BlockFace.DOWN).getLocation();
+            current.setX(topLeftCorner.getX());
+            current.setZ(topLeftCorner.getZ());
+        }
+
+        return !this.world.getEntityManager().getEntitiesInside(this.boundingBox, this).isEmpty();
+    }
+
+    private boolean canHoldPainting(Location where) {
+        if (!where.getBlock().getType().isSolid()) {
+            return false;
+        }
+
+        Block inFront = where.clone().getBlock().getRelative(facing.getBlockFace());
+        if (inFront.getType().isSolid()) {
+            return false;
+        }
+        return true;
+    }
+
+    private Location getTopLeftCorner() {
+        BlockFace left = getLeftFace().getBlockFace();
+        Location topLeft = location.clone();
+        int topMod = art.getBlockHeight() / 2;
+        int widthMod = Math.max(0, art.getBlockHeight() / 2 - 1);
+
+        topLeft.add(left.getModX() * widthMod, topMod, left.getModZ() * widthMod);
+        return topLeft;
+    }
+
+    private HangingFace getLeftFace() {
+        return HangingFace.values()[(facing.ordinal() + 1) % 4];
+    }
+
+    @Override
+    public double getWidth() {
+        return 0.5;
+    }
+
+    @Override
+    public double getHeight() {
+        return 0.5;
     }
 }
