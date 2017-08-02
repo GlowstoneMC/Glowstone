964be35ebaf41b2d982fdb4a5502c7870b6a4375
momothereal <momothereal.mc@gmail.com>
2017-08-01 20:15:41 -0400

    Revert last commit (except timeouts) because of passwords.
    Must look deeper into this (while we wait for connection to test server to be fixed)

--- .circle/deploy.sh
+++ .circle/deploy.sh
@@ -1,8 +1,4 @@
-function execute_safely {
-    "$@"
-    return 0
-}
-execute_safely ssh -t glowstone@185.116.156.30 "sudo systemctl stop glowstone" -o ConnectTimeout=15
-execute_safely ssh -t glowstone@185.116.156.30 "rm -rf glowstone.jar worlds/*" -o ConnectTimeout=15
-execute_safely sftp -b .circle/upload glowstone@185.116.156.30 -o ConnectTimeout=15
-execute_safely ssh -t glowstone@185.116.156.30 "sudo systemctl start glowstone" -o ConnectTimeout=15
+ssh -t glowstone@185.116.156.30 "sudo systemctl stop glowstone" -o ConnectTimeout=15
+ssh -t glowstone@185.116.156.30 "rm -rf glowstone.jar worlds/*" -o ConnectTimeout=15
+sftp -b .circle/upload glowstone@185.116.156.30 -o ConnectTimeout=15
+ssh -t glowstone@185.116.156.30 "sudo systemctl start glowstone" -o ConnectTimeout=15
