b4d330463144ec8e310c5ef1d43378f007039d8d
momothereal <momothereal.mc@gmail.com>
2017-08-01 20:04:33 -0400

    [CircleCI] Don't fail build when test-server deployment fails

--- .circle/deploy.sh
+++ .circle/deploy.sh
@@ -1,4 +1,8 @@
-ssh -t glowstone@185.116.156.30 "sudo systemctl stop glowstone"
-ssh -t glowstone@185.116.156.30 "rm -rf glowstone.jar worlds/*"
-sftp -b .circle/upload glowstone@185.116.156.30
-ssh -t glowstone@185.116.156.30 "sudo systemctl start glowstone"
+function execute_safely {
+    "$@"
+    return 0
+}
+execute_safely ssh -t glowstone@185.116.156.30 "sudo systemctl stop glowstone" -o ConnectTimeout=15
+execute_safely ssh -t glowstone@185.116.156.30 "rm -rf glowstone.jar worlds/*" -o ConnectTimeout=15
+execute_safely sftp -b .circle/upload glowstone@185.116.156.30 -o ConnectTimeout=15
+execute_safely ssh -t glowstone@185.116.156.30 "sudo systemctl start glowstone" -o ConnectTimeout=15
