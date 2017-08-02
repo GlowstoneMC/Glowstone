ffa2527715bc929f8bad2ab3d1ea744f81723e91
momothereal <momothereal.mc@gmail.com>
2017-08-02 17:42:27 -0400

    [CircleCI] Move options before target host/commands

--- .circle/deploy.sh
+++ .circle/deploy.sh
@@ -1,4 +1,4 @@
-ssh -t glowstone@185.116.156.30 "sudo systemctl stop glowstone" -o ConnectTimeout=15
-ssh -t glowstone@185.116.156.30 "rm -rf glowstone.jar worlds/*" -o ConnectTimeout=15
-sftp -b .circle/upload glowstone@185.116.156.30 -o ConnectTimeout=15
-ssh -t glowstone@185.116.156.30 "sudo systemctl start glowstone" -o ConnectTimeout=15
+ssh -o ConnectTimeout=15 -t glowstone@185.116.156.30 "sudo systemctl stop glowstone"
+ssh -o ConnectTimeout=15 -t glowstone@185.116.156.30 "rm -rf glowstone.jar worlds/*"
+sftp -o ConnectTimeout=15 -b .circle/upload glowstone@185.116.156.30
+ssh -o ConnectTimeout=15 -t glowstone@185.116.156.30 "sudo systemctl start glowstone"
