ba2db62e73c585635a3cffc0a10da5f8ebb31630
Foorack <maxfaxalv@gmail.com>
2017-08-08 19:46:57 +0200

    i18n BlockBed

--- src/main/java/net/glowstone/block/blocktype/BlockBed.java
+++ src/main/java/net/glowstone/block/blocktype/BlockBed.java
@@ -237,12 +237,12 @@ public class BlockBed extends BlockType {
         // Sleeping is only possible during the night or a thunderstorm
         // Tick values for day/night time taken from the minecraft wiki
         if (world.getTime() < 12541 || world.getTime() > 23458 || world.isThundering()) {
-            player.sendMessage("You can only sleep at night");
+            player.sendMessage(GlowServer.lang.getString("event.sleep.day"));
             return true;
         }
 
         if (isOccupied(block)) {
-            player.sendMessage("This bed is occupied.");
+            player.sendMessage(GlowServer.lang.getString("event.sleep.occupied"));
             return true;
         }
 
@@ -253,7 +253,7 @@ public class BlockBed extends BlockType {
         for (LivingEntity e : world.getLivingEntities()) {
             // Check for hostile mobs relative to the block below the head of the bed
             if (e instanceof Creature && (e.getType() != EntityType.PIG_ZOMBIE || ((PigZombie) e).isAngry()) && isWithinDistance(e, block.getRelative(BlockFace.DOWN), 8, 5, 8)) {
-                player.sendMessage("You may not rest now, there are monsters nearby");
+                player.sendMessage(GlowServer.lang.getString("event.sleep.monsters"));
                 return true;
             }
         }
