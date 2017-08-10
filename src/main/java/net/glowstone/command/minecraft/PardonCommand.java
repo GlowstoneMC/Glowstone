e80fc46b3ed81e1c1dd0fff4d6b93d4f282fb700
Foorack <maxfaxalv@gmail.com>
2017-08-10 05:32:13 +0200

    i18n PardonCommand

--- src/main/java/net/glowstone/command/minecraft/PardonCommand.java
+++ src/main/java/net/glowstone/command/minecraft/PardonCommand.java
@@ -1,5 +1,7 @@
 package net.glowstone.command.minecraft;
 
+import net.glowstone.GlowServer;
+
 import org.bukkit.BanList;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -11,7 +13,7 @@ import java.util.Collections;
 
 public class PardonCommand extends VanillaCommand {
     public PardonCommand() {
-        super("pardon", "Unbans a player from the server.", "/pardon <name>", Collections.emptyList());
+        super("pardon", GlowServer.lang.getString("command.minecraft.pardon.description"), GlowServer.lang.getString("command.minecraft.pardon.usage"), Collections.emptyList());
         setPermission("minecraft.command.pardon");
     }
 
@@ -21,18 +23,18 @@ public class PardonCommand extends VanillaCommand {
             return false;
         }
         if (args.length != 1) {
-            sender.sendMessage(ChatColor.RED + "Usage: " + usageMessage);
+            sender.sendMessage(ChatColor.RED + GlowServer.lang.getString(sender, "command.generic.usage", GlowServer.lang.getString(sender, "command.minecraft.pardon.usage")));
             return false;
         }
         String name = args[0];
         OfflinePlayer player = Bukkit.getOfflinePlayer(name);
         BanList banList = Bukkit.getServer().getBanList(BanList.Type.NAME);
         if (!banList.isBanned(player.getName())) {
-            sender.sendMessage(ChatColor.RED + "Could not unban player " + name);
+            sender.sendMessage(ChatColor.RED + GlowServer.lang.getString(sender, "command.minecraft.pardon.failed", name));
             return false;
         }
         banList.pardon(player.getName());
-        sender.sendMessage("Unbanned player " + name);
+        sender.sendMessage(GlowServer.lang.getString(sender, "command.minecraft.pardon.success", name));
         return true;
     }
 }
