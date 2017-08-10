4b2a4cfc37f48bb96b9bf5f474e6942c42df17df
Foorack <maxfaxalv@gmail.com>
2017-08-10 05:15:05 +0200

    i18n OpCommand

--- src/main/java/net/glowstone/command/minecraft/OpCommand.java
+++ src/main/java/net/glowstone/command/minecraft/OpCommand.java
@@ -1,5 +1,7 @@
 package net.glowstone.command.minecraft;
 
+import net.glowstone.GlowServer;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
@@ -10,7 +12,7 @@ import java.util.Collections;
 
 public class OpCommand extends VanillaCommand {
     public OpCommand() {
-        super("op", "Turns a player into a server operator.", "/op <player>", Collections.emptyList());
+        super("op", GlowServer.lang.getString("command.minecraft.op.description"), GlowServer.lang.getString("command.minecraft.op.usage"), Collections.emptyList());
         setPermission("minecraft.command.op");
     }
 
@@ -18,13 +20,13 @@ public class OpCommand extends VanillaCommand {
     public boolean execute(CommandSender sender, String label, String[] args) {
         if (!testPermission(sender)) return false;
         if (args.length != 1) {
-            sender.sendMessage(ChatColor.RED + "Usage: " + usageMessage);
+            sender.sendMessage(ChatColor.RED + GlowServer.lang.getString(sender, "command.generic.usage", GlowServer.lang.getString(sender, "command.minecraft.op.usage")));
             return false;
         }
         String name = args[0];
         OfflinePlayer player = Bukkit.getOfflinePlayer(name);
         player.setOp(true);
-        sender.sendMessage("Opped " + player.getName());
+        sender.sendMessage(GlowServer.lang.getString(sender, "command.minecraft.op.opped", player.getName()));
         return true;
     }
 }
