1b7fc161aedeee870dc2ca56d362ed7122b21568
Foorack <maxfaxalv@gmail.com>
2017-08-11 18:29:05 +0200

    Add i18n tests

--- src/main/java/net/glowstone/util/lang/I.java
+++ src/main/java/net/glowstone/util/lang/I.java
@@ -1,6 +1,9 @@
 package net.glowstone.util.lang;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 import java.util.ResourceBundle;
 import java.util.MissingResourceException;
 
@@ -17,6 +20,11 @@ import lombok.Setter;
 public final class I {
     private I() {}// Prevent instantiation of this class
 
+    /**
+     * A full list of all available locales in Minecraft.
+     * Updated as of 11 August 2017.
+     */
+    public static final List<String> LOCALES = Arrays.asList("af_ZA", "ar_SA", "ast_ES", "az_AZ", "be_BY", "bg_BG", "br_FR", "ca-val_ES", "ca_ES", "cs_CZ", "cy_GB", "da_DK", "de_AT", "de_DE", "el_GR", "en_7S", "en_AU", "en_CA", "en_GB", "en_NZ", "en_UD", "en_US", "eo_UY", "es_AR", "es_ES", "es_MX", "es_UY", "es_VE", "et_EE", "eu_ES", "fa_IR", "fi_FI", "fil_PH", "fo_FO", "fr_CA", "fr_FR", "fy_NL", "ga_IE", "gd_GB", "gl_ES", "gv_IM", "haw", "he_IL", "hi_IN", "hr_HR", "hu_HU", "hy_AM", "id_ID", "io", "is_IS", "it_IT", "ja_JP", "jbo_EN", "ka_GE", "ko_KR", "ksh_DE", "kw_GB", "la_VA", "lb_LU", "li_LI", "lol_US", "lt_LT", "lv_LV", "mi_NZ", "mk_MK", "mn_MN", "ms_MY", "mt_MT", "nb_NO", "nds_DE", "nl_NL", "nn_NO", "no_NO", "oc_FR", "pl_PL", "pt_BR", "pt_PT", "qya_AA", "ro_RO", "ru_RU", "sk_SK", "sl_SI", "sme", "so_SO", "sq_AL", "sr_SP", "sv_SE", "swg_DE", "th_TH", "tl_PH", "tlh_AA", "tr_TR", "tzl_TZL", "uk_UA", "vi_VN", "zh_CN", "zh_TW");
     /**
      * Path basename used when looking up language resources.
      */
@@ -41,12 +49,12 @@ public final class I {
 
     /**
      * Set the new default locale to be used during server-wide translation lookups.
-     * The Locale should be in the following format: <language>_<COUNTRY>
+     * The Locale should be in the following format: <language>-<COUNTRY>
      * 
      * For example:
      *   - en-US for English (US)
      *   - fr-FR for French
-     *   - es_ES for Spanish
+     *   - es-ES for Spanish
      *
      * A full list of Minecraft supported language codes can be found here:
      * https://minecraft.gamepedia.com/Language#Available_languages *
@@ -54,13 +62,13 @@ public final class I {
      *   * Remember to replace the underscore `_` with a hyphen `-`, so en_US becomes en-US.
      *
      * @param locale New locale to be set as default locale.
-     * @throws IllegalArgumentException Thrown if the provided locale does not exist.
+     * @throws MissingResourceException Thrown if the specified locale does not exist.
      */
     public static void setDefaultLocale(@NonNull String locale) {
         try {
             ResourceBundle.getBundle(BASE_NAME, Locale.forLanguageTag(locale));
         } catch (MissingResourceException ex) {
-            throw new IllegalArgumentException("Missing locale '" + locale + "'. class=" + ex.getClassName());
+            throw new MissingResourceException("Missing locale '" + locale + "'. class=" + ex.getClassName(), ex.getClassName(), ex.getKey());
         }
         defaultLocale = locale;
     }
@@ -92,13 +100,14 @@ public final class I {
      * @param locale Locale to use when looking up translation.
      * @param key Language translation key mapping to a specific string in the translations.
      * @param args Arguments used by the formatter to replace placeholder variables. If there are more arguments than format specifiers, the extra arguments are ignored.
+     * @throws MissingResourceException Thrown if the provided specified does not exist.
     */
     public static String trl(@NonNull String locale, @NonNull String key, @NonNull Object ... args) {
         ResourceBundle rb;
         try {
             rb = ResourceBundle.getBundle(BASE_NAME, Locale.forLanguageTag(locale));
         } catch (MissingResourceException ex) {
-            throw new IllegalArgumentException("Missing locale '" + locale + "'. class=" + ex.getClassName());
+            throw new MissingResourceException("Missing locale '" + locale + "'. class=" + ex.getClassName(), ex.getClassName(), ex.getKey());
         }
 
         String result;
@@ -109,7 +118,7 @@ public final class I {
             if (fallback.containsKey(key)) {
                 result = fallback.getString(key);
             } else {
-                throw new MissingResourceException("Could not find translation for key '" + key + "' in fallback locale", locale, key);
+                throw new MissingTranslationException("Could not find translation for key '" + key + "' in fallback locale", locale, key);
             }
         }
         for (int i = 0; i != args.length; i++) {
@@ -124,7 +133,7 @@ public final class I {
      * @param player Player used when looking up locale.
      * @return The players locale code. For example `en-US` for English (US).
      */
-    public static String getEffectiveLocale(CommandSender sender) {
+    public static String getEffectiveLocale(@NonNull CommandSender sender) {
         // Only return the players locale if locale isn't locked.
         if (sender instanceof Player && !localeLocked) {
             String locale = ((Player) sender).getLocale();
@@ -132,6 +141,36 @@ public final class I {
         } else {
             return defaultLocale;
         }
+    }
+
+    /**
+     * Check if the specified locale exists or not.
+     *
+     * @throws MissingResourceException Thrown if the specified locale does not exist.
+     */
+    public static boolean doesLocaleExist(@NonNull String locale) {
+        try {
+            ResourceBundle.getBundle(BASE_NAME, Locale.forLanguageTag(locale));
+            return true;
+        } catch (MissingResourceException ex) {
+            return false;
+        } 
+    }
+
+    /**
+     * Returns a Set of translation keys exising in the specified locale.
+     *
+     * @throws MissingResourceException Thrown if the specified locale does not exist.
+     */
+    public static Set<String> getTranslationKeys(@NonNull String locale) {
+        ResourceBundle rb;
+        try {
+            rb = ResourceBundle.getBundle(BASE_NAME, Locale.forLanguageTag(locale));
+        } catch (MissingResourceException ex) {
+            throw new MissingResourceException("Missing locale '" + locale + "'. class=" + ex.getClassName(), ex.getClassName(), ex.getKey());
+        }
+
+        return rb.keySet();
     }
 
 }
