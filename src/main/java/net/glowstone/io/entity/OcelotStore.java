c262154d5023d28066f8656cb9fa6e9f2140a062
mastercoms <mastercoms@tutanota.de>
2017-12-12 07:16:30 -0500

    Less confusing ocelot type get

--- src/main/java/net/glowstone/io/entity/OcelotStore.java
+++ src/main/java/net/glowstone/io/entity/OcelotStore.java
@@ -3,7 +3,7 @@ package net.glowstone.io.entity;
 import net.glowstone.entity.passive.GlowOcelot;
 import net.glowstone.util.nbt.CompoundTag;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Ocelot.Type;
+import org.bukkit.entity.Ocelot;
 
 class OcelotStore extends TameableStore<GlowOcelot> {
 
@@ -15,9 +15,9 @@ class OcelotStore extends TameableStore<GlowOcelot> {
     public void load(GlowOcelot entity, CompoundTag compound) {
         super.load(entity, compound);
         if (compound.isInt("CatType")) {
-            entity.setCatType(Type.getType(compound.getInt("CatType")));
+            entity.setCatType(Ocelot.Type.getType(compound.getInt("CatType")));
         } else {
-            entity.setCatType(Type.WILD_OCELOT);
+            entity.setCatType(Ocelot.Type.WILD_OCELOT);
         }
 
     }
