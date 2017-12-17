9254c7488592729f4810ac260f3bea59075e23c8
momothereal <momothereal.mc@gmail.com>
2017-12-16 20:02:26 -0500

    Fix some issues with block/item interaction (see #595)
    * Removed impossible null check
    * Revert newly placed block on client-side when perception is incorrect
    * Call and handle right-click-air interact event in UseItemHandler

--- src/main/java/net/glowstone/net/handler/play/player/UseItemHandler.java
+++ src/main/java/net/glowstone/net/handler/play/player/UseItemHandler.java
@@ -1,6 +1,7 @@
 package net.glowstone.net.handler.play.player;
 
 import com.flowpowered.network.MessageHandler;
+import net.glowstone.EventFactory;
 import net.glowstone.block.ItemTable;
 import net.glowstone.block.itemtype.ItemType;
 import net.glowstone.block.itemtype.ItemType.Context;
@@ -9,6 +10,9 @@ import net.glowstone.net.GlowSession;
 import net.glowstone.net.message.play.player.UseItemMessage;
 import net.glowstone.util.InventoryUtil;
 import org.bukkit.Material;
+import org.bukkit.event.Event;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class UseItemHandler implements MessageHandler<GlowSession, UseItemMessage> {
@@ -16,7 +20,14 @@ public class UseItemHandler implements MessageHandler<GlowSession, UseItemMessag
     @Override
     public void handle(GlowSession session, UseItemMessage message) {
         GlowPlayer player = session.getPlayer();
-        ItemStack holding = player.getInventory().getItem(message.getEquipmentSlot());
+        ItemStack holding = InventoryUtil
+                .itemOrEmpty(player.getInventory().getItem(message.getEquipmentSlot()));
+
+        PlayerInteractEvent event = EventFactory
+                .onPlayerInteract(player, Action.RIGHT_CLICK_AIR, message.getEquipmentSlot());
+        if (event.useItemInHand() == null || event.useItemInHand() == Event.Result.DENY) {
+            return;
+        }
 
         if (!InventoryUtil.isEmpty(holding)) {
             ItemType type = ItemTable.instance().getItem(holding.getType());
@@ -24,8 +35,7 @@ public class UseItemHandler implements MessageHandler<GlowSession, UseItemMessag
                 if (type.getContext() == Context.AIR || type.getContext() == Context.ANY) {
                     type.rightClickAir(player, holding);
                 } else {
-                    if (holding.getType() == Material.WATER_BUCKET
-                        || holding.getType() == Material.LAVA_BUCKET) {
+                    if (holding.getType() == Material.WATER_BUCKET || holding.getType() == Material.LAVA_BUCKET) {
                         holding.setType(Material.BUCKET);
                     }
                 }
