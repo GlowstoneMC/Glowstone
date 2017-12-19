a822f0c9d79badf5c89ce1c877456841117deadf
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2017-12-18 19:52:48 -0800

    Implement GlowMapRenderer.render() (#608)
    Related to #572

--- src/main/java/net/glowstone/block/MaterialValueManager.java
+++ src/main/java/net/glowstone/block/MaterialValueManager.java
@@ -58,5 +58,13 @@ public interface MaterialValueManager {
          * @return the slipperiness
          */
         double getSlipperiness();
+
+        /**
+         * Returns the base map color for this value. Map pixels with this as the highest block can
+         * be this value plus 0 to 3.
+         *
+         * @return the base map color for this material
+         */
+        byte getBaseMapColor();
     }
 }
