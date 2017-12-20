adf0b2a15b5d7e4428231b6c29a7e92eda6ac5d9
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-12-19 21:14:41 -0800

    Implement arrow enchantments, damage calculations, flame
    All bow enchantments except knockback should now work.

--- src/main/java/net/glowstone/block/itemtype/ItemBow.java
+++ src/main/java/net/glowstone/block/itemtype/ItemBow.java
@@ -1,9 +1,11 @@
 package net.glowstone.block.itemtype;
 
+import java.util.concurrent.ThreadLocalRandom;
 import net.glowstone.entity.GlowPlayer;
+import net.glowstone.inventory.GlowInventorySlot;
+import net.glowstone.util.InventoryUtil;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Projectile;
 import org.bukkit.entity.SpectralArrow;
 import org.bukkit.entity.TippedArrow;
 import org.bukkit.inventory.ItemStack;
@@ -12,24 +14,32 @@ import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionEffect;
 
 public class ItemBow extends ItemTimedUsage {
-    private Class<? extends Projectile> currentArrowType;
+    private static final long TICKS_TO_FULLY_CHARGE = 20;
+    private static final double MAX_BASE_DAMAGE = 9;
+    private static final double MAX_SPEED = 53;
+    private Class<? extends Arrow> currentArrowType;
     private PotionMeta currentTippedArrowMeta;
 
     @Override
     public void startUse(GlowPlayer player, ItemStack item) {
         ItemStack arrow = null;
-        findArrow: for (ItemStack itemStack : player.getInventory().getContents()) {
+        GlowInventorySlot arrowSlot = null;
+        findArrow: for (GlowInventorySlot slot : player.getInventory().getSlots()) {
+            ItemStack itemStack = arrowSlot.getItem();
             switch (itemStack.getType()) {
                 case ARROW:
                     arrow = itemStack;
+                    arrowSlot = slot;
                     currentArrowType = Arrow.class;
                     break;
                 case SPECTRAL_ARROW:
                     arrow = itemStack;
+                    arrowSlot = slot;
                     currentArrowType = SpectralArrow.class;
                     break findArrow;
                 case TIPPED_ARROW:
                     arrow = itemStack;
+                    arrowSlot = slot;
                     currentArrowType = TippedArrow.class;
                     ItemMeta itemMeta = itemStack.getItemMeta();
                     if (itemMeta instanceof PotionMeta) {
@@ -43,17 +53,22 @@ public class ItemBow extends ItemTimedUsage {
         if (arrow != null) {
             if (currentArrowType != Arrow.class
                     || !item.containsEnchantment(Enchantment.ARROW_INFINITE)) {
-                arrow.setAmount(arrow.getAmount() - 1);
-                // TODO: make atomic, delete if zero
+                // Consume the arrow
+                int amount = arrow.getAmount();
+                if (amount > 1) {
+                    arrow.setAmount(arrow.getAmount() - 1);
+                } else {
+                    arrowSlot.setItem(InventoryUtil.createEmptyStack());
+                }
             }
             player.setUsageItem(item);
-            player.setUsageTime(20);
+            player.setUsageTime(TICKS_TO_FULLY_CHARGE);
         }
     }
 
     @Override
     public void endUse(GlowPlayer player, ItemStack item) {
-        Projectile launchedArrow = player.launchProjectile(currentArrowType);
+        Arrow launchedArrow = player.launchProjectile(currentArrowType);
         if (currentArrowType == TippedArrow.class) {
             TippedArrow launchedTippedArrow = (TippedArrow) launchedArrow;
             launchedTippedArrow.setBasePotionData(currentTippedArrowMeta.getBasePotionData());
@@ -62,6 +77,22 @@ public class ItemBow extends ItemTimedUsage {
                 launchedTippedArrow.addCustomEffect(effect, true);
             }
             currentTippedArrowMeta = null;
+        }
+        double chargeFraction = Math.max(0.0,
+                1.0 - (TICKS_TO_FULLY_CHARGE - player.getUsageTime()) / TICKS_TO_FULLY_CHARGE);
+        double damage = MAX_BASE_DAMAGE * chargeFraction
+                * (1 + 0.25 * item.getEnchantmentLevel(Enchantment.ARROW_DAMAGE));
+        launchedArrow.setVelocity(player.getEyeLocation().getDirection().normalize().multiply(
+                chargeFraction * MAX_SPEED));
+        launchedArrow.spigot().setDamage(damage);
+        if (item.containsEnchantment(Enchantment.ARROW_FIRE)) {
+            // Arrow will burn as long as it's in flight, unless extinguished by water
+            launchedArrow.setFireTicks(Integer.MAX_VALUE);
+        }
+        launchedArrow.setKnockbackStrength(item.getEnchantmentLevel(Enchantment.ARROW_KNOCKBACK));
+        // 20% crit chance
+        if (ThreadLocalRandom.current().nextDouble() < 0.2) {
+            launchedArrow.setCritical(true);
         }
         player.setUsageItem(null);
         player.setUsageTime(0);
--- src/main/java/net/glowstone/entity/projectile/GlowArrow.java
+++ src/main/java/net/glowstone/entity/projectile/GlowArrow.java
@@ -4,17 +4,25 @@ import lombok.Getter;
 import lombok.Setter;
 import net.glowstone.entity.meta.MetadataIndex;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.Dispenser;
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.projectiles.ProjectileSource;
 import org.bukkit.util.Vector;
 
 public class GlowArrow extends GlowProjectile implements Arrow {
 
     private volatile PickupStatus customPickupStatus = null;
     private final Arrow.Spigot spigot = new GlowArrow.Spigot();
+    @Getter @Setter private int knockbackStrength;
+    /**
+     * How long an entity burns after being shot with a burning arrow.
+     */
+    private static final int TARGET_BURN_TICKS = 100;
 
     public GlowArrow(Location location) {
         super(location);
@@ -27,6 +35,13 @@ public class GlowArrow extends GlowProjectile implements Arrow {
         if (!isOnGround()) {
             setVelocity(getVelocity().subtract(new Vector(0, 0.05, 0)));
             location.add(getVelocity());
+            if (isTouchingMaterial(Material.WATER)
+                    || isTouchingMaterial(Material.STATIONARY_WATER)) {
+                setFireTicks(0);
+            } else if (isTouchingMaterial(Material.LAVA)
+                    || isTouchingMaterial(Material.STATIONARY_LAVA)) {
+                setFireTicks(Integer.MAX_VALUE);
+            }
         }
     }
 
@@ -41,28 +56,31 @@ public class GlowArrow extends GlowProjectile implements Arrow {
 
     @Override
     public void collide(Block block) {
-        // No-op: arrows don't despawn on hitting a block
+        setFireTicks(0); // Arrows stop burning when they land and don't ignite blocks
+        // Don't despawn
     }
 
     @Override
     public void collide(LivingEntity entity) {
-        entity.damage(getShooter() instanceof Dispenser ? 1 : 6);
+        double damage = spigot.getDamage();
+        ProjectileSource shooter = getShooter();
+        if (isCritical()) {
+            damage += 1.0;
+        }
+        entity.damage(damage, shooter instanceof Entity ? (Entity) shooter : null,
+                EntityDamageEvent.DamageCause.PROJECTILE);
+        // Burning arrow ignites target, but doesn't stack if target is already on fire.
+        if (getFireTicks() > 0 && entity.getFireTicks() < TARGET_BURN_TICKS) {
+            entity.setFireTicks(TARGET_BURN_TICKS);
+        }
+        // TODO: knockback
+        entity.setArrowsStuck(entity.getArrowsStuck() + 1);
         remove();
     }
 
     @Override
     protected int getObjectId() {
         return 60;
-    }
-
-    @Override
-    public int getKnockbackStrength() {
-        return 0;
-    }
-
-    @Override
-    public void setKnockbackStrength(int strength) {
-
     }
 
     @Override
