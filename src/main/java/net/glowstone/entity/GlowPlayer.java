9a79e89adf883ca632984944e03e0a6f4b866283
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-12-22 16:29:14 -0800

    Remove unused import

--- src/main/java/net/glowstone/entity/GlowPlayer.java
+++ src/main/java/net/glowstone/entity/GlowPlayer.java
@@ -24,7 +24,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.SortedSet;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.ThreadLocalRandom;
