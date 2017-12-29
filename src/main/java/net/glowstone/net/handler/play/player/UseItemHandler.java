b6fd1c550dbb3db956fbe56357f16d77a91515d5
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-12-28 19:33:32 -0800

    Fix bad merge of UseItemHandler

--- src/main/java/net/glowstone/net/handler/play/player/UseItemHandler.java
+++ src/main/java/net/glowstone/net/handler/play/player/UseItemHandler.java
@@ -1,32 +1,37 @@
 package net.glowstone.net.handler.play.player;
 
 import com.flowpowered.network.MessageHandler;
+import net.glowstone.EventFactory;
 import net.glowstone.block.ItemTable;
 import net.glowstone.block.itemtype.ItemProjectile;
 import net.glowstone.block.itemtype.ItemTimedUsage;
 import net.glowstone.block.itemtype.ItemType;
+import net.glowstone.block.itemtype.ItemType.Context;
+import net.glowstone.entity.GlowPlayer;
 import net.glowstone.net.GlowSession;
 import net.glowstone.net.message.play.player.UseItemMessage;
 import net.glowstone.util.InventoryUtil;
-
 import org.bukkit.GameMode;
 import org.bukkit.Material;
-import org.bukkit.entity.Projectile;
 import org.bukkit.event.Event;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class UseItemHandler implements MessageHandler<GlowSession, UseItemMessage> {
+
     @Override
     public void handle(GlowSession session, UseItemMessage message) {
-        ItemStack holding = session.getPlayer().getInventory().getItemInMainHand();
-        if (message.getHand() == UseItemMessage.OFF_HAND) {
-            holding = session.getPlayer().getInventory().getItemInOffHand();
-        }
-        if (holding == null) {
+        GlowPlayer player = session.getPlayer();
+        ItemStack holding = InventoryUtil
+                .itemOrEmpty(player.getInventory().getItem(message.getEquipmentSlot()));
+
+        PlayerInteractEvent event = EventFactory
+                .onPlayerInteract(player, Action.RIGHT_CLICK_AIR, message.getEquipmentSlot());
+        if (event.useItemInHand() == null || event.useItemInHand() == Event.Result.DENY) {
             return;
         }
+
         if (!InventoryUtil.isEmpty(holding)) {
             ItemType type = ItemTable.instance().getItem(holding.getType());
             if (type != null) {
@@ -34,11 +39,11 @@ public class UseItemHandler implements MessageHandler<GlowSession, UseItemMessag
                     type.rightClickAir(player, holding);
                 } else {
                     if ((holding.getType() == Material.WATER_BUCKET
-                        || holding.getType() == Material.LAVA_BUCKET) 
+                            || holding.getType() == Material.LAVA_BUCKET)
                             && player.getGameMode() != GameMode.CREATIVE) {
                         holding.setType(Material.BUCKET);
                     } else if (type instanceof ItemTimedUsage) {
-            	        ((ItemTimedUsage) type).startUse(session.getPlayer(), holding);
+                        ((ItemTimedUsage) type).startUse(session.getPlayer(), holding);
                     } else if (type instanceof ItemProjectile) {
                         ((ItemProjectile) type).use(session.getPlayer(), holding);
                     }
--- src/main/java/net/glowstone/net/message/play/player/UseItemMessage.java
+++ src/main/java/net/glowstone/net/message/play/player/UseItemMessage.java
@@ -2,12 +2,14 @@ package net.glowstone.net.message.play.player;
 
 import com.flowpowered.network.Message;
 import lombok.Data;
+import org.bukkit.inventory.EquipmentSlot;
 
 @Data
 public class UseItemMessage implements Message {
 
-    public static final int MAIN_HAND = 0;
-    public static final int OFF_HAND = 1;
-
     private final int hand;
+
+    public EquipmentSlot getEquipmentSlot() {
+        return hand == 1 ? EquipmentSlot.OFF_HAND : EquipmentSlot.HAND;
+    }
 }
