fd2245968eaf1aa0eb6b4f7565b5de4fcbbe92a8
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-12-29 01:31:23 -0800

    Refactor: arrow is spent at END of firing

--- src/main/java/net/glowstone/block/itemtype/ItemBow.java
+++ src/main/java/net/glowstone/block/itemtype/ItemBow.java
@@ -4,12 +4,13 @@ import java.util.concurrent.ThreadLocalRandom;
 import net.glowstone.entity.GlowPlayer;
 import net.glowstone.inventory.GlowInventorySlot;
 import net.glowstone.util.InventoryUtil;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.SpectralArrow;
 import org.bukkit.entity.TippedArrow;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionEffect;
 
@@ -17,50 +18,26 @@ public class ItemBow extends ItemTimedUsage {
     private static final long TICKS_TO_FULLY_CHARGE = 20;
     private static final double MAX_BASE_DAMAGE = 9;
     private static final double MAX_SPEED = 53;
-    private Class<? extends Arrow> currentArrowType;
-    private PotionMeta currentTippedArrowMeta;
+    private ItemStack currentArrow;
 
     @Override
     public void startUse(GlowPlayer player, ItemStack item) {
-        ItemStack arrow = null;
-        GlowInventorySlot arrowSlot = null;
-        findArrow: for (GlowInventorySlot slot : player.getInventory().getSlots()) {
-            ItemStack itemStack = slot.getItem();
+        findArrow: for (ItemStack itemStack : player.getInventory().getContents()) {
             switch (itemStack.getType()) {
                 case ARROW:
-                    arrow = itemStack;
-                    arrowSlot = slot;
-                    currentArrowType = Arrow.class;
+                    if (currentArrow == null) {
+                        currentArrow = itemStack;
+                    }
                     break;
                 case SPECTRAL_ARROW:
-                    arrow = itemStack;
-                    arrowSlot = slot;
-                    currentArrowType = SpectralArrow.class;
-                    break findArrow;
                 case TIPPED_ARROW:
-                    arrow = itemStack;
-                    arrowSlot = slot;
-                    currentArrowType = TippedArrow.class;
-                    ItemMeta itemMeta = itemStack.getItemMeta();
-                    if (itemMeta instanceof PotionMeta) {
-                        currentTippedArrowMeta = (PotionMeta) itemMeta;
-                    }
+                    currentArrow = itemStack;
                     break findArrow;
                 default:
                     // do nothing
             }
         }
-        if (arrow != null) {
-            if (currentArrowType != Arrow.class
-                    || !item.containsEnchantment(Enchantment.ARROW_INFINITE)) {
-                // Consume the arrow
-                int amount = arrow.getAmount();
-                if (amount > 1) {
-                    arrow.setAmount(amount - 1);
-                } else {
-                    arrowSlot.setItem(InventoryUtil.createEmptyStack());
-                }
-            }
+        if (currentArrow != null) {
             player.setUsageItem(item);
             player.setUsageTime(TICKS_TO_FULLY_CHARGE);
         }
@@ -68,35 +45,64 @@ public class ItemBow extends ItemTimedUsage {
 
     @Override
     public void endUse(GlowPlayer player, ItemStack item) {
-        Arrow launchedArrow = player.launchProjectile(currentArrowType);
-        if (currentArrowType == TippedArrow.class) {
-            TippedArrow launchedTippedArrow = (TippedArrow) launchedArrow;
-            launchedTippedArrow.setBasePotionData(currentTippedArrowMeta.getBasePotionData());
-            launchedTippedArrow.setColor(currentTippedArrowMeta.getColor());
-            for (PotionEffect effect : currentTippedArrowMeta.getCustomEffects()) {
-                launchedTippedArrow.addCustomEffect(effect, true);
+        Arrow launchedArrow = null;
+        boolean consumeArrow = (player.getGameMode() != GameMode.CREATIVE);
+        Material arrowType = currentArrow.getType();
+        switch (arrowType) {
+            case ARROW:
+                if (item.containsEnchantment(Enchantment.ARROW_INFINITE)) {
+                    consumeArrow = false;
+                }
+                launchedArrow = player.launchProjectile(Arrow.class);
+                break;
+            case TIPPED_ARROW:
+                launchedArrow = player.launchProjectile(TippedArrow.class);
+                TippedArrow launchedTippedArrow = (TippedArrow) launchedArrow;
+                PotionMeta meta = (PotionMeta) currentArrow.getItemMeta();
+                launchedTippedArrow.setBasePotionData(meta.getBasePotionData());
+                launchedTippedArrow.setColor(meta.getColor());
+                for (PotionEffect effect : meta.getCustomEffects()) {
+                    launchedTippedArrow.addCustomEffect(effect, true);
+                }
+                break;
+            case SPECTRAL_ARROW:
+                launchedArrow = player.launchProjectile(SpectralArrow.class);
+                break;
+            default:
+                player.getServer().getLogger().severe(String.format("Attempt to fire a %s from a bow",
+                        arrowType));
+
+        }
+        if (launchedArrow != null) {
+            if (consumeArrow) {
+                int amount = currentArrow.getAmount();
+                if (amount <= 1) {
+                    player.getInventory().remove(currentArrow);
+                } else {
+                    currentArrow.setAmount(amount - 1);
+                }
             }
-            currentTippedArrowMeta = null;
-        }
-        double chargeFraction = Math.max(0.0,
-                1.0 - (TICKS_TO_FULLY_CHARGE - player.getUsageTime()) / TICKS_TO_FULLY_CHARGE);
-        double damage = MAX_BASE_DAMAGE * chargeFraction
-                * (1 + 0.25 * item.getEnchantmentLevel(Enchantment.ARROW_DAMAGE));
-        launchedArrow.setVelocity(launchedArrow.getVelocity().normalize().multiply(
-                chargeFraction * MAX_SPEED));
-        launchedArrow.spigot().setDamage(damage);
-        if (item.containsEnchantment(Enchantment.ARROW_FIRE)) {
-            // Arrow will burn as long as it's in flight, unless extinguished by water
-            launchedArrow.setFireTicks(Integer.MAX_VALUE);
-        }
-        launchedArrow.setKnockbackStrength(item.getEnchantmentLevel(Enchantment.ARROW_KNOCKBACK));
-        // 20% crit chance
-        if (ThreadLocalRandom.current().nextDouble() < 0.2) {
-            launchedArrow.setCritical(true);
+            double chargeFraction = Math.max(0.0,
+                    1.0 - (TICKS_TO_FULLY_CHARGE - player.getUsageTime()) / TICKS_TO_FULLY_CHARGE);
+            double damage = MAX_BASE_DAMAGE * chargeFraction
+                    * (1 + 0.25 * item.getEnchantmentLevel(Enchantment.ARROW_DAMAGE));
+            launchedArrow.setVelocity(launchedArrow.getVelocity().normalize().multiply(
+                    chargeFraction * MAX_SPEED));
+            launchedArrow.spigot().setDamage(damage);
+            if (item.containsEnchantment(Enchantment.ARROW_FIRE)) {
+                // Arrow will burn as long as it's in flight, unless extinguished by water
+                launchedArrow.setFireTicks(Integer.MAX_VALUE);
+            }
+            launchedArrow
+                    .setKnockbackStrength(item.getEnchantmentLevel(Enchantment.ARROW_KNOCKBACK));
+            // 20% crit chance
+            if (ThreadLocalRandom.current().nextDouble() < 0.2) {
+                launchedArrow.setCritical(true);
+            }
         }
         InventoryUtil.damageItem(player, item);
         player.setUsageItem(null);
         player.setUsageTime(0);
-        currentArrowType = null;
+        currentArrow = null;
     }
 }
