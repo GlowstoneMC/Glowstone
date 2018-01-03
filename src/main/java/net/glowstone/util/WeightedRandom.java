6228f19f1466a87489e5b5e9c1a4511aebcd99be
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-01-02 22:52:40 -0800

    Fix CheckStyle issues in util/WeightedRandom.java (#755)

--- src/main/java/net/glowstone/util/WeightedRandom.java
+++ src/main/java/net/glowstone/util/WeightedRandom.java
@@ -4,6 +4,14 @@ import java.util.Random;
 
 public final class WeightedRandom {
 
+    /**
+     * Selects a random item from a weighted distribution.
+     *
+     * @param random the PRNG to use
+     * @param possibilities the distribution to sample from
+     * @param <T> the type of each of the {@code possibilities}
+     * @return a random item
+     */
     public static <T extends Choice> T getRandom(Random random, Iterable<T> possibilities) {
         int weights = 0;
         for (T possibility : possibilities) {
@@ -18,10 +26,10 @@ public final class WeightedRandom {
                 return t;
             }
         }
-
         return null;
     }
 
+    @FunctionalInterface
     public interface Choice {
 
         int getWeight();
