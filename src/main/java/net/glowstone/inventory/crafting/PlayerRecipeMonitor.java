4389f4f798636707e8dc07b7da657afbc9a414c4
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-01-02 17:35:45 -0800

    Fix CheckStyle issues in inventory/crafting/PlayerRecipeMonitor.java (#756)

--- src/main/java/net/glowstone/inventory/crafting/PlayerRecipeMonitor.java
+++ src/main/java/net/glowstone/inventory/crafting/PlayerRecipeMonitor.java
@@ -13,10 +13,16 @@ import net.glowstone.util.nbt.TagType;
 public final class PlayerRecipeMonitor {
 
     private final GlowPlayer player;
-    private final Set<String> recipes, toBeDisplayed;
+    private final Set<String> recipes;
+    private final Set<String> toBeDisplayed;
     private boolean bookOpen;
     private boolean filterCraftable;
 
+    /**
+     * Creates an instance associated with the given player and with an empty recipe book.
+     *
+     * @param player the player
+     */
     public PlayerRecipeMonitor(GlowPlayer player) {
         this.player = player;
         this.bookOpen = false;
@@ -25,6 +31,11 @@ public final class PlayerRecipeMonitor {
         this.toBeDisplayed = new HashSet<>();
     }
 
+    /**
+     * Creates a message to send this recipe book's state to the client.
+     *
+     * @return an {@link UnlockRecipesMessage} containing this recipe book's state
+     */
     public UnlockRecipesMessage createInitMessage() {
         int status = UnlockRecipesMessage.ACTION_INIT;
         int[] recipeIds = new int[0]; // todo: conversion to internal IDs
@@ -33,6 +44,11 @@ public final class PlayerRecipeMonitor {
             recipeIds);
     }
 
+    /**
+     * Restores state from an NBT tag.
+     *
+     * @param playerData an NBT tag containing a compound subtag named recipeBook
+     */
     public void read(CompoundTag playerData) {
         if (!playerData.isCompound("recipeBook")) {
             return;
@@ -52,6 +68,11 @@ public final class PlayerRecipeMonitor {
         }
     }
 
+    /**
+     * Populates a recipeBook compound subtag and adds it to the given tag.
+     *
+     * @param playerData a compound tag describing the player
+     */
     public void write(CompoundTag playerData) {
         CompoundTag recipeBook = new CompoundTag();
         recipeBook.putBool("isFilteringCraftable", filterCraftable);
