f4f32c7a004bdc8093950c10b82800536837c90c
mastercoms <mastercoms@tutanota.de>
2018-01-07 13:01:59 -0500

    Fix script directories and add documentation

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-# Setup script for Glowstone
+# Run script within the directory
+BINDIR=$(dirname "$(readlink -fn "$0")")
+cd "$BINDIR"
+
+# Build Glowstone
 (cd .. && mvn -T 1C -B package)
