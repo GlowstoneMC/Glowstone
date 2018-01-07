6aec6f98ef6571e8a52f5a8adfd3c8516b31c775
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-01-06 18:36:08 -0800

    Droppers no longer act like Dispensers

--- src/main/java/net/glowstone/block/entity/state/GlowDropper.java
+++ src/main/java/net/glowstone/block/entity/state/GlowDropper.java
@@ -1,9 +1,14 @@
 package net.glowstone.block.entity.state;
 
 import net.glowstone.block.GlowBlock;
+import net.glowstone.dispenser.DefaultDispenseBehavior;
+import net.glowstone.dispenser.DispenseBehavior;
+import org.bukkit.Material;
 import org.bukkit.block.Dropper;
 
 public class GlowDropper extends GlowDispenser implements Dropper {
+
+    private static final DispenseBehavior JUST_DROP_THE_ITEM = new DefaultDispenseBehavior();
 
     public GlowDropper(GlowBlock block) {
         super(block);
@@ -15,8 +20,7 @@ public class GlowDropper extends GlowDispenser implements Dropper {
     }
 
     @Override
-    public boolean dispense() {
-        // todo: drop, not dispense
-        return super.dispense();
+    protected DispenseBehavior getDispenseBehavior(Material itemType) {
+        return JUST_DROP_THE_ITEM;
     }
 }
