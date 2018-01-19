30b28fd0ebe334f1d4f47ac74bc4894f30aa96a5
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-01-19 13:37:44 -0800

    Unused import

--- src/main/java/net/glowstone/entity/GlowLivingEntity.java
+++ src/main/java/net/glowstone/entity/GlowLivingEntity.java
@@ -58,7 +58,6 @@ import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Fireball;
