298976bc8c51e496f6db7eb480007309081f885f
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-01-19 20:59:29 -0800

    Unused import

--- src/main/java/net/glowstone/entity/passive/GlowBat.java
+++ src/main/java/net/glowstone/entity/passive/GlowBat.java
@@ -3,7 +3,6 @@ package net.glowstone.entity.passive;
 import com.flowpowered.network.Message;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.UUID;
 import net.glowstone.entity.GlowAmbient;
 import net.glowstone.entity.meta.MetadataIndex;
 import net.glowstone.net.message.play.entity.EntityHeadRotationMessage;
