e084ecac25dadbe11268a16d6ca4b8a20cf8c9e7
momothereal <momothereal.mc@gmail.com>
2018-01-21 16:30:54 -0500

    Cleanup GlowSlime constructor

--- src/main/java/net/glowstone/entity/monster/GlowSlime.java
+++ src/main/java/net/glowstone/entity/monster/GlowSlime.java
@@ -18,29 +18,11 @@ public class GlowSlime extends GlowMonster implements Slime {
 
     protected GlowSlime(Location loc, EntityType type) {
         super(loc, type, 1);
-        byte size = 1;
-        double health = 1;
-        int randomSize = ThreadLocalRandom.current().nextInt(3);
-        switch (randomSize) {
-            case 0:
-                size = 1;
-                health = 1;
-                break;
-            case 1:
-                size = 2;
-                health = 4;
-                break;
-            case 2:
-                size = 4;
-                health = 16;
-                break;
-            default:
-                throw new InternalError("ThreadLocalRandom.nextInt(3) returned " + randomSize);
-        }
+        int size = 1 + ThreadLocalRandom.current().nextInt(3);
+        setMaxHealth(size * size); // max health = size^2
+        setHealth(maxHealth); // reset health to max
         setBoundingBox(0.51000005 * size, 0.51000005 * size);
         setSize(size);
-        setMaxHealth(health);
-        setHealth(health);
     }
 
     @Override
