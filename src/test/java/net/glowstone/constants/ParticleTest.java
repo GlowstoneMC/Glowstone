42932d491eebb0e82b95e1fe308d840bfaed894f
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-02-04 16:32:50 -0800

    Unused import

--- src/test/java/net/glowstone/constants/ParticleTest.java
+++ src/test/java/net/glowstone/constants/ParticleTest.java
@@ -4,7 +4,6 @@ import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.material.MaterialData;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
