0ca13f945231fff9c430fa0250f0d7bb59b3f205
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-02-04 17:37:59 -0800

    Revert "Change return type to Stream"
    This reverts commit 4397f1266b7b5721b574bc094add830861d9595d.

--- src/test/java/net/glowstone/constants/ParticleTest.java
+++ src/test/java/net/glowstone/constants/ParticleTest.java
@@ -6,6 +6,8 @@ import org.bukkit.material.MaterialData;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -18,11 +20,12 @@ public class ParticleTest {
 
     private static final MaterialData STONE = new MaterialData(Material.STONE, (byte) 1);
 
-    public static Stream<Effect> getCases() {
+    public static Collection<Effect> getCases() {
         try {
             return Stream.of(Effect.values())
                     .parallel()
-                    .filter(effect -> effect.getType() == Effect.Type.PARTICLE);
+                    .filter(effect -> effect.getType() == Effect.Type.PARTICLE)
+                    .collect(Collectors.toList());
         } finally {
             // Necessary due to https://github.com/junit-team/junit5/issues/1283
             System.err.println();
