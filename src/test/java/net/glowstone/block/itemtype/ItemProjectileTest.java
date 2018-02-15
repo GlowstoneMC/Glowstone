2d99fbec0b2628b907e62f78a298d2cf62062f22
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-02-14 21:52:17 -0800

    Unused imports

--- src/test/java/net/glowstone/block/itemtype/ItemProjectileTest.java
+++ src/test/java/net/glowstone/block/itemtype/ItemProjectileTest.java
@@ -1,23 +1,16 @@
 package net.glowstone.block.itemtype;
 
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import lombok.RequiredArgsConstructor;
-import net.glowstone.block.GlowBlock;
 import org.bukkit.Material;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.Vector;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
 /**
  * Superclass of tests of {@link ItemProjectile} subclasses.
