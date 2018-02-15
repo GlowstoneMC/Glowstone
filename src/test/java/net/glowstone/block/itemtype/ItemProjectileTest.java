578361d26c9122e3ef05e1b450258b39be3a1e96
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-02-14 21:39:40 -0800

    Fix projectile tests

--- src/test/java/net/glowstone/block/itemtype/ItemProjectileTest.java
+++ src/test/java/net/glowstone/block/itemtype/ItemProjectileTest.java
@@ -6,18 +6,24 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import lombok.RequiredArgsConstructor;
+import net.glowstone.block.GlowBlock;
 import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 /**
  * Superclass of tests of {@link ItemProjectile} subclasses.
  *
  * @param <T> the projectile class that we expect will be passed to
- *         {@link Player#launchProjectile(Class)}
+ *            {@link Player#launchProjectile(Class)}
  */
 @RequiredArgsConstructor
 public abstract class ItemProjectileTest<T extends Projectile> extends ItemTypeTest {
@@ -26,14 +32,13 @@ public abstract class ItemProjectileTest<T extends Projectile> extends ItemTypeT
     protected final Class<T> projectileClass;
 
     @Test
-    public void testUse() {
+    public void testRightClickAir() {
         ItemStack itemStack = new ItemStack(type, 1);
         inventory.setItemInMainHand(itemStack);
         T projectile = Mockito.mock(projectileClass);
         when(player.launchProjectile(projectileClass)).thenReturn(projectile);
-        item.use(player, itemStack);
+        item.rightClickAir(player, itemStack);
         verify(player, times(1)).launchProjectile(projectileClass);
-        // FIXME: assertEmpty(inventory.getItemInMainHand());
+        assertEmpty(inventory.getItemInMainHand());
     }
-
 }
