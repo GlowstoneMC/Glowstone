b6d1be4191d82b0a51ed85d2095c30f29dd157be
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-02-24 11:17:47 -0800

    Unused imports

--- src/test/java/net/glowstone/entity/passive/GlowFishingHookTest.java
+++ src/test/java/net/glowstone/entity/passive/GlowFishingHookTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.argThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Matchers.intThat;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -14,26 +13,20 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 import net.glowstone.EventFactory;
 import net.glowstone.block.GlowBlock;
-import net.glowstone.entity.GlowEntityTest;
 import net.glowstone.entity.FishingRewardManager;
+import net.glowstone.entity.GlowEntityTest;
 import net.glowstone.entity.GlowPlayer;
 import net.glowstone.entity.monster.GlowCreeper;
-import net.glowstone.scheduler.GlowScheduler;
-import net.glowstone.testutils.ServerShim;
 import net.glowstone.util.InventoryUtil;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.inventory.ItemStack;
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.internal.matchers.GreaterThan;
 import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 
 public class GlowFishingHookTest extends GlowEntityTest<GlowFishingHook> {
 
