6635c5673f8a702fae02a695433ab075c8e4cc2b
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-03-09 16:45:05 -0800

    Bugfix: test ctor needs to be public

--- src/test/java/net/glowstone/entity/passive/GlowCowTest.java
+++ src/test/java/net/glowstone/entity/passive/GlowCowTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 public class GlowCowTest extends GlowAgeableTest<GlowCow> {
 
-    protected GlowCowTest() {
+    public GlowCowTest() {
         super(GlowCow::new);
     }
 
