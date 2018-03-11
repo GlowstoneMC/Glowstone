049a46370a514b04d282888cefcaaadd13c574d0
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-03-10 18:35:57 -0800

    Bug fixes

--- src/test/java/net/glowstone/entity/GlowHumanEntityTest.java
+++ src/test/java/net/glowstone/entity/GlowHumanEntityTest.java
@@ -3,7 +3,7 @@ package net.glowstone.entity;
 import java.util.function.Function;
 import org.bukkit.Location;
 
-public class GlowHumanEntityTest<T extends GlowHumanEntity> extends GlowLivingEntityTest<T> {
+public abstract class GlowHumanEntityTest<T extends GlowHumanEntity> extends GlowLivingEntityTest<T> {
     protected GlowHumanEntityTest(
             Function<Location, ? extends T> entityCreator) {
         super(entityCreator);
