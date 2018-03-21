2b50105cd5a1078d255d768f694c470eb004c429
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-03-20 17:37:17 -0700

    Blaze test, with test of setOnFire

---
+++ src/test/java/net/glowstone/entity/monster/GlowBlazeTest.java
@@ -0,0 +1,24 @@
+package net.glowstone.entity.monster;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class GlowBlazeTest extends net.glowstone.entity.monster.GlowMonsterTest<GlowBlaze> {
+
+    public GlowBlazeTest() {
+        super(GlowBlaze::new);
+    }
+
+    @Test
+    void setOnFire() {
+        entity.setOnFire(true);
+        assertTrue(entity.isOnFire());
+    }
+
+    @Test
+    void setNotOnFire() {
+        entity.setOnFire(false);
+        assertFalse(entity.isOnFire());
+    }
+}
