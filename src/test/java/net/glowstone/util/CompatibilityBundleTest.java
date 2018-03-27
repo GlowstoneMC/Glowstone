ec59a982ede69f5307c9901d652c47ceebb78e45
smartboyathome <smartboyathome@gmail.com>
2018-03-26 18:49:59 -0700

    Add 'compatibility bundles' to allow runtime library upgrades (#879)

---
+++ src/test/java/net/glowstone/util/CompatibilityBundleTest.java
@@ -0,0 +1,31 @@
+package net.glowstone.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompatibilityBundleTest {
+    @Test
+    public void fromConfigTest() {
+        assertConfigValueMatchesBundle(null, CompatibilityBundle.CRAFTBUKKIT);
+        assertConfigValueMatchesBundle("", CompatibilityBundle.CRAFTBUKKIT);
+        assertConfigValueMatchesBundle("     ", CompatibilityBundle.CRAFTBUKKIT);
+
+        assertConfigValueMatchesBundle("CRAFTBUKKIT", CompatibilityBundle.CRAFTBUKKIT);
+        assertConfigValueMatchesBundle("craftbukkit", CompatibilityBundle.CRAFTBUKKIT);
+        assertConfigValueMatchesBundle("cRaFtBuKkIt", CompatibilityBundle.CRAFTBUKKIT);
+
+        assertConfigValueMatchesBundle("NONE", CompatibilityBundle.NONE);
+        assertConfigValueMatchesBundle("none", CompatibilityBundle.NONE);
+        assertConfigValueMatchesBundle("NoNe", CompatibilityBundle.NONE);
+
+        assertConfigValueMatchesBundle("unknown", null);
+        assertConfigValueMatchesBundle(" CRAFTBUKKIT ", null);
+        assertConfigValueMatchesBundle("NONE.", null);
+    }
+
+    private void assertConfigValueMatchesBundle(String configValue, CompatibilityBundle expected) {
+        CompatibilityBundle actual = CompatibilityBundle.fromConfig(configValue);
+        assertEquals(expected, actual);
+    }
+}
