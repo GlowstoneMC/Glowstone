a2d6f2b28b857d8c9c7d4c6431d90916c4f01e2a
Florent Clarret <florent.clarret@gmail.com>
2018-04-17 12:18:39 +0200

    feat(breed): feed the animals

--- src/main/java/net/glowstone/entity/GlowAnimal.java
+++ src/main/java/net/glowstone/entity/GlowAnimal.java
@@ -1,10 +1,17 @@
 package net.glowstone.entity;
 
+import java.util.EnumSet;
+import java.util.Set;
 import net.glowstone.entity.ai.EntityDirector;
 import net.glowstone.entity.ai.MobState;
+import net.glowstone.net.message.play.player.InteractEntityMessage;
+import net.glowstone.util.InventoryUtil;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Represents an Animal, such as a Cow.
@@ -30,5 +37,37 @@ public class GlowAnimal extends GlowAgeable implements Animals {
     @Override
     protected int getAmbientDelay() {
         return 120;
+    }
+
+    @Override
+    public boolean entityInteract(GlowPlayer player, InteractEntityMessage message) {
+        super.entityInteract(player, message);
+
+        if (message.getAction() == InteractEntityMessage.Action.INTERACT.ordinal()) {
+            ItemStack item = InventoryUtil
+                    .itemOrEmpty(player.getInventory().getItem(message.getHandSlot()));
+
+            if (player.getGameMode().equals(GameMode.SPECTATOR)
+                    || InventoryUtil.isEmpty(item)) {
+                return false;
+            }
+
+            if (GameMode.CREATIVE != player.getGameMode()
+                    && getBreedingFood().contains(item.getType())) {
+                // TODO set love mode if possible and spawn particles
+                // TODO heal
+                player.getInventory().consumeItemInMainHand();
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns a set containing the breeding foods for the current animal.
+     * @return a set containing Material
+     */
+    public Set<Material> getBreedingFood() {
+        return EnumSet.noneOf(Material.class);
     }
 }
--- src/test/java/net/glowstone/entity/GlowAnimalTest.java
+++ src/test/java/net/glowstone/entity/GlowAnimalTest.java
@@ -1,11 +1,21 @@
 package net.glowstone.entity;
 
+import java.util.EnumSet;
 import java.util.function.Function;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public abstract class GlowAnimalTest<T extends GlowAnimal> extends GlowAgeableTest<T> {
     protected GlowAnimalTest(
             Function<Location, ? extends T> entityCreator) {
         super(entityCreator);
+    }
+
+    @Test
+    public void testGetBreedingFood() {
+        assertEquals(EnumSet.noneOf(Material.class), entity.getBreedingFood());
     }
 }
