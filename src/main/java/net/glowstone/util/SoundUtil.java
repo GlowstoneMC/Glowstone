e701289c01460728d1de650743610dc90b50b4cb
momothereal <momothereal.mc@gmail.com>
2018-04-29 13:32:27 -0400

    Enhance documentation for SoundUtil class

--- src/main/java/net/glowstone/util/SoundUtil.java
+++ src/main/java/net/glowstone/util/SoundUtil.java
@@ -1,6 +1,9 @@
 package net.glowstone.util;
 
-import java.util.Arrays;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.collect.Sets;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 import net.glowstone.GlowWorld;
 import net.glowstone.entity.GlowPlayer;
@@ -8,40 +11,49 @@ import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
 
+/**
+ * A collection of utility methods to play sounds.
+ */
 public class SoundUtil {
 
     /**
-     * Plays a sound with a random pitch, but excludes one player from hearing it.
+     * Plays a sound with a random pitch, but excludes specified players from hearing it.
      *
      * @param location the sound location
-     * @param sound the sound to play
-     * @param volume the volume multiplier
-     * @param pitch the pitch modifier
-     * @param exclude the player not to play the sound for
+     * @param sound    the sound to play
+     * @param volume   the volume multiplier
+     * @param pitch    the pitch modifier
+     * @param exclude  the players not to play the sound for
+     * @throws NullPointerException if any of the location, sound, or exclude parameters is null.
      */
     public static void playSoundAtLocationExcept(Location location, Sound sound, float volume,
             float pitch, GlowPlayer... exclude) {
-        if (location == null || sound == null) {
-            return;
-        }
+        checkNotNull(location);
+        checkNotNull(sound);
+        checkNotNull(exclude);
+
+        Set<GlowPlayer> excludeSet = Sets.newHashSet(exclude);
         GlowWorld world = (GlowWorld) location.getWorld();
         double radiusSquared = volume * volume * 256;
-        world.getRawPlayers().stream().filter(player ->
-                player.getLocation().distanceSquared(location) <= radiusSquared
-                        && !Arrays.asList(exclude).contains(player))
+        world.getRawPlayers()
+                .stream()
+                .filter(player -> player.getLocation().distanceSquared(location) <= radiusSquared
+                        && !excludeSet.contains(player))
                 .forEach(player -> player.playSound(location, sound, volume, pitch));
     }
 
     /**
-     * Plays a sound with a random pitch, but excludes one player from hearing it.
+     * Plays a sound with a random pitch, but excludes specified players from hearing it.
      *
-     * @param location the sound location
-     * @param sound the sound to play
-     * @param volume the volume multiplier
-     * @param pitchBase if {@code allowNegative}, the average pitch modifier; otherwise, the minimum
-     * @param pitchRange the maximum deviation of the pitch modifier compared to {@code pitchBase}
+     * @param location      the sound location
+     * @param sound         the sound to play
+     * @param volume        the volume multiplier
+     * @param pitchBase     if {@code allowNegative}, the average pitch modifier; otherwise, the
+     *                      minimum
+     * @param pitchRange    the maximum deviation of the pitch modifier compared to {@code
+     * pitchBase}
      * @param allowNegative if true, distribution is triangular rather than uniform
-     * @param exclude the player not to play the sound for
+     * @param exclude       the players not to play the sound for
      */
     public static void playSoundPitchRange(Location location, Sound sound, float volume,
             float pitchBase, float pitchRange, boolean allowNegative, GlowPlayer... exclude) {
@@ -55,14 +67,30 @@ public class SoundUtil {
         playSoundAtLocationExcept(location, sound, volume, pitch, exclude);
     }
 
+    /**
+     * Plays a sound with a random pitch, but excludes specified players from hearing it.
+     *
+     * @param location   the sound location
+     * @param sound      the sound to play
+     * @param volume     the volume multiplier
+     * @param pitchBase  the average pitch modifier.
+     * @param pitchRange the maximum deviation of the pitch modifier compared to {@code pitchBase}
+     * @param exclude    the players not to play the sound for
+     */
     public static void playSoundPitchRange(Location location, Sound sound, float volume,
             float pitchBase, float pitchRange, GlowPlayer... exclude) {
         playSoundPitchRange(location, sound, volume, pitchBase, pitchRange, true, exclude);
     }
 
+    /**
+     * Generates a random float between {@code -range} and {@code range}.
+     *
+     * @param range the bounds of the random float.
+     * @return A randomly generated float.
+     */
     public static float randomReal(float range) {
         ThreadLocalRandom rand = ThreadLocalRandom.current();
-        return (rand.nextFloat() - rand.nextFloat()) * range;
+        return (2.0F * rand.nextFloat() - 1.0F) * range;
     }
 
     /**
