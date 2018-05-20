bf0ab224590b27dac9b1454e5228df115d37bdbc
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-05-20 09:34:51 -0700

    `LocalizedString.send(CommandSender, Object...)` (#920)
    New method that sends a LocalizedString, possibly populated with args, to a CommandSender. Will be used much more as command responses are i18nized.

--- src/main/java/net/glowstone/i18n/LocalizedString.java
+++ src/main/java/net/glowstone/i18n/LocalizedString.java
@@ -1,7 +1,11 @@
 package net.glowstone.i18n;
 
+import org.bukkit.command.CommandSender;
+
 public interface LocalizedString {
     String get();
 
     String get(Object... args);
+
+    void send(CommandSender recipient, Object... args);
 }
