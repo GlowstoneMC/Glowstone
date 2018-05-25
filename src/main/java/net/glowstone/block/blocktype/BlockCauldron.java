2f16c28ae93be2e188ceef93dbd27bdcd671550a
momothereal <momothereal.mc@gmail.com>
2018-05-25 00:05:52 -0400

    Implement CauldronLevelChangeEvent
    Made failed tests print full stacktrace

--- src/main/java/net/glowstone/block/blocktype/BlockCauldron.java
+++ src/main/java/net/glowstone/block/blocktype/BlockCauldron.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+
+import net.glowstone.EventFactory;
 import net.glowstone.block.GlowBlock;
 import net.glowstone.entity.GlowPlayer;
 import net.glowstone.inventory.GlowItemFactory;
@@ -13,6 +15,8 @@ import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.banner.Pattern;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.block.CauldronLevelChangeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BannerMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
@@ -31,6 +35,8 @@ public class BlockCauldron extends BlockNeedsTool {
         }
 
         switch (player.getItemInHand().getType()) {
+            // todo: filling the cauldron by emptying a bottle
+            // todo: emptying the cauldron by filling a bucket
             case WATER_BUCKET:
                 fillCauldron(player, block);
                 return true;
@@ -54,19 +60,25 @@ public class BlockCauldron extends BlockNeedsTool {
     }
 
     private void fillCauldron(GlowPlayer player, GlowBlock block) {
+        // fired when a player fills the cauldron using a water bucket
         if (block.getData() < 3) {
+            if (!setCauldronLevel(block, 3, player,
+                    CauldronLevelChangeEvent.ChangeReason.BUCKET_EMPTY)) {
+                return;
+            }
             if (player.getGameMode() != GameMode.CREATIVE) {
                 player.getItemInHand().setType(Material.BUCKET);
             }
-
-            block.setData((byte) 3);
         }
     }
 
     private void fillBottle(GlowPlayer player, GlowBlock block) {
+        // fired when a player fills an empty bottle from the cauldron
         if (block.getData() > 0) {
-            block.setData((byte) (block.getData() - 1));
-
+            if (!setCauldronLevel(block, block.getData() - 1, player,
+                    CauldronLevelChangeEvent.ChangeReason.BOTTLE_FILL)) {
+                return;
+            }
             if (player.getGameMode() != GameMode.CREATIVE) {
                 Map<Integer, ItemStack> drops = player.getInventory()
                     .addItem(new ItemStack(Material.POTION));
@@ -81,6 +93,7 @@ public class BlockCauldron extends BlockNeedsTool {
     }
 
     private boolean bleachBanner(GlowPlayer player, GlowBlock block) {
+        // fired when a player bleaches a banner using the cauldron
         if (player.getGameMode() == GameMode.CREATIVE) {
             return false;
         }
@@ -92,11 +105,12 @@ public class BlockCauldron extends BlockNeedsTool {
             if (layers == null || layers.isEmpty()) {
                 return false;
             }
-
+            if (!setCauldronLevel(block, block.getData() - 1, player,
+                    CauldronLevelChangeEvent.ChangeReason.BANNER_WASH)) {
+                return false;
+            }
             meta.setPatterns(layers);
             inHand.setItemMeta(meta);
-
-            block.setData((byte) (block.getData() - 1));
             return true;
         } else {
             return false;
@@ -104,16 +118,31 @@ public class BlockCauldron extends BlockNeedsTool {
     }
 
     private boolean bleachLeatherArmor(GlowPlayer player, GlowBlock block) {
+        // fired when a player bleaches a leather armor piece using the cauldron
         if (block.getData() > 0) {
+            if (!setCauldronLevel(block, block.getData() - 1, player,
+                    CauldronLevelChangeEvent.ChangeReason.ARMOR_WASH)) {
+                return false;
+            }
             ItemStack inHand = player.getItemInHand();
             LeatherArmorMeta im = (LeatherArmorMeta) inHand.getItemMeta();
             im.setColor(GlowItemFactory.instance().getDefaultLeatherColor());
             inHand.setItemMeta(im);
-            block.setData((byte) (block.getData() - 1));
             return true;
         } else {
             return false;
         }
+    }
+
+    private boolean setCauldronLevel(GlowBlock block, int newLevel, Entity entity,
+                                     CauldronLevelChangeEvent.ChangeReason reason) {
+        int oldLevel = (int) block.getData();
+        CauldronLevelChangeEvent event = EventFactory.getInstance().callEvent(
+                new CauldronLevelChangeEvent(block, entity, reason, oldLevel, newLevel));
+        if (!event.isCancelled()) {
+            block.setData((byte) newLevel);
+        }
+        return !event.isCancelled();
     }
 
     @Override
