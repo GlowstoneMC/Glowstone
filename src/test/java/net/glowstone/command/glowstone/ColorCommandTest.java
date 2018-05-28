d7c4058512b6ad20a6abc41856018cdeda6420d2
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-05-28 13:54:37 -0700

    Style tweak

--- src/test/java/net/glowstone/command/glowstone/ColorCommandTest.java
+++ src/test/java/net/glowstone/command/glowstone/ColorCommandTest.java
@@ -4,17 +4,20 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import net.glowstone.command.CommandTest;
+import org.junit.Test;
 
 public class ColorCommandTest extends CommandTest<ColorCommand> {
 
-    @Override
-    public void testExecuteWithoutPermission() {
-        assertThat(command.execute(sender, "label", new String[0]), is(true));
-    }
     // TODO: Add more tests.
 
     public ColorCommandTest() {
         super(ColorCommand::new);
+    }
+
+    @Test
+    @Override
+    public void testExecuteWithoutPermission() {
+        assertThat(command.execute(sender, "label", new String[0]), is(true));
     }
 }
 
