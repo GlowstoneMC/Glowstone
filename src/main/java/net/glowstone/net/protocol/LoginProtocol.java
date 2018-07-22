b99d3cc1965c2eb8129fbda7d9bd2b0dbd0d007d
momothereal <momothereal.mc@gmail.com>
2018-07-22 03:19:01 -0400

    Arrange new protocol opcodes, add todos for new packets

--- src/main/java/net/glowstone/net/protocol/LoginProtocol.java
+++ src/main/java/net/glowstone/net/protocol/LoginProtocol.java
@@ -26,10 +26,12 @@ public final class LoginProtocol extends GlowProtocol {
         inbound(0x00, LoginStartMessage.class, LoginStartCodec.class, LoginStartHandler.class);
         inbound(0x01, EncryptionKeyResponseMessage.class, EncryptionKeyResponseCodec.class,
             EncryptionKeyResponseHandler.class);
+        // TODO 0x02 : Login Plugin Request packet
 
         outbound(0x00, KickMessage.class, KickCodec.class);
         outbound(0x01, EncryptionKeyRequestMessage.class, EncryptionKeyRequestCodec.class);
         outbound(0x02, LoginSuccessMessage.class, LoginSuccessCodec.class);
         outbound(0x03, SetCompressionMessage.class, SetCompressionCodec.class);
+        // TODO 0x04 : Login Plugin Response packet
     }
 }
