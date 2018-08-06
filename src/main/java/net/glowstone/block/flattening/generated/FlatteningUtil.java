9d73673354f79367d32feacc560fc45ec9730527
momothereal <momothereal.mc@gmail.com>
2018-08-06 05:33:12 -0400

    Add util to get base ID from state ID

--- src/main/java/net/glowstone/block/flattening/generated/FlatteningUtil.java
+++ src/main/java/net/glowstone/block/flattening/generated/FlatteningUtil.java
@@ -9,4 +9,8 @@ public class FlatteningUtil {
     public static List<BlockFace> getPossibleBlockFaces(Material material) {
         return new ArrayList<>(GeneratedFlatteningData.DIRECTIONAL_POSSIBLE_FACES.get(material));
     }
+
+    public static int getStateBaseId(int stateId) {
+        return GeneratedFlatteningData.STATE_BASE_IDS[stateId];
+    }
 }
