223ee80d6f144b989b696ddb1d58d85a2850b243
mastercoms <mastercoms@tuta.io>
2018-10-16 01:31:07 -0400

    Update OrePopulator to 1.13

---
+++ src/main/java/net/glowstone/generator/populators/common/BaseOrePopulator.java
@@ -0,0 +1,35 @@
+package net.glowstone.generator.populators.common;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import net.glowstone.generator.objects.OreType;
+import net.glowstone.generator.objects.OreVein;
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.generator.BlockPopulator;
+
+public class BaseOrePopulator extends BlockPopulator {
+    protected final Map<OreType, Integer> ores = new LinkedHashMap<>();
+
+    @Override
+    public void populate(World world, Random random, Chunk chunk) {
+        int cx = chunk.getX() << 4;
+        int cz = chunk.getZ() << 4;
+
+        for (Entry<OreType, Integer> entry : ores.entrySet()) {
+
+            OreType oreType = entry.getKey();
+            for (int n = 0; n < entry.getValue(); n++) {
+
+                int sourceX = cx + random.nextInt(16);
+                int sourceZ = cz + random.nextInt(16);
+                int sourceY = oreType.getRandomHeight(random);
+
+                new OreVein(oreType).generate(world, random, sourceX, sourceY, sourceZ);
+            }
+        }
+    }
+
+}
