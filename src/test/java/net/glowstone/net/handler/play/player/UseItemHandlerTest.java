27f4edecd49d20fb2c7edb5e6b368eb2c43218a9
John Lehmann <j1o1h1n@gmail.com>
2018-11-13 23:41:31 +1100

    Cleanup test

--- src/test/java/net/glowstone/net/handler/play/player/UseItemHandlerTest.java
+++ src/test/java/net/glowstone/net/handler/play/player/UseItemHandlerTest.java
@@ -4,6 +4,7 @@ import net.glowstone.EventFactory;
 import net.glowstone.GlowServer;
 import net.glowstone.GlowWorld;
 import net.glowstone.block.GlowBlock;
+import net.glowstone.block.itemtype.ItemTypeTest;
 import net.glowstone.entity.EntityIdManager;
 import net.glowstone.entity.EntityManager;
 import net.glowstone.entity.GlowPlayer;
@@ -20,77 +21,96 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.Set;
 
-public class UseItemHandlerTest {
+public class UseItemHandlerTest extends ItemTypeTest {
+
+    private EventFactory actualEventFactory;
+
+    private GlowWorld world;
+    private GlowServer server;
+    private EntityManager entityManager;
+    private EntityIdManager entityIdManager;
+    private GlowSession session;
+    private GlowPlayer player;
+    private GlowBlock emptyBlock;
+    private GlowBlock targetBlock;
+    private EventFactory eventFactory;
+
+    @Before
+    public void setupMocks() {
+        // cache the event factory
+        actualEventFactory = EventFactory.getInstance();
+        // install the mock event factory
+        eventFactory = Mockito.mock(EventFactory.class);
+        EventFactory.setInstance(eventFactory);
+
+        // create mocks
+        world = Mockito.mock(GlowWorld.class);
+        server = Mockito.mock(GlowServer.class);
+        entityManager = Mockito.mock(EntityManager.class);
+        entityIdManager = Mockito.mock(EntityIdManager.class);
+        session = Mockito.mock(GlowSession.class);
+        player = Mockito.mock(GlowPlayer.class);
+        emptyBlock = Mockito.mock(GlowBlock.class);
+        targetBlock = Mockito.mock(GlowBlock.class);
+
+        // world and server behaviours
+        Mockito.when(world.getEntityManager()).thenReturn(entityManager);
+        Mockito.when(world.getServer()).thenReturn(server);
+        Mockito.when(server.getEntityIdManager()).thenReturn(entityIdManager);
+    }
 
     @Test
     public void testRightClickAir() throws Exception {
-        EventFactory origEventFactory = EventFactory.getInstance();
-        try {
-            // setup mocks
-            GlowWorld world = Mockito.mock(GlowWorld.class);
-            GlowServer server = Mockito.mock(GlowServer.class);
-            EntityManager entityManager = Mockito.mock(EntityManager.class);
-            EntityIdManager entityIdManager = Mockito.mock(EntityIdManager.class);
-            GlowSession session = Mockito.mock(GlowSession.class);
-            GlowPlayer player = Mockito.mock(GlowPlayer.class);
-            GlowBlock emptyBlock = Mockito.mock(GlowBlock.class);
-            GlowBlock targetBlock = Mockito.mock(GlowBlock.class);
-            EventFactory eventFactory = Mockito.mock(EventFactory.class);
-            EventFactory.setInstance(eventFactory);
+        // prepare objects under test
+        Location location = new Location(world, 1.0, 1.0, 1.0);
+        Location playerLocation = new Location(world, 2.0, 2.0, 2.0);
+        GlowPlayerInventory inventory = new GlowPlayerInventory(player);
+        ItemStack stack = new ItemStack(Material.BOAT_JUNGLE);
+        GlowBoat boat = new GlowBoat(location);
+        inventory.setItemInMainHand(stack);
+        UseItemMessage message = new UseItemMessage(EquipmentSlot.HAND.ordinal());
+        PlayerInteractEvent event = new PlayerInteractEvent(player,
+                Action.RIGHT_CLICK_AIR, stack, null, null);
 
-            // world and server behaviours
-            Mockito.when(world.getEntityManager()).thenReturn(entityManager);
-            Mockito.when(world.getServer()).thenReturn(server);
-            Mockito.when(server.getEntityIdManager()).thenReturn(entityIdManager);
+        // prepare mock behaviours
+        Mockito.when(session.getPlayer()).thenReturn(player);
+        Mockito.when(player.getInventory()).thenReturn(inventory);
+        Mockito.when(player.getLocation()).thenReturn(playerLocation);
+        Mockito.when(player.getTargetBlock((Set<Material>) null,
+                5)).thenReturn(targetBlock);
+        Mockito.when(player.getGameMode()).thenReturn(GameMode.SURVIVAL);
+        Mockito.when(eventFactory.onPlayerInteract(player, Action.RIGHT_CLICK_AIR,
+                EquipmentSlot.HAND)).thenReturn(event);
 
-            // setup items under test
-            Location location = new Location(world, 1.0, 1.0, 1.0);
-            Location playerLocation = new Location(world, 2.0, 2.0, 2.0);
-            GlowPlayerInventory inventory = new GlowPlayerInventory(player);
-            Assert.assertFalse(inventory.contains(Material.BOAT_JUNGLE, 1));
+        Mockito.when(emptyBlock.isEmpty()).thenReturn(Boolean.TRUE);
+        Mockito.when(emptyBlock.getLocation()).thenReturn(location);
+        Mockito.when(targetBlock.isEmpty()).thenReturn(Boolean.FALSE);
+        Mockito.when(targetBlock.getRelative(BlockFace.UP)).thenReturn(emptyBlock);
+        Mockito.when(targetBlock.getWorld()).thenReturn(world);
+        Mockito.when(world.spawn(location, Boat.class)).thenReturn(boat);
 
-            ItemStack stack = new ItemStack(Material.BOAT_JUNGLE);
-            inventory.setItemInMainHand(stack);
-            Assert.assertTrue(inventory.contains(Material.BOAT_JUNGLE, 1));
-
-            UseItemMessage message = new UseItemMessage(EquipmentSlot.HAND.ordinal());
-            PlayerInteractEvent event = new PlayerInteractEvent(player,
-                    Action.RIGHT_CLICK_AIR, stack, null, null);
-            GlowBoat boat = new GlowBoat(location);
-
-            // prepare mock behaviours
-            Mockito.when(session.getPlayer()).thenReturn(player);
-            Mockito.when(player.getInventory()).thenReturn(inventory);
-            Mockito.when(player.getLocation()).thenReturn(playerLocation);
-            Mockito.when(player.getTargetBlock((Set<Material>) null,
-                    5)).thenReturn(targetBlock);
-            Mockito.when(player.getGameMode()).thenReturn(GameMode.SURVIVAL);
-            Mockito.when(eventFactory.onPlayerInteract(player, Action.RIGHT_CLICK_AIR,
-                    EquipmentSlot.HAND)).thenReturn(event);
-
-            Mockito.when(emptyBlock.isEmpty()).thenReturn(Boolean.TRUE);
-            Mockito.when(emptyBlock.getLocation()).thenReturn(location);
-            Mockito.when(targetBlock.isEmpty()).thenReturn(Boolean.FALSE);
-            Mockito.when(targetBlock.getRelative(BlockFace.UP)).thenReturn(emptyBlock);
-            Mockito.when(targetBlock.getWorld()).thenReturn(world);
-            Mockito.when(world.spawn(location, Boat.class)).thenReturn(boat);
-
-            // run test
-            Assert.assertTrue(inventory.contains(Material.BOAT_JUNGLE, 1));
-            UseItemHandler handler = new UseItemHandler();
-            handler.handle(session, message);
-            Assert.assertFalse(inventory.contains(Material.BOAT_JUNGLE, 1));
-        } finally {
-            EventFactory.setInstance(origEventFactory);
-        }
+        // run test
+        Assert.assertTrue(inventory.contains(Material.BOAT_JUNGLE, 1));
+        UseItemHandler handler = new UseItemHandler();
+        handler.handle(session, message);
+        // after calling use item and creating a boat, the inventory no longer contains a boat
+        Assert.assertFalse(inventory.contains(Material.BOAT_JUNGLE, 1));
     }
 
+
+    @After
+    public void tearDown() {
+        // restore the original event factory
+        EventFactory.setInstance(actualEventFactory);
+    }
 
 
 }
