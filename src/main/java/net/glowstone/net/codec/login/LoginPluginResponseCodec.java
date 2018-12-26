10273d657661f3130d0f16a7551d41df64865214
momothereal <momothereal.mc@gmail.com>
2018-12-26 16:33:36 -0500

    Add LoginPluginRequest and LoginPluginResponse packets

---
+++ src/main/java/net/glowstone/net/codec/login/LoginPluginResponseCodec.java
@@ -0,0 +1,28 @@
+package net.glowstone.net.codec.login;
+
+import com.flowpowered.network.Codec;
+import com.flowpowered.network.util.ByteBufUtils;
+import io.netty.buffer.ByteBuf;
+import java.io.IOException;
+import net.glowstone.net.message.login.LoginPluginResponseMessage;
+
+public class LoginPluginResponseCodec implements Codec<LoginPluginResponseMessage> {
+    @Override
+    public LoginPluginResponseMessage decode(ByteBuf buf) throws IOException {
+        int transactionId = ByteBufUtils.readVarInt(buf);
+        boolean successful = buf.readBoolean();
+        int remainingBytes = buf.readableBytes();
+        ByteBuf data = buf.readBytes(remainingBytes);
+
+        return new LoginPluginResponseMessage(transactionId, successful, data);
+    }
+
+    @Override
+    public ByteBuf encode(ByteBuf buf, LoginPluginResponseMessage message) throws IOException {
+        ByteBufUtils.writeVarInt(buf, message.getTransactionId());
+        buf.writeBoolean(message.isSuccessful());
+        buf.writeBytes(message.getData());
+
+        return buf;
+    }
+}
