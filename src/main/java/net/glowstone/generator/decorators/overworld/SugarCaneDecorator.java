249f660a7e1103eac7918dd63984ac0ed1b2c20c
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-12-27 19:31:12 -0800

    Fix an exception from SugarCaneDecorator in holey worlds

--- src/main/java/net/glowstone/generator/decorators/overworld/SugarCaneDecorator.java
+++ src/main/java/net/glowstone/generator/decorators/overworld/SugarCaneDecorator.java
@@ -12,11 +12,14 @@ public class SugarCaneDecorator extends BlockDecorator {
     public void decorate(World world, Random random, Chunk source) {
         int sourceX = (source.getX() << 4) + random.nextInt(16);
         int sourceZ = (source.getZ() << 4) + random.nextInt(16);
-        int sourceY = random.nextInt(world.getHighestBlockYAt(sourceX, sourceZ) << 1);
-        for (int j = 0; j < 20; j++) {
-            int x = sourceX + random.nextInt(4) - random.nextInt(4);
-            int z = sourceZ + random.nextInt(4) - random.nextInt(4);
-            new SugarCane().generate(world, random, x, sourceY, z);
+        int maxY = world.getHighestBlockYAt(sourceX, sourceZ);
+        if (maxY > 0) {
+            int sourceY = random.nextInt(maxY << 1);
+            for (int j = 0; j < 20; j++) {
+                int x = sourceX + random.nextInt(4) - random.nextInt(4);
+                int z = sourceZ + random.nextInt(4) - random.nextInt(4);
+                new SugarCane().generate(world, random, x, sourceY, z);
+            }
         }
     }
 }
