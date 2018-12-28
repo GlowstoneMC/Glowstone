5ed496978351a1ebaac381bd1f782a4156be4f93
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2018-12-27 19:32:34 -0800

    Refactor for shallower nesting

--- src/main/java/net/glowstone/generator/decorators/overworld/SugarCaneDecorator.java
+++ src/main/java/net/glowstone/generator/decorators/overworld/SugarCaneDecorator.java
@@ -13,13 +13,14 @@ public class SugarCaneDecorator extends BlockDecorator {
         int sourceX = (source.getX() << 4) + random.nextInt(16);
         int sourceZ = (source.getZ() << 4) + random.nextInt(16);
         int maxY = world.getHighestBlockYAt(sourceX, sourceZ);
-        if (maxY > 0) {
-            int sourceY = random.nextInt(maxY << 1);
-            for (int j = 0; j < 20; j++) {
-                int x = sourceX + random.nextInt(4) - random.nextInt(4);
-                int z = sourceZ + random.nextInt(4) - random.nextInt(4);
-                new SugarCane().generate(world, random, x, sourceY, z);
-            }
+        if (maxY <= 0) {
+            return;
+        }
+        int sourceY = random.nextInt(maxY << 1);
+        for (int j = 0; j < 20; j++) {
+            int x = sourceX + random.nextInt(4) - random.nextInt(4);
+            int z = sourceZ + random.nextInt(4) - random.nextInt(4);
+            new SugarCane().generate(world, random, x, sourceY, z);
         }
     }
 }
