b5cc02a269d327f1ee735afcb7b6964684b209f2
VaiTon <eyadlorenzo@gmail.com>
2019-02-10 17:55:42 +0100

    Regional difficulty (#1019)

--- src/main/java/net/glowstone/io/anvil/AnvilChunkIoService.java
+++ src/main/java/net/glowstone/io/anvil/AnvilChunkIoService.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 import net.glowstone.block.GlowBlock;
 import net.glowstone.block.ItemTable;
 import net.glowstone.block.blocktype.BlockType;
@@ -85,6 +86,7 @@ public final class AnvilChunkIoService implements ChunkIoService {
         // initialize the chunk
         chunk.initializeSections(sections);
         chunk.setPopulated(levelTag.getBoolean("TerrainPopulated", false)); // NON-NLS
+        levelTag.readLong("InhabitedTime", chunk::setInhabitedTime);
 
         // read biomes
         levelTag.readByteArray("Biomes", chunk::setBiomes); // NON-NLS
@@ -172,8 +174,9 @@ public final class AnvilChunkIoService implements ChunkIoService {
         // core properties
         levelTags.putInt("xPos", chunk.getX()); // NON-NLS
         levelTags.putInt("zPos", chunk.getZ()); // NON-NLS
-        levelTags.putBool("TerrainPopulated", chunk.isPopulated()); // NON-NLS
         levelTags.putLong("LastUpdate", 0); // NON-NLS
+        levelTags.putLong("InhabitedTime", chunk.getInhabitedTime()); // NON-NLS
+        levelTags.putBool("TerrainPopulated", chunk.isPopulated()); // NON-NLS
 
         // chunk sections
         List<CompoundTag> sectionTags = new ArrayList<>();
