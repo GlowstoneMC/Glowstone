27725af076de984dd49f918696731627a316a3ca
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-05-07 15:38:22 -0700

    Strengthen MaterialUtil sanity check

--- src/test/java/net/glowstone/block/MaterialUtilTest.java
+++ src/test/java/net/glowstone/block/MaterialUtilTest.java
@@ -1,14 +1,21 @@
 package net.glowstone.block;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Collection;
 import org.junit.jupiter.api.Test;
 
 class MaterialUtilTest {
     @Test
     public void testSanityChecks() {
-        assertTrue(MaterialUtil.SLABS.containsAll(MaterialUtil.WOODEN_SLABS));
-        assertTrue(MaterialUtil.STAIRS.containsAll(MaterialUtil.WOODEN_STAIRS));
-        assertTrue(MaterialUtil.BUTTONS.containsAll(MaterialUtil.WOODEN_BUTTONS));
+        assertContainsAllAndNotOnly(MaterialUtil.SLABS, MaterialUtil.WOODEN_SLABS);
+        assertContainsAllAndNotOnly(MaterialUtil.STAIRS, MaterialUtil.WOODEN_STAIRS);
+        assertContainsAllAndNotOnly(MaterialUtil.BUTTONS, MaterialUtil.WOODEN_BUTTONS);
+    }
+
+    private static void assertContainsAllAndNotOnly(Collection<?> superset, Collection<?> subset) {
+        assertTrue(superset.containsAll(subset));
+        assertFalse(subset.containsAll(superset))
     }
 }
