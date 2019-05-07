3a9c79013e5e3815413e0e1ba52a04de8e9cf034
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-05-07 15:01:18 -0700

    Update FlowerType

--- src/main/java/net/glowstone/generator/objects/FlowerType.java
+++ src/main/java/net/glowstone/generator/objects/FlowerType.java
@@ -6,20 +6,18 @@ import org.bukkit.Material;
 
 @RequiredArgsConstructor
 public enum FlowerType {
-    DANDELION(Material.YELLOW_FLOWER, 0),
-    POPPY(Material.RED_ROSE, 0),
-    BLUE_ORCHID(Material.RED_ROSE, 1),
-    ALLIUM(Material.RED_ROSE, 2),
-    HOUSTONIA(Material.RED_ROSE, 3),
-    TULIP_RED(Material.RED_ROSE, 4),
-    TULIP_ORANGE(Material.RED_ROSE, 5),
-    TULIP_WHITE(Material.RED_ROSE, 6),
-    TULIP_PINK(Material.RED_ROSE, 7),
-    OXEYE_DAISY(Material.RED_ROSE, 8);
+    DANDELION(Material.DANDELION),
+    POPPY(Material.POPPY),
+    BLUE_ORCHID(Material.BLUE_ORCHID),
+    ALLIUM(Material.ALLIUM),
+    /* TODO: Update */ HOUSTONIA(Material.LEGACY_RED_ROSE),
+    TULIP_RED(Material.RED_TULIP),
+    TULIP_ORANGE(Material.ORANGE_TULIP),
+    TULIP_WHITE(Material.WHITE_TULIP),
+    TULIP_PINK(Material.PINK_TULIP),
+    OXEYE_DAISY(Material.OXEYE_DAISY);
 
     @Getter
     private final Material type;
-    @Getter
-    private final int data;
 }
 
