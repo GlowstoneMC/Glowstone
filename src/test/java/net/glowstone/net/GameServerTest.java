c88c0d8afc943d21b0e74c1f6ca2d844e76809bb
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-05-29 19:53:14 -0700

    Bug fix: install a log handler, rather than using reflection

--- src/test/java/net/glowstone/net/GameServerTest.java
+++ src/test/java/net/glowstone/net/GameServerTest.java
@@ -1,12 +1,18 @@
 package net.glowstone.net;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.lang.reflect.Field;
 import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.logging.Handler;
 import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import net.glowstone.GlowServer;
 import org.junit.jupiter.api.AfterEach;
@@ -20,6 +26,23 @@ import org.powermock.reflect.Whitebox;
 public class GameServerTest {
 
     private static final InetSocketAddress LOCALHOST_IPV4;
+    private final List<LogRecord> logRecords = new ArrayList<>();
+    private final Handler HANDLER = new Handler() {
+        @Override
+        public void publish(LogRecord logRecord) {
+            logRecords.add(logRecord);
+        }
+
+        @Override
+        public void flush() {
+            // No-op.
+        }
+
+        @Override
+        public void close() {
+            // No-op.
+        }
+    };
 
     static {
         try {
@@ -37,14 +60,12 @@ public class GameServerTest {
 
     @AfterEach
     public void tearDownClass() throws IllegalAccessException {
-        setLogger(unspiedLogger);
+        GlowServer.logger.removeHandler(HANDLER);
     }
 
     @BeforeEach
     public void setUp() throws IllegalAccessException {
-        unspiedLogger = GlowServer.logger;
-        logger = Mockito.mock(Logger.class);
-        setLogger(logger);
+        GlowServer.logger.addHandler(HANDLER);
         glowServer = Mockito.mock(GlowServer.class, Answers.RETURNS_SMART_NULLS);
         CountDownLatch latch = new CountDownLatch(1);
         gameServer = new GameServer(glowServer, latch);
@@ -60,12 +81,23 @@ public class GameServerTest {
     @Test
     public void testOnBindSuccess() throws UnknownHostException {
         gameServer.onBindSuccess(LOCALHOST_IPV4);
-        Mockito.verify(logger).log(Level.INFO, "Successfully bound server to 127.0.0.1:25565.");
+        assertLoggedExactlyOnce("Successfully bound server to 127.0.0.1:25565.", Level.INFO);
     }
 
     @Test
     public void testOnBindFailure() {
         gameServer.onBindFailure(LOCALHOST_IPV4, new ConnectException("Exception-handling test"));
-        Mockito.verify(logger).log(Level.SEVERE, "Failed to bind server to 127.0.0.1:25565.");
+        assertLoggedExactlyOnce("Failed to bind server to 127.0.0.1:25565.", Level.SEVERE);
+    }
+
+    private void assertLoggedExactlyOnce(String expectedMessage, Level expectedLevel) {
+        int matches = 0;
+        for (LogRecord logRecord : logRecords) {
+            if (logRecord.getMessage().equals(expectedMessage)) {
+                matches++;
+                assertEquals(expectedLevel, logRecord.getLevel());
+            }
+        }
+        assertEquals(1, matches);
     }
 }
