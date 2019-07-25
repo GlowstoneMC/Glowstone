8b4e87b94c5dcc64d6820e23c068e67c456d30c7
mastercoms <mastercoms@tuta.io>
2019-07-25 13:49:14 -0400

    update material values

--- src/main/java/net/glowstone/block/BuiltinMaterialValueManager.java
+++ src/main/java/net/glowstone/block/BuiltinMaterialValueManager.java
@@ -43,7 +43,7 @@ public class BuiltinMaterialValueManager implements MaterialValueManager {
             }
             ConfigurationSection materialSection
                     = valuesSection.getConfigurationSection(strMaterial);
-            values.put(material, new BuiltinValueCollection(materialSection));
+            values.put(material, new BuiltinValueCollection(material, materialSection));
         }
     }
 
@@ -57,9 +57,15 @@ public class BuiltinMaterialValueManager implements MaterialValueManager {
 
     private final class BuiltinValueCollection implements ValueCollection {
 
+        private Material material;
         private final ConfigurationSection section;
 
         BuiltinValueCollection(ConfigurationSection section) {
+            this.section = section;
+        }
+
+        BuiltinValueCollection(Material material, ConfigurationSection section) {
+            this.material = material;
             this.section = section;
         }
 
@@ -73,8 +79,11 @@ public class BuiltinMaterialValueManager implements MaterialValueManager {
 
         @Override
         public float getHardness() {
-            float hardness = ((Number) get("hardness")).floatValue();
-            return hardness == -1 ? Float.MAX_VALUE : hardness;
+            if (material == null) {
+                float hardness = ((Number) get("hardness")).floatValue();
+                return hardness == -1 ? Float.MAX_VALUE : hardness;
+            }
+            return material.getHardness();
         }
 
         @Override
@@ -85,7 +94,10 @@ public class BuiltinMaterialValueManager implements MaterialValueManager {
 
         @Override
         public float getBlastResistance() {
-            return ((Number) get("blastResistance")).floatValue();
+            if (material == null) {
+                return ((Number) get("blastResistance")).floatValue();
+            }
+            return material.getBlastResistance();
         }
 
         @Override
