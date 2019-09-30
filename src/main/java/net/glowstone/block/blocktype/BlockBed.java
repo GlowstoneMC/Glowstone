f81a6b0d3dedc5b630ee2630cae4e3c9b3f30c00
Alex Pazderp <alexPazder@hotmail.ca>
2019-09-30 01:02:50 -0700

    Update BlockBed.java

--- src/main/java/net/glowstone/block/blocktype/BlockBed.java
+++ src/main/java/net/glowstone/block/blocktype/BlockBed.java
@@ -118,7 +118,7 @@ public class BlockBed extends BlockType {
                 block.setType(Material.AIR);
             }
         }
-        if (changedBlock.equals(getHead(block).getRelative(BlockFace.DOWN)) || (changedBlock.equals(getFoot(block).getRelative(BlockFace.DOWN))) {
+        if (changedBlock.equals(getHead(block).getRelative(BlockFace.DOWN)) || changedBlock.equals(getFoot(block).getRelative(BlockFace.DOWN))) {
             if (newType == Material.AIR) {
                 block.setType(Material.AIR);
             }
