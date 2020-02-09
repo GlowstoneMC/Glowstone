45eb4d205cbee452accec00cce9396a003c8e074
mosemister <nekronmoseley@gmail.com>
2020-02-09 15:44:56 +0000

    Updated StateUtil

--- src/main/java/net/glowstone/block/data/state/StateUtil.java
+++ src/main/java/net/glowstone/block/data/state/StateUtil.java
@@ -4,7 +4,24 @@ import org.bukkit.block.BlockFace;
 
 public interface StateUtil {
 
-    BlockFace[] SIXTEEN_BLOCK_FACES = {BlockFace.SOUTH, BlockFace.SOUTH_SOUTH_WEST, BlockFace.SOUTH_WEST, BlockFace.WEST_SOUTH_WEST, BlockFace.WEST, BlockFace.WEST_NORTH_WEST, BlockFace.NORTH_WEST, BlockFace.NORTH_NORTH_WEST, BlockFace.NORTH, BlockFace.NORTH_NORTH_EAST, BlockFace.NORTH_EAST, BlockFace.EAST_NORTH_EAST, BlockFace.EAST, BlockFace.EAST_SOUTH_EAST, BlockFace.EAST, BlockFace.EAST_SOUTH_EAST, BlockFace.SOUTH_EAST, BlockFace.SOUTH_SOUTH_EAST};
+    BlockFace[] SIXTEEN_BLOCK_FACES = {
+            BlockFace.SOUTH,
+            BlockFace.SOUTH_SOUTH_WEST,
+            BlockFace.SOUTH_WEST,
+            BlockFace.WEST_SOUTH_WEST,
+            BlockFace.WEST,
+            BlockFace.WEST_NORTH_WEST,
+            BlockFace.NORTH_WEST,
+            BlockFace.NORTH_NORTH_WEST,
+            BlockFace.NORTH,
+            BlockFace.NORTH_NORTH_EAST,
+            BlockFace.NORTH_EAST,
+            BlockFace.EAST_NORTH_EAST,
+            BlockFace.EAST,
+            BlockFace.EAST_SOUTH_EAST,
+            BlockFace.SOUTH_EAST,
+            BlockFace.SOUTH_SOUTH_EAST
+    };
 
     static BlockFace getBlockFace(int id, BlockFace... faces){
         return faces[id];
