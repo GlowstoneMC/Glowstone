10c53867ff7daa8b04d156fb29ae32215f3f69df
mastercoms <mastercoms@tuta.io>
2021-01-16 14:31:15 -0500

    remove import group in LinkstonePluginScanner

--- src/main/java/net/glowstone/util/linkstone/LinkstonePluginScanner.java
+++ src/main/java/net/glowstone/util/linkstone/LinkstonePluginScanner.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-
 import net.glowstone.linkstone.annotations.LField;
 import net.glowstone.linkstone.runtime.Boxes;
 import net.glowstone.linkstone.runtime.FieldSet;
