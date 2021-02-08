5ec1a5a5d2e1e4d8c64b2ea5c7196740efed08c6
x4e <x4e_x4e@protonmail.com>
2021-02-08 14:05:03 +0000

    Improve GlowAdvancementDisplay documentation

--- src/main/java/net/glowstone/advancement/GlowAdvancementDisplay.java
+++ src/main/java/net/glowstone/advancement/GlowAdvancementDisplay.java
@@ -3,6 +3,8 @@ package net.glowstone.advancement;
 import com.flowpowered.network.util.ByteBufUtils;
 import io.netty.buffer.ByteBuf;
 import java.io.IOException;
+import java.util.Objects;
+
 import lombok.Data;
 import net.glowstone.net.GlowBufUtils;
 import net.glowstone.util.TextMessage;
@@ -12,13 +14,33 @@ import org.bukkit.inventory.ItemStack;
 
 @Data
 public class GlowAdvancementDisplay {
-
+    /**
+     * The title for this advancement
+     */
     private final TextMessage title;
+    /**
+     * The description for this advancement
+     */
     private final TextMessage description;
+    /**
+     * The icon to represent this advancement
+     */
     private final ItemStack icon;
+    /**
+     * The type of frame for the icon
+     */
     private final FrameType type;
+    /**
+     * The optional directory for the background to use in this advancement tab (used only for the root advancement)
+     */
     private final NamespacedKey background;
+    /**
+     * The x coordinate of the advancement
+     */
     private final float x;
+    /**
+     * The y coordinate of the advancement
+     */
     private final float y;
 
     private static final int HAS_BACKGROUND_TEXTURE = 0x1;
@@ -30,8 +52,8 @@ public class GlowAdvancementDisplay {
      *
      * @param buf the buffer to write to
      * @param hasBackgroundTexture Whether the advancement notification has a background texture
-     * @param showToast Whether the advancement should be displayed as a toast notification
-     * @param hidden
+     * @param showToast Whether or not to show the toast pop up after completing this advancement
+     * @param hidden Whether or not to hide this advancement and all its children from the advancement screen until this advancement have been completed
      * @return {@code buf}, with this notification written to it
      * @throws IOException if a string is too long
      */
@@ -45,13 +67,24 @@ public class GlowAdvancementDisplay {
         GlowBufUtils.writeSlot(buf, icon);
         ByteBufUtils.writeVarInt(buf, type.ordinal());
         buf.writeInt(flags);
-        ByteBufUtils.writeUTF8(buf, background.toString());
+        ByteBufUtils.writeUTF8(buf, Objects.toString(background, null));
         buf.writeFloat(x);
         buf.writeFloat(y);
         return buf;
     }
 
     public enum FrameType {
-        TASK, CHALLENGE, GOAL
+        /**
+         * Normal Tile
+         */
+        TASK,
+        /**
+         * A tile with a more fancy spiked border, used for the kill all mobs advancement
+         */
+        CHALLENGE,
+        /**
+         * A tile with a rounded border, used for the full beacon advancement
+         */
+        GOAL
     }
 }
