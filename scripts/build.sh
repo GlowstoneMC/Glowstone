e4863f062b2dde4733defe0fe3311b743eb562e4
mastercoms <mastercoms@tuta.io>
2021-07-01 15:46:26 -0400

    fix build flags

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ BINDIR=$(dirname "$(readlink -fn "$0")")
 cd "$BINDIR"
 
 # Build Glowstone
-(cd .. && MAVEN_OPTS="-Xss4m -XX:+TieredCompilation -XX:TieredStopAtLevel=1" mvn -Dmaven.test.skip=true -T 1C -B package)
+(cd .. && mvn -Dmaven.test.skip=true -T 1C -B package)
