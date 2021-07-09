31d9df122fd046467f67e23bb4e6cebb7e13192c
mastercoms <mastercoms@tuta.io>
2021-07-08 21:59:33 -0400

    fix import order

--- src/main/java/net/glowstone/net/codec/login/LoginPluginResponseCodec.java
+++ src/main/java/net/glowstone/net/codec/login/LoginPluginResponseCodec.java
@@ -2,9 +2,10 @@ package net.glowstone.net.codec.login;
 
 import com.flowpowered.network.Codec;
 import com.flowpowered.network.util.ByteBufUtils;
-import io.netty.buffer.ByteBuf;
-import java.io.IOException;
 import net.glowstone.net.message.login.LoginPluginResponseMessage;
+import io.netty.buffer.ByteBuf;
+
+import java.io.IOException;
 
 public class LoginPluginResponseCodec implements Codec<LoginPluginResponseMessage> {
     @Override
