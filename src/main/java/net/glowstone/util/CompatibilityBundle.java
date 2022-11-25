4be18cb992f6ed827b4fc2443dfe621bc1bccfeb
mcoms <mastercoms@tuta.io>
2022-11-25 06:05:25 -0500

    port security fix to 1.12 branch

--- src/main/java/net/glowstone/util/CompatibilityBundle.java
+++ src/main/java/net/glowstone/util/CompatibilityBundle.java
@@ -22,12 +22,12 @@ public enum CompatibilityBundle {
                 LibraryManager.HashAlgorithm.SHA1, "81a0bcda2f100dc91dc402554f60ed2f696cded5"),
             new Library("mysql", "mysql-connector-java", "5.1.46",
                 LibraryManager.HashAlgorithm.SHA1, "9a3e63b387e376364211e96827bc27db8d7a92e9"),
-            new Library("org.apache.logging.log4j", "log4j-slf4j-impl", "2.8.1",
-                    LibraryManager.HashAlgorithm.SHA1, "709b07000087f3a0dec1bc4b5e05281499588cae"),
-            new Library("org.apache.logging.log4j", "log4j-core", "2.8.1",
-                    LibraryManager.HashAlgorithm.SHA1, "4ac28ff2f1ddf05dae3043a190451e8c46b73c31"),
-            new Library("org.apache.logging.log4j", "log4j-iostreams", "2.8.1",
-                    LibraryManager.HashAlgorithm.SHA1, "dab5403f14a8a7c553850066f9979563d19c1632"),
+            new Library("org.apache.logging.log4j", "log4j-slf4j-impl", "2.17.1",
+                    LibraryManager.HashAlgorithm.SHA1, "84692d456bcce689355d33d68167875e486954dd"),
+            new Library("org.apache.logging.log4j", "log4j-core", "2.17.1",
+                LibraryManager.HashAlgorithm.SHA1, "779f60f3844dadc3ef597976fcb1e5127b1f343d"),
+            new Library("org.apache.logging.log4j", "log4j-iostreams", "2.17.1",
+                LibraryManager.HashAlgorithm.SHA1, "6ebd6d2186fae85d294b5d1994d711242d314427"),
             new Library("org.apache.commons", "commons-lang3", "3.5",
                 LibraryManager.HashAlgorithm.SHA1, "6c6c702c89bfff3cd9e80b04d668c5e190d588c6")
         )
