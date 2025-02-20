04c759d413190493cf1e42ba24f309d51a1025e4
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-20 21:29:15 +0200

    remove space and rework

--- src/main/java/net/glowstone/util/Convert.java
+++ src/main/java/net/glowstone/util/Convert.java
@@ -1,6 +1,12 @@
 package net.glowstone.util;
 
-public class Convert {
+import static java.lang.Character.digit;
+import static java.nio.charset.StandardCharsets.US_ASCII;
+
+public final class Convert {
+
+    private Convert() {
+    }
 
     /**
      * Converts a hexadecimal string to a byte array.
@@ -9,14 +15,17 @@ public class Convert {
      * @return {@code hex} as a byte array
      */
     public static byte[] fromHex(String hex) {
-        int len = hex.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
-                + Character.digit(hex.charAt(i + 1), 16));
+        int i = hex.length();
+        char[] chars = hex.toCharArray();
+        byte[] data = new byte[i >> 1];
+        while (--i > -1) {
+            int j = i >> 1;
+            data[j] = (byte) (digit(chars[i], 16) + (digit(chars[--i], 16) << 4));
         }
         return data;
     }
+
+    private static final byte[] HEX = "0123456789ABCDEF".getBytes(US_ASCII);
 
     /**
      * Converts a byte array to a hexadecimal string.
@@ -25,10 +34,15 @@ public class Convert {
      * @return {@code bytes} as a hexadecimal string
      */
     public static String fromBytes(byte[] bytes) {
-        StringBuilder sb = new StringBuilder(40);
-        for (byte b : bytes) {
-            sb.append(String.format("%02X ", b)); // NON-NLS
+        int i = bytes.length;
+        int j = i << 1;
+        byte[] hexChars = new byte[j];
+        while (--i > -1) {
+            int v = bytes[i] & 0xFF;
+            hexChars[--j] = HEX[v & 0x0F];
+            hexChars[--j] = HEX[v >>> 4];
         }
-        return sb.toString();
+        return new String(hexChars, US_ASCII);
     }
+
 }
