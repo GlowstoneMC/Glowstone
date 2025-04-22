7e2db592af1abb9f044458bb674b27b76b91f5bb
vegaale <vegaale@umich.edu>
2025-04-22 19:54:29 -0400

    Initial investigation: added TODOEventHooks.java to track missing event API triggers

---
+++ src/main/java/net/glowstone/TODOEventHooks.java
@@ -0,0 +1,13 @@
+package net.glowstone;
+
+/**
+ * Temporary class to track missing Bukkit event hook points.
+ * 
+ * TODO: Hook PlayerInteractEvent in entity.PlayerInteractionManager
+ * TODO: Add InventoryClickEvent trigger inside inventory.InventoryMonitor
+ * 
+ * This class is for internal planning use only and will be removed after integration.
+ */
+public class TODOEventHooks {
+    // Placeholder for event audit tracking.
+}
